eecbf3f2e8156cc32cd86f9b85c6e7952860978b
Dev Steve <wearypossum4770@yahoo.com>
2024-02-16 18:42:48 -0500

    (test): leet-code #66

---
+++ test/easy/plus-one.test.ts
@@ -0,0 +1,34 @@
+import { describe, test, expect } from "bun:test";
+import { plusOne } from "~/easy/plus-one";
+describe("Add one to a large integer", () => {
+    test.each([
+        [
+            [1, 2, 3],
+            [1, 2, 4],
+        ],
+        [
+            [4, 3, 2, 1],
+            [4, 3, 2, 2],
+        ],
+        [[9], [1, 0]],
+        [[0], [1]],
+        [
+            [1, 8],
+            [1, 9],
+        ],
+        [
+            [1, 9],
+            [2, 0],
+        ],
+        [
+            [9, 9],
+            [1, 0, 0],
+        ],
+    ])(
+        "Given a large integer represented as %s add one to the value to equal %s",
+        async (digits, output) => {
+            const result = plusOne(digits);
+            expect(result).toStrictEqual(output);
+        },
+    );
+});
