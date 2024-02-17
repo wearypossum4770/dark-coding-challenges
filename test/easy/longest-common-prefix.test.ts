67745757aa7b5626ac09685139208c1daea591bd
Dev Steve <wearypossum4770@yahoo.com>
2024-02-16 19:18:18 -0500

    (test): update test file

--- test/easy/longest-common-prefix.test.ts
+++ test/easy/longest-common-prefix.test.ts
@@ -1,13 +1,15 @@
 import { describe, test, expect } from "bun:test";
-import { longestCommonPrefix } from '~/easy/longest-common-prefix'
-
+import { longestCommonPrefix } from "~/easy/longest-common-prefix";
 
 describe('Find longest common "prefix"', () => {
     test.each([
-[["flower","flow","flight"] , 'fl'],
-[["dog","racecar","car"] , ''],
-    ])('Given the array of %o the longest common continuous string is %s', (strs, output) => {
-        const result = longestCommonPrefix(strs)
-        expect(result).toStrictEqual(output)
-    })
-})
+        [["flower", "flow", "flight"], "fl"],
+        [["dog", "racecar", "car"], ""],
+    ])(
+        "Given the array of %o the longest common continuous string is %s",
+        async (strs, output) => {
+            const result = longestCommonPrefix(strs);
+            expect(result).toStrictEqual(output);
+        },
+    );
+});
