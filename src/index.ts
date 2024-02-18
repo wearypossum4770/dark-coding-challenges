59047b4a865fa86a7dca6d3ac29cc8ecba1fa13f
Dev Steve <wearypossum4770@yahoo.com>
2024-02-18 13:02:46 -0500

    (module): create js entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import * as easy from './easy'
+import * as easy from "./easy";
 
+export { easy };
 
-export { easy }
+export const max = (arr: number[]): number => Math.max.apply(null, arr);
+export const countArrayString = (arr: string[]) =>
+    arr.map(({ length }) => length);
+export const longestArrayString = (arr: string[]) => max(countArrayString(arr));
