ebf1c735b2850d27330765fbb0c69f681548a986
Dev Steve <wearypossum4770@yahoo.com>
2024-02-18 13:02:56 -0500

    (module): create rust entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod easy;
+mod shapes;
+
+use crate::easy::{grading_students::grading_students, two_sum::two_sum};
+
 fn main() {
+    // println!("\n\n a value here{:?}", two_sum(Vec::from([2, 7, 11, 15]), 9));
+    println!("\n\n a value here{:?}", two_sum(Vec::from([3, 2, 4]), 6));
+    println!("{:?}", grading_students(vec![73, 67, 38, 33]));
     println!("Hello, world!");
 }
