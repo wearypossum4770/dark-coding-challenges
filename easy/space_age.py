e1dd6a1228d665bd21262e948deeaa0afc506451
Dev Steve <wearypossum4770@yahoo.com>
2024-02-20 10:08:33 -0500

    (chore): update

--- easy/space_age.py
+++ easy/space_age.py
@@ -1,13 +1,14 @@
 def orbital_periods(planet):
     return {
-    "mercury":  0.2408467,
-    "venus":  0.61519726,
-    "mars":  1.8808158,
-    "jupiter":  11.862615,
-    "saturn":  29.447498,
-    "uranus":  84.016846,
-    "neptune":  164.79132,
+        "mercury": 0.2408467,
+        "venus": 0.61519726,
+        "mars": 1.8808158,
+        "jupiter": 11.862615,
+        "saturn": 29.447498,
+        "uranus": 84.016846,
+        "neptune": 164.79132,
     }.get(planet, 1)
+
 
 def space_age(planet, seconds):
     """
@@ -16,4 +17,3 @@ def space_age(planet, seconds):
     earth_years = 31_557_600
     periods = orbital_periods(planet=planet)
     return seconds / (earth_years * periods)
-
--- easy/test_space_age.py
+++ easy/test_space_age.py
@@ -3,16 +3,18 @@ from pytest import approx
 from easy.space_age import space_age
 
 
-@pytest.mark.parametrize("planet, seconds,expected", [
-("earth", 1000000000, 31.69),
-("mercury", 2134835688, 280.88),
-("venus", 189839836, 9.78),
-("mars", 2129871239, 35.88),
-("jupiter", 901876382, 2.41),
-("saturn", 2000000000, 2.15),
-("uranus", 1210123456, 0.46),
-("neptune", 1821023456, 0.35),
-
-])
+@pytest.mark.parametrize(
+    "planet, seconds,expected",
+    [
+        ("earth", 1000000000, 31.69),
+        ("mercury", 2134835688, 280.88),
+        ("venus", 189839836, 9.78),
+        ("mars", 2129871239, 35.88),
+        ("jupiter", 901876382, 2.41),
+        ("saturn", 2000000000, 2.15),
+        ("uranus", 1210123456, 0.46),
+        ("neptune", 1821023456, 0.35),
+    ],
+)
 def test_space_age(planet, seconds, expected):
     assert approx(space_age(planet, seconds), rel=1e-2) == expected
