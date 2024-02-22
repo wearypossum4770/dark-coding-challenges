931a26c5eacbe5c10ad34c3d870d5b8c6beb5ba9
Dev Steve <wearypossum4770@yahoo.com>
2024-02-21 21:55:34 -0500

    (feat): Leet Code # 66

---
+++ easy/plus_one.py
@@ -0,0 +1,17 @@
+from array import array
+def plus_one(digits):
+    """
+    DOCUMENTATION
+    Runtime 31 ms Beats 90.55% of users with Python3
+    Memory 16.72 MB Beats 46.56% of users with Python3
+    """
+    carry = 1
+    numbers = array('l', [0] * len(digits))
+    for (index, digit) in enumerate(reversed(digits)):
+        value = digit + carry
+        numbers[index] = value %10
+        carry = value // 10
+    if carry:
+        numbers.append(carry)
+    numbers.reverse()
+    return numbers.tolist()
---
+++ easy/test_plus_one.py
@@ -0,0 +1,15 @@
+import pytest
+from easy.plus_one import plus_one
+
+
+@pytest.mark.parametrize("num,expected", [
+([1, 2, 3], [1, 2, 4]),
+([4, 3, 2, 1], [4, 3, 2, 2]),
+([9], [1, 0]),
+([0], [1]),
+([1, 8], [1, 9]),
+([1, 9], [2, 0]),
+([9, 9], [1, 0, 0]),
+])
+def test_plus_one(num, expected):
+    assert plus_one(num) == expected
