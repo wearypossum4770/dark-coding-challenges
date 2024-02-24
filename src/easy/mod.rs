b37a75a4e0271e9ec819a826b5f0af7c5b34455d
Dev Steve <wearypossum4770@yahoo.com>
2024-02-24 00:24:20 -0500

    add math functions

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,6 +1,7 @@
 // pub mod roman_to_int;
 pub mod grading_students;
 // pub mod longest_common_prefix;
+pub mod dart_score;
 pub mod division;
 pub mod find_digits;
 pub mod is_leap_year;
@@ -11,3 +12,16 @@ pub mod subtraction;
 pub mod summation;
 pub mod two_sum;
 pub mod utopian_tree;
+
+pub fn distance(x: f32, y: f32, a: f32, b: f32) -> f32 {
+    let x_diff: f32 = a - x;
+    let y_diff: f32 = b - y;
+    let squared_x: f32 = x_diff.powi(2);
+    let squared_y: f32 = y_diff.powi(2);
+    let root: f32 = squared_x + squared_y;
+    root.sqrt()
+}
+
+pub fn distance_from_origin(x: f32, y: f32) -> f32 {
+    distance(x, y, 0.0, 0.0)
+}
