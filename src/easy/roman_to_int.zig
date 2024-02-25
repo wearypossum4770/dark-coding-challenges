17ce20492bbba422e7bfaf412ead103c1eda9c86
Dev Steve <wearypossum4770@yahoo.com>
2024-02-24 20:52:44 -0500

    (sync): upload romaninterer

---
+++ src/easy/roman_to_int.zig
@@ -0,0 +1,9 @@
+// const integer = try std.fmt.parseInt(i32, foo, 10);
+
+pub fn roman_to_integer() void {}
+
+test "A string is returned" {
+    const message = "John Doe";
+    const a = greeting(message);
+    try expect(a == "Hello World! John Doe");
+}
