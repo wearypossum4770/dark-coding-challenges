003335592e28597a231cd11934c532f2253dd47b
Dev Steve <wearypossum4770@yahoo.com>
2024-02-25 20:10:45 -0500

    add modules

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -4,15 +4,16 @@ pub mod grading_students;
 pub mod dart_score;
 pub mod division;
 pub mod find_digits;
+pub mod find_pangram;
 pub mod is_leap_year;
 pub mod palindromic_number;
 pub mod plus_one;
+pub mod reverse_string;
 pub mod space_age;
 pub mod subtraction;
 pub mod summation;
 pub mod two_sum;
 pub mod utopian_tree;
-
 pub fn distance(x: f32, y: f32, a: f32, b: f32) -> f32 {
     let x_diff: f32 = a - x;
     let y_diff: f32 = b - y;
