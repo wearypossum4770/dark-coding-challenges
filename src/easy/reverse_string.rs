4d71e53da215efed5853247792b921516fdf1d26
Dev Steve <wearypossum4770@yahoo.com>
2024-02-25 20:05:17 -0500

    (feat): Exercism Rever String rust

--- src/easy/reverse_string.rs
+++ src/easy/reverse_string.rs
@@ -1,12 +1,40 @@
-pub fn reverse_string() -> bool {
-//
-
+pub fn reverse_string(sentence: &str) -> String {
+    sentence.chars().rev().collect::<String>()
 }
 #[cfg(test)]
 mod tests {
-use super::reverse_string;
-#[test]
-fn test_reverse_string() {
-assert_eq!(reverse_string(parameters), returnvalue);
-}
+    use super::reverse_string;
+    #[test]
+    fn an_empty_string() {
+        assert_eq!(reverse_string(&""), "".to_string());
+    }
+    #[test]
+    fn a_word() {
+        assert_eq!(reverse_string(&"robot"), "tobor".to_string());
+    }
+    #[test]
+    fn a_capitalized_word() {
+        assert_eq!(reverse_string(&"Ramen"), "nemaR".to_string());
+    }
+    #[test]
+    fn a_sentence_with_punctuation() {
+        assert_eq!(reverse_string(&"I'm hungry!"), "!yrgnuh m'I".to_string());
+    }
+    #[test]
+    fn a_palindrome() {
+        assert_eq!(reverse_string(&"racecar"), "racecar".to_string());
+    }
+    #[test]
+    fn an_even_sized_word() {
+        assert_eq!(reverse_string(&"drawer"), "reward".to_string());
+    }
+    #[test]
+    fn wide_characters() {
+        assert_eq!(reverse_string(&"子猫"), "猫子".to_string());
+    }
+    #[test]
+    #[cfg(feature = "grapheme")]
+    fn grapheme_clusters() {
+        assert_eq!(reverse_string(&"uüu"), "uüu".to_string());
+    }
 }
