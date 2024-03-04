015cf00a385858b0be0850b4664f9501530e3fbe
Dev Steve <wearypossum4770@yahoo.com>
2024-03-04 15:41:08 -0500

    (feat): setup shapes modules

---
+++ src/shapes/circle.rs
@@ -0,0 +1,6 @@
+const PI: f64 = std::f64::consts::FRAC_1_PI;
+
+// Function to calculate the area of a circle.
+pub fn calculate_area(radius: f64) -> f64 {
+    PI * radius * radius
+}
---
+++ src/shapes/rectangle.rs
@@ -0,0 +1,3 @@
+pub fn calculate_area(length: f64, width: f64) -> f64 {
+    length * width
+}
