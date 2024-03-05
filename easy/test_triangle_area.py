95b7ea82f93cea0646805a932918b756f2f5fa95
Dev Steve <wearypossum4770@yahoo.com>
2024-03-05 09:57:49 -0500

    (feat): triangle area python

--- easy/test_triangle_area.py
+++ easy/test_triangle_area.py
@@ -1,12 +1,16 @@
-
 import pytest
 from easy.triangle_area import triangle_area
-@pytest.mark.parametrize("base, height, expected",[
-(3, 2, 3),
-(5, 4, 10),
-(10, 10, 50),
-(0, 60, 0),
-(12, 11, 66),
-])
-def test_triangle_area(base, height,  expected):
-	assert triangle_area(base, height) == expected
+
+
+@pytest.mark.parametrize(
+    "base, height, expected",
+    [
+        (3, 2, 3),
+        (5, 4, 10),
+        (10, 10, 50),
+        (0, 60, 0),
+        (12, 11, 66),
+    ],
+)
+def test_triangle_area(base, height, expected):
+    assert triangle_area(base, height) == expected
--- easy/triangle_area.py
+++ easy/triangle_area.py
@@ -1,6 +1,5 @@
-
 def triangle_area(base, height):
-	"""
-	DOCUMENTATION
-	"""
-	return (base * height)/ 2
+    """
+    DOCUMENTATION
+    """
+    return (base * height) / 2
