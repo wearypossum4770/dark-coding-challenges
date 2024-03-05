aa4884cddb6b5063faa2d16a38b7019b07026e4a
Dev Steve <wearypossum4770@yahoo.com>
2024-03-05 09:58:32 -0500

    (feat): triangle area js

--- src/easy/triangle-area.ts
+++ src/easy/triangle-area.ts
@@ -1,1 +1,2 @@
-export const triangleArea = (base:number, height: number): number =>( base * height)/ 2
+export const triangleArea = (base: number, height: number): number =>
+    (base * height) / 2;
