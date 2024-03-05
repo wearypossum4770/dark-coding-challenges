97658571af2a8dae76371bd849342f87b9146264
Dev Steve <wearypossum4770@yahoo.com>
2024-03-04 21:43:59 -0500

    (feat): edabit next int rust

--- src/easy/next_integer.rs
+++ src/easy/next_integer.rs
@@ -1,16 +1,11 @@
-pub fn next_integer(num: i32) -> i32 {
-num + 1
-
+pub fn next_integer() -> bool {
+    //
 }
 #[cfg(test)]
 mod tests {
-use super::next_integer;
-#[test]
-fn test_next_integer() {
-assert_eq!(next_integer(2,), 3, );
-assert_eq!(next_integer(-9, ),-8, );
-assert_eq!(next_integer(0,), 1, );
-assert_eq!(next_integer(999,), 1000, );
-assert_eq!(next_integer(73,), 74, );
-}
+    use super::next_integer;
+    #[test]
+    fn test_next_integer() {
+        assert_eq!(next_integer(parameters), returnvalue);
+    }
 }
