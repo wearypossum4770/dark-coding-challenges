e381db7fbcd1708535fdcc73f6e263425960e51d
Dev Steve <wearypossum4770@yahoo.com>
2024-03-05 23:09:45 -0500

    (feat): edabit cubed number rust

---
+++ src/easy/cubed_number.rs
@@ -0,0 +1,12 @@
+pub fn cubed_number() -> bool {
+//
+
+}
+#[cfg(test)]
+mod tests {
+use super::cubed_number;
+#[test]
+fn test_cubed_number() {
+assert_eq!(cubed_number(parameters), returnvalue);
+}
+}
