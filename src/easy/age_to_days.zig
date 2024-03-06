728dd3c73c35ebb04195489d3a99fe82c537660d
Dev Steve <wearypossum4770@yahoo.com>
2024-03-05 23:00:06 -0500

    (feat): edabit age to days zig

---
+++ src/easy/age_to_days.zig
@@ -0,0 +1,11 @@
+const expect = @import("std").testing.expect;
+
+pub fn ageToDays(age: u16) u16 {
+	return age * 365;
+}
+test "age to days" {
+try expect(ageToDays(10) == 3650);
+try expect(ageToDays(0) == 0);
+try expect(ageToDays(73) == 26645);
+
+}
