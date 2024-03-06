fdeec8b53385db4304319b1b645bbea5117f9dac
Dev Steve <wearypossum4770@yahoo.com>
2024-03-06 08:27:41 -0500

    (test): edabit circut power python

--- easy/test_circuit_power.py
+++ easy/test_circuit_power.py
@@ -2,6 +2,11 @@ import pytest
 from easy.circuit_power import circuit_power
 
 
-@pytest.mark.parametrize("num,expected", [])
-def test_circuit_power(num, expected):
-    assert circuit_power(num) == expected
+@pytest.mark.parametrize("voltage, current,expected", [
+        (110, 3, 330),
+        (230, 10, 2300),
+        (480, 20, 9600),
+
+])
+def test_circuit_power(voltage, current, expected):
+    assert circuit_power(voltage, current) == expected
