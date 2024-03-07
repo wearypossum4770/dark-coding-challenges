3e73f7067962f035ec9240ce81eab7998c5e0428
Dev Steve <wearypossum4770@yahoo.com>
2024-03-06 23:21:06 -0500

    (feat): less than or equal to zero zig

--- src/easy/less_than_or_equal_to_zero.zig
+++ src/easy/less_than_or_equal_to_zero.zig
@@ -4,8 +4,7 @@ pub fn lessThanOrEqualToZero(number: i32) bool {
     return number <= 0;
 }
 test "integer subtraction" {
-try expectEqual(lessThanOrEqualToZero(5), false);
-try expectEqual(lessThanOrEqualToZero(0), true);
-try expectEqual(lessThanOrEqualToZero(-5), true);
-
+    try expectEqual(lessThanOrEqualToZero(5), false);
+    try expectEqual(lessThanOrEqualToZero(0), true);
+    try expectEqual(lessThanOrEqualToZero(-5), true);
 }
