8114b6014f58e4c787c0d69b74141d20f9e0008b
Dev Steve <wearypossum4770@yahoo.com>
2024-03-08 18:17:21 -0500

    add test cases

--- easy/test_farm_animals.py
+++ easy/test_farm_animals.py
@@ -2,6 +2,14 @@ import pytest
 from easy.farm_animals import farm_animals
 
 
-@pytest.mark.parametrize("num,expected", [])
-def test_farm_animals(num, expected):
-    assert farm_animals(num) == expected
+@pytest.mark.parametrize(
+    "chickens, cows, pigs,expected",
+    [
+        (5, 2, 8, 50),
+        (3, 4, 7, 50),
+        (1, 2, 3, 22),
+        (3, 5, 2, 34),
+    ],
+)
+def test_farm_animals(chickens, cows, pigs, expected):
+    assert farm_animals(chickens, cows, pigs) == expected
