5a8f2b44a71d1dfb60998823b62e7275c5489bf2
Dev Steve <wearypossum4770@yahoo.com>
2024-03-08 22:18:57 -0500

    (feat): edabit frames in a minute rust

---
+++ src/easy/frames_per_second.rs
@@ -0,0 +1,19 @@
+pub fn frames_per_second(minutes: i32, fps: i32) -> i32 {
+    fps * (minutes * 60)
+
+}
+#[cfg(test)]
+mod tests {
+use super::frames_per_second;
+#[test]
+fn test_frames_per_second() {
+assert_eq!(frames_per_second(1, 1), 60);
+assert_eq!(frames_per_second(10, 1), 600);
+assert_eq!(frames_per_second(10, 25), 15_000);
+assert_eq!(frames_per_second(500, 60), 1_800_000);
+assert_eq!(frames_per_second(0, 60), 0);
+assert_eq!(frames_per_second(99, 1), 5_940);
+assert_eq!(frames_per_second(419, 70), 1_759_800);
+assert_eq!(frames_per_second(52, 33), 102_960);
+}
+}
