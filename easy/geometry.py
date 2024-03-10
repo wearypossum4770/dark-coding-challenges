cd8d46af096bbcd52c4c6ff82d4213555957b8bb
Dev Steve <wearypossum4770@yahoo.com>
2024-03-10 12:16:44 -0400

    format

--- easy/geometry.py
+++ easy/geometry.py
@@ -1,5 +1,4 @@
-
 def rectangle_area(length, width):
-	if length > 0 and width > 0:
-		return length * width
-	return -1
+    if length > 0 and width > 0:
+        return length * width
+    return -1
--- easy/test_geometry.py
+++ easy/test_geometry.py
@@ -1,17 +1,21 @@
-
 import pytest
 from easy.geometry import rectangle_area
-@pytest.mark.parametrize("length, width,expected",[
-(5, 3, 15),
-(8, 5, 40),
-(5, 4, 20),
-(2, 3, 6),
-(10_000, 10_000, 100_000_000),
-(-2, -5, -1),
-(0, 3, -1),
-(5, -3, -1),
-(0, 1, -1),
-(-1, 0, -1),
-])
+
+
+@pytest.mark.parametrize(
+    "length, width,expected",
+    [
+        (5, 3, 15),
+        (8, 5, 40),
+        (5, 4, 20),
+        (2, 3, 6),
+        (10_000, 10_000, 100_000_000),
+        (-2, -5, -1),
+        (0, 3, -1),
+        (5, -3, -1),
+        (0, 1, -1),
+        (-1, 0, -1),
+    ],
+)
 def test_rectangle_area(length, width, expected):
-	assert rectangle_area(length, width) == expected
+    assert rectangle_area(length, width) == expected
