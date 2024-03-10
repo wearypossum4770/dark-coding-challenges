cd5ef440cad30977f7f84e03761da426900e0b13
Dev Steve <wearypossum4770@yahoo.com>
2024-03-10 00:56:02 -0500

    (feat): edabit compare string lengths python

---
+++ easy/compare_string_length.py
@@ -0,0 +1,6 @@
+
+def compare_string_length(a, b):
+	"""
+	DOCUMENTATION
+	"""
+	return len(a) == len(b)
---
+++ easy/test_compare_string_length.py
@@ -0,0 +1,14 @@
+
+import pytest
+from easy.compare_string_length import compare_string_length
+@pytest.mark.parametrize("a, b,expected",[
+("AB", "CD", True),
+("ABC", "DE", False),
+("hello", "edabit", False),
+("meow", "woof", True),
+("jrnvjrnnt", "cvjknfjvmfvnfjn", False),
+("jkvnjrt", "krnf", False),
+("Facebook", "Snapchat", True),
+])
+def test_compare_string_length(a, b, expected):
+	assert compare_string_length(a, b) == expected
