bcd831d18bbb2f86f68c947fedbb7c501e302db0
Dev Steve <wearypossum4770@yahoo.com>
2024-03-10 12:16:59 -0400

    fix failing test

--- src/easy/remainder.ts
+++ src/easy/remainder.ts
@@ -1,8 +1,13 @@
-export const remainder = (numerator: number, denominator: number): number => numerator % denominator;
+export const remainder = (numerator: number, denominator: number): number =>
+    numerator % denominator;
 
-export const divisibleByFive = (numerator: number): boolean => dividesEvenly(numerator,5)
+export const divisibleByFive = (numerator: number): boolean =>
+    dividesEvenly(numerator, 5);
 
-export const divisibleBy100 = (numerator: number): boolean => dividesEvenly(numerator, 100);
+export const divisibleBy100 = (numerator: number): boolean =>
+    dividesEvenly(numerator, 100);
 
-export const dividesEvenly = (numerator: number, denominator: number): boolean =>
-    remainder(numerator, denominator) === 0;
+export const dividesEvenly = (
+    numerator: number,
+    denominator: number,
+): boolean => remainder(numerator, denominator) === 0;
