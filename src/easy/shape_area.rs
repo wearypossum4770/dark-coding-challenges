a4ba25447ce470a102e5eef9bbb2d68e87edbbba
Dev Steve <wearypossum4770@yahoo.com>
2024-03-17 11:29:21 -0400

    (feat): edabit shape area rust

---
+++ src/easy/shape_area.rs
@@ -0,0 +1,11 @@
+pub fn shape_area() -> bool {
+    //
+}
+#[cfg(test)]
+mod tests {
+    use super::shape_area;
+    #[test]
+    fn test_shape_area() {
+        assert_eq!(shape_area(parameters), returnvalue);
+    }
+}
