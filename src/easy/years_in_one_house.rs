d950711f18469ec90e47cb752010aac08b93c79e
Dev Steve <wearypossum4770@yahoo.com>
2024-03-17 15:13:31 -0400

    complete challenge in rust

--- src/easy/years_in_one_house.rs
+++ src/easy/years_in_one_house.rs
@@ -1,12 +1,14 @@
-pub fn years_in_one_house() -> bool {
-    //
+use std::ops::Div;
+
+pub fn years_in_one_house(age: u32, moves: u32) -> u32 {
+    let average = age as f32 / (moves as f32 + 1.0);
+    average.round() as u32
 }
 #[cfg(test)]
 mod tests {
     use super::years_in_one_house;
     #[test]
     fn test_years_in_one_house() {
-        assert_eq!(years_in_one_house(parameters), returnvalue);
         assert_eq!(years_in_one_house(30, 1), 15);
         assert_eq!(years_in_one_house(15, 2), 5);
         assert_eq!(years_in_one_house(80, 0), 80);
