cf325ebeb4b76929231d8bbdeac40ccb14218431
Dev Steve <wearypossum4770@yahoo.com>
2025-01-31 22:21:03 -0500

    fix(py): revert to correct d905b3e implementation.
    related-to #1

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -4,11 +4,13 @@ Runtime: 0 ms Beats 100.00%
 Memory: 19.04 MB Beats 16.67% after refactor 18.90 MB Beats 34.34%
 """
 
+
 def two_sum(nums: [int], target: int) -> (int, int):
     mapper = {}
-    for index in range(len(nums)):
-        diff = target - num
-        if diff in mapper:
-            return (diff, index)
+    for index, num in enumerate(nums):
+        val = mapper.get(target - num)
+        if val is not None:
+            return (val, index)
         mapper[num] = index
+
     return None
