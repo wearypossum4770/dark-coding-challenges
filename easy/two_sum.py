2ef157d475b610968cf0f753e6566823bbe5ebef
Dev Steve <wearypossum4770@yahoo.com>
2025-01-31 22:13:15 -0500

    docs(py): include docs for commit d905b3e.
    related-to #1

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,3 +1,9 @@
+"""
+[Leet Code](https://leetcode.com/problems/two-sum/)
+Runtime: 0 ms Beats 100.00%
+Memory: 19.04 MB Beats 16.67% after refactor 18.90 MB Beats 34.34%
+"""
+
 def two_sum(nums: [int], target: int) -> (int, int):
     mapper = {}
     for index in range(len(nums)):
