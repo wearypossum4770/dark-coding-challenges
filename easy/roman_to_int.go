0d593d63b5368f469b32e9c3cd0f1f6b25af1099
Dev Steve <wearypossum4770@yahoo.com>
2025-02-03 01:33:49 -0500

    fix(refactor): remove unused import for 9ab446b
    relates-to: #13

--- easy/roman_to_int.go
+++ easy/roman_to_int.go
@@ -1,15 +1,12 @@
 package easy
 
-import (
-	"fmt"
-)
-
 func getValueWithDefault(m map[string]int, key string, defaultValue int) int {
 	if value, ok := m[key]; ok {
 		return value
 	}
 	return defaultValue
 }
+
 func RomanToInt(romanNumber string) int {
 	romanNumerals := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 	total := 0
