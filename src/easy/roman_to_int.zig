02d3ef4a0bc5a6be558a1af8b3a7d5cf61b7dc49
Dev Steve <wearypossum4770@yahoo.com>
2025-02-03 00:14:22 -0500

    chore(refactor): create module for roman hashmap func for 428b909
    relates-to #13

--- src/easy/roman_to_int.zig
+++ src/easy/roman_to_int.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
-const ProjectRoot = @import("root");
-const AutoHashMap = std.hash_map.AutoHashMap;
-const Allocator = std.mem.Allocator;
+const utilities = @import("./utilities.zig");
+const expectEqual = std.testing.expectEqual;
 
 pub fn romanToInt(romanNumber: []const u8) i32 {
-    var romanNumerals = ProjectRoot.createRomanNumerals(i32, std.heap.page_allocator);
+    var romanNumerals = utilities.createRomanNumerals(i32, std.heap.page_allocator);
     var total: i32 = 0;
     var index: usize = 0;
     for (romanNumber) |numeral| {
