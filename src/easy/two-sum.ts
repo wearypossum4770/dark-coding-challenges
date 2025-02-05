d0d9bc2fec34bce08a0519826da830c117040e07
Dev Steve <wearypossum4770@yahoo.com>
2025-02-05 18:14:42 -0500

    lint(ts): format and lint 0ae0182 via prettier
    relates-to #13

--- src/easy/two-sum.ts
+++ src/easy/two-sum.ts
@@ -16,13 +16,13 @@
  * - Example:  [0, 1]
  */
 export const twoSum = (nums: number[], target: number): number[] => {
-  const mapper = new Map<number, number>();
-  for (let i = 0; i < nums.length; i++) {
-    const complement = mapper.get(target - nums[i]);
-    if (complement) {
-      return [complement, i];
+    const mapper = new Map<number, number>();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = mapper.get(target - nums[i]);
+        if (complement) {
+            return [complement, i];
+        }
+        mapper.set(nums[i], i);
     }
-    mapper.set(nums[i], i);
-  }
-  return [];
+    return [];
 };
