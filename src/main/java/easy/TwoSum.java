1fec20a6a458e99342b9febe68a47d33201d3204
Dev Steve <wearypossum4770@yahoo.com>
2025-02-05 18:15:55 -0500

    lint(java): format and lint 6538e81 via prettier
    relates-to #13

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -4,19 +4,19 @@ import java.util.HashMap;
 
 public class TwoSum {
 
-  public static int[] twoSum(int[] nums, int target) {
-    HashMap<Integer, Integer> seen = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-      int complement = target - nums[i];
-      if (seen.containsKey(complement)) {
-        return new int[] {seen.get(complement), i};
-      }
-      seen.put(nums[i], i);
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[] { seen.get(complement), i };
+            }
+            seen.put(nums[i], i);
+        }
+        return new int[] {};
     }
-    return new int[] {};
-  }
 
-  public int[] solve(int[] nums, int target) {
-    return twoSum(nums, target);
-  }
+    public int[] solve(int[] nums, int target) {
+        return twoSum(nums, target);
+    }
 }
