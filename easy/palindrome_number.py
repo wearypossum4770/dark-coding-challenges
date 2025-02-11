e43c686b4cde0807c36188946b2fda222060831c
Dev Steve <wearypossum4770@yahoo.com>
2025-02-10 23:15:36 -0500

    feat(py): implement palindrome number challenge.
    <h2>Summary</h2>
    | Challenge Title                   | Challenge Provider                                            |
    | --------------------------------- | ------------------------------------------------------------- |
    | Palindrome Number                 | [Leet Code](https://leetcode.com/problems/palindrome-number/) |
    | Check if a Number is A Palindrome | [Edabit](https://edabit.com/challenge/6dbpFcrW4schC6zDP)      |
    
    <h2>Metrics</h2>
    | Resource  | Value     | Stats   |
    | --------- | --------- | ------  |
    | Runtime   |  7ms      | 63.61%  |
    | Memory    | 17.93MB   | 18.41%  |
    
    <h2>Description</h2>
    The Palindrome Number challenge asks you to determine whether a given integer is a palindrome. A palindrome is a number that reads the same backward as forward.
    
    Given an integer `x`, return `true` if `x` is a palindrome, and `false` otherwise.
    
    <h2>Constraints</h2>
    <ul>
    <li>The integer can be negative or positive.</li>
    <li>The integer can have any number of digits.</li>
    <li>Number representation does not allow leading zeros, they are insignificant.</li>
    </ul>
    <h2>Strategy</h2>
    - Iterative approach
    - Two pointer technique
    
    <h2>Changes Made</h2>
    Developed a function to determine if a number is palindromic:
    - Reject negative numbers due to their sign.
    - Zero and single digit numbers are is a palindromes.
    - Numbers divisible by 10 are non-palindromic.
    - Convert the number to a string.
    - Use two pointers to index the string.
    - Loop until left pointer != right pointer.
    - Early return if left pointer does not equal right pointer.
    - Consumer iterator, return `true` because all characters are the same.
    
    <h2>Conclusion</h2>
    This solution addresses the requirements of the
    'RPalindrome Number' problem.
    
    related-to #9

---
+++ easy/palindrome_number.py
@@ -0,0 +1,10 @@
+def palindrome_number(x: int) -> bool:
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+    number = str(x)
+    
+    right = len(number) - 1
+    for left in range(right):
+        if number[left] != number[right - left]:
+            return False
+    return True
