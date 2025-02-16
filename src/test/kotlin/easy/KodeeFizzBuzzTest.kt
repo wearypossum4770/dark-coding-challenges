85627ae9b2a4ae68c5a2d20df00aa31b33bdc9eb
Dev Steve <wearypossum4770@yahoo.com>
2025-02-16 01:38:15 -0500

    test(kt): include unit test for challenge 3e45368
    relates-to: #412

---
+++ src/test/kotlin/easy/KodeeFizzBuzzTest.kt
@@ -0,0 +1,98 @@
+package easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class KodeeFizzBuzzTest {
+    @ParameterizedTest
+    @MethodSource("fixtureMethod")
+    fun testFizzBuzz(
+        candidate: Int,
+        expected: List<String>,
+    ) {
+        val result = kodeeFizzBuzz(candidate)
+        assertEquals(expected, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun fixtureMethod(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(
+                    5,
+                    // spotless:off
+                    listOf("1", "2", "Fizz", "4", "Buzz"),
+                    // spotless:on
+                ),
+                Arguments.of(
+                    3,
+                    // spotless:off
+                    listOf("1", "2", "Fizz"),
+                    // spotless:on
+                ),
+                Arguments.of(
+                    10,
+                    // spotless:off
+                    listOf("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"),
+                    // spotless:on
+                ),
+                Arguments.of(
+                    16,
+                    // spotless:off
+                    listOf("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16"),
+                    // spotless:on
+                ),
+                Arguments.of(
+                    15,
+                    // spotless:off
+                    listOf("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"),
+                    // spotless:on
+                ),
+                Arguments.of(
+                    35,
+                    // spotless:off
+                    listOf(
+                        "1",
+                        "2",
+                        "Fizz",
+                        "4",
+                        "Buzz",
+                        "Fizz",
+                        "7",
+                        "8",
+                        "Fizz",
+                        "Buzz",
+                        "11",
+                        "Fizz",
+                        "13",
+                        "14",
+                        "FizzBuzz",
+                        "16",
+                        "17",
+                        "Fizz",
+                        "19",
+                        "Buzz",
+                        "Fizz",
+                        "22",
+                        "23",
+                        "Fizz",
+                        "Buzz",
+                        "26",
+                        "Fizz",
+                        "28",
+                        "29",
+                        "FizzBuzz",
+                        "31",
+                        "32",
+                        "Fizz",
+                        "34",
+                        "Buzz",
+                    ),
+                    // spotless:on
+                ),
+            )
+    }
+}
