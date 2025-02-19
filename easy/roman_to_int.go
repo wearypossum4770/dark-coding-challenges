cf2fcd51943f5a43af5d15691295e6356a114f14
Dev Steve <wearypossum4770@yahoo.com>
2025-02-19 17:55:48 -0500

    refactor(go): change data types to bytes for 8c439a3
    relates-to: #13

--- easy/roman_to_int.go
+++ easy/roman_to_int.go
@@ -1,19 +1,19 @@
 package easy
 
-func getValueWithDefault(m map[string]int, key string, defaultValue int) int {
-	if value, ok := m[key]; ok {
+func getValueWithDefault(m map[byte]int, k byte, v int) int {
+	if value, ok := m[k]; ok {
 		return value
 	}
-	return defaultValue
+	return v
 }
 
 func RomanToInt(romanNumber string) int {
-	romanNumerals := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+	romanNumerals := map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 	total := 0
 	index := 0
 	for index < len(romanNumber)-1 {
-		next := getValueWithDefault(romanNumerals, string(romanNumber[index+1]), 0)
-		current := getValueWithDefault(romanNumerals, string(romanNumber[index]), 0)
+		next := getValueWithDefault(romanNumerals, romanNumber[index+1], 0)
+		current := getValueWithDefault(romanNumerals, romanNumber[index], 0)
 		if current < next {
 			total -= current
 		} else {
@@ -21,5 +21,5 @@ func RomanToInt(romanNumber string) int {
 		}
 		index += 1
 	}
-	return total + getValueWithDefault(romanNumerals, string(romanNumber[index]), 0)
+	return total + getValueWithDefault(romanNumerals, romanNumber[index], 0)
 }
