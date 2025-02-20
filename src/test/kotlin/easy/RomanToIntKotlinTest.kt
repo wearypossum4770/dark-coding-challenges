cf0e1e1970ec66ae61d468af96e4377dac00351d
Dev Steve <wearypossum4770@yahoo.com>
2025-02-19 23:35:46 -0500

    test(kt): include unit test for challenge 7f56ed2
    relates-to: #13

---
+++ src/test/kotlin/easy/RomanToIntKotlinTest.kt
@@ -0,0 +1,36 @@
+package easy
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class RomanToIntKotlinTest {
+    @ParameterizedTest
+    @CsvSource(
+        "'IV', 4",
+        "'XC', 90",
+        "'CM', 900",
+        "'MMM', 3_000",
+        "'III', 3",
+        "'XXVII', 27",
+        "'LVIII', 58",
+        "'MCMXCIV', 1_994",
+        "'MMMCMXCIV', 3_994",
+        "'I', 1",
+        "'VIII', 8",
+        "'XXIX', 29",
+        "'LIV', 54",
+        "'CCV', 205",
+        "'CDXLIV', 444",
+        "'CMXCIX', 999",
+        "'M', 1_000",
+        "'MMMDCCCLXXXVIII', 3_888",
+        "'MMMCMX', 3_910",
+    )
+    fun testRomanToInt(
+        roman: String,
+        arabic: Int,
+    ) {
+        val result = romanToKotlinInt(roman)
+        assertEquals(arabic, result)
+    }
+}
