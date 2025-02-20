7efbffc121a8b67ca41ce907475212d68142cdd7
Dev Steve <wearypossum4770@yahoo.com>
2025-02-20 12:02:11 -0500

    test(rs): includes unit test for challenge 8dff172
    relates-to: #9

--- src/easy/palindrome_number.rs
+++ src/easy/palindrome_number.rs
@@ -12,3 +12,53 @@ pub fn palindrome_number(x: i32) -> bool {
     }
     true
 }
+
+#[cfg(test)]
+mod tests {
+    use super::palindrome_number;
+
+    #[test]
+    fn test_is_neg_121_a_palindrome() {
+        assert_eq!(false, palindrome_number(-121));
+    }
+    #[test]
+    fn test_is_121_a_palindrome() {
+        assert_eq!(true, palindrome_number(121));
+    }
+    #[test]
+    fn test_is_10_a_palindrome() {
+        assert_eq!(false, palindrome_number(10));
+    }
+    #[test]
+    fn test_is_neg_101_a_palindrome() {
+        assert_eq!(false, palindrome_number(-101));
+    }
+    #[test]
+    fn test_is_neg_10_a_palindrome() {
+        assert_eq!(false, palindrome_number(-10));
+    }
+    #[test]
+    fn test_is_neg_1_a_palindrome() {
+        assert_eq!(false, palindrome_number(-1));
+    }
+    #[test]
+    fn test_is_0_a_palindrome() {
+        assert_eq!(true, palindrome_number(0));
+    }
+    #[test]
+    fn test_is_3_a_palindrome() {
+        assert_eq!(true, palindrome_number(3));
+    }
+    #[test]
+    fn test_is_8_a_palindrome() {
+        assert_eq!(true, palindrome_number(8));
+    }
+    #[test]
+    fn test_is_9_a_palindrome() {
+        assert_eq!(true, palindrome_number(9));
+    }
+    #[test]
+    fn test_is_11_a_palindrome() {
+        assert_eq!(true, palindrome_number(11));
+    }
+}
