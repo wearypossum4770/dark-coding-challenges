70afeffb3db585054758c0df74b81f34396a10cc
Dev Steve <wearypossum4770@yahoo.com>
2025-02-20 11:44:54 -0500

    feat(py): implement palindrome number challenge solution.
     # Current Challenge
    | Challenge Title                          | Challenge Provider                                               |
    | ---------------------------------------- | ---------------------------------------------------------------- |
    | Palindrome Number                        | [Leet Code](https://leetcode.com/problems/palindrome-number/)     |
    | Check if a Number is a Palindrome        | [Edabit](https://edabit.com/challenge/6dbpFcrW4schC6zDP)         |
    
     # Metrics
    | Resource  | Value     | Stats    |
    | --------- | --------- | -------  |
    | Runtime   |  7 ms     | 63.60 %  |
    | Memory    | 17.93 MB  | 19.48 %  |
    
     # Summary
    Given an integer `x`, return true if `x` is a palindrome, and `false` otherwise..
    
     # Description:
    A palindrome is a fascinating concept in language and mathematics. To learn more about what a palindrome is, [click here](#palindrome-definition).
    
    A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization). Some examples of palindromes include:
    An integer is a palindrome when it reads the same forward and backward.
    For example, `121` is a palindrome while `123` is not.
    
     # Strategy
    - Iteration
    - Two Pointer.
    - String Conversion
    
     # Constraints
    - The number is between -2_147_483_648 and 2_147_483_647.
    
     # Changes Made
    Created a method to determine if an integer is a palindrome.
    - Return `FALSE` early if the number is negative (negative sign) or if it is divisible by 10 (no leading zeros) unless it is the number zero itself.
    - Convert the integer into a string.
    - Create a left pointer at the start of the string, and a right pointer to the end of the string.
    - Loop until index of left pointer is greater than or equal to index of right pointer.
    - Return `FALSE` early If value of left pointer is not equal to the value of the right pointer.
    - Increment left pointer index, decrement right pointer index.
    - Exhaust iterations, return true.
    
    relates-to: #9

---
+++ easy/palindrome_number.py
@@ -0,0 +1,9 @@
+def palindrome_number(self, x: int) -> bool:
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+    number = str(x)
+    right = len(number) - 1
+    for left in range(right):
+        if number[left] != number[right - left]:
+            return False
+    return True
