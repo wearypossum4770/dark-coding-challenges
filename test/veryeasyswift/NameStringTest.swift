01a61c9837fac4a91bfb41320537ba8bf660ed3a
Your Name <wearypossum4770@yahoo.com>
2025-02-24 18:15:48 -0500

    test(swift): challenge completed for 7c6a98c

---
+++ test/veryeasyswift/NameStringTest.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ("Mubashir", "MubashirEdabit"),
+  ("Matt", "MattEdabit"),
+  ("Swift", "SwiftEdabit"),
+  ("Airforce", "AirforceEdabit"),
+])
+func nameString(name: String, outcome: String) {
+  #expect(nameString(name) == outcome)
+}
