808c95d5574a3e933b9031506526a46c7813de7c
Your Name <wearypossum4770@yahoo.com>
2025-02-24 18:15:49 -0500

    test(swift): challenge completed for 94db414

---
+++ test/veryeasyswift/NameStringGreetingTest.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ("Gerald", "Hello Gerald!"),
+  ("Fatima", "Hello Fatima!"),
+  ("Ed", "Hello Ed!"),
+  ("Tiffany", "Hello Tiffany!"),
+])
+func nameStringGreeting(name: String, outcome: String) {
+  #expect(nameStringGreeting(name) == outcome)
+}
