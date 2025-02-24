258c3af0c9c4565cc530d617482b22aa29225e3b
Your Name <wearypossum4770@yahoo.com>
2025-02-24 18:15:49 -0500

    test(swift): challenge completed for d07d143

---
+++ test/veryeasyswift/NextIntegerTest.swift
@@ -0,0 +1,15 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+
+  (2, 3),
+  (-9, -8),
+  (0, 1),
+  (999, 1000),
+  (73, 74),
+])
+func nextInteger(value: Int, outcome: Int) {
+  #expect(nextInteger(value) == outcome)
+}
