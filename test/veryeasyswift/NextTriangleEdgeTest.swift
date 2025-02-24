1ef522dba4304f1596bf31b6848a3a58c16e02e0
Your Name <wearypossum4770@yahoo.com>
2025-02-24 18:15:49 -0500

    test(swift): challenge completed for fa56aa4

---
+++ test/veryeasyswift/NextTriangleEdgeTest.swift
@@ -0,0 +1,14 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  (5, 4, 8),
+  (8, 3, 10),
+  (7, 9, 15),
+  (10, 4, 13),
+  (7, 2, 8),
+])
+func nextTriangleEdge(a: Int, b: Int, outcome: Int) {
+  #expect(nextTriangleEdge(a, b) == outcome)
+}
