13e68c1f0669dfa7dcfbc01a0915c191b74e5e4e
Your Name <wearypossum4770@yahoo.com>
2025-02-24 18:15:45 -0500

    test(swift): challenge completed for b41f87c

---
+++ test/veryeasyswift/HowManySecondsTest.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  (2, 7_200),
+  (10, 36_000),
+  (24, 86_400),
+  (36, 129_600),
+])
+func howManySeconds(hours: Int, outcome: Int) {
+  #expect(howManySeconds(hours) == outcome)
+}
