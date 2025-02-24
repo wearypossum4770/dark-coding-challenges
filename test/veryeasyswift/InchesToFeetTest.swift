8eb024d63ff00b86a9692ef27185f052e2da3330
Your Name <wearypossum4770@yahoo.com>
2025-02-24 18:15:46 -0500

    test(swift): challenge completed for 991fcc6

---
+++ test/veryeasyswift/InchesToFeetTest.swift
@@ -0,0 +1,15 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  (12, 1),
+  (360, 30),
+  (3612, 301),
+  (324, 27),
+  (3012, 251),
+  (11, 0),
+])
+func inchesToFeet(inches: Int, outcome: Int) {
+  #expect(inchesToFeet(inches) == outcome)
+}
