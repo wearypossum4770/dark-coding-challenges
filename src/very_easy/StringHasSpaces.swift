ed24d53145aa6e624392a37b829fa05ed3fabe2d
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:08:31 -0500

    fea(swift): challenge completed

---
+++ src/very_easy/StringHasSpaces.swift
@@ -0,0 +1,9 @@
+// [Edabit](https://edabit.com/challenge/ne9MdAStmGtYBtYGt)
+func stringHasSpaces(_ str: String) -> Bool {
+  for char in str {
+    if char == " " {
+      return true
+    }
+  }
+  return false
+}
