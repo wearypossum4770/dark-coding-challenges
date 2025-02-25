3c56b9ff0159fd21a55894aee3636eddf8b9478f
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:09:36 -0500

    chore(lint): lint and format via swift-format

--- src/very_easy/ArraySum.swift
+++ src/very_easy/ArraySum.swift
@@ -1,5 +1,6 @@
 // Memory 1356 KiB
 // Runtime 0.09311 seconds
+// https://edabit.com/challenge/qrmSn8EmGH7EWeh6o
 func arraySum(array: [Int], initial: Int? = 0) -> Int {
   if array.isEmpty {
     return 0
