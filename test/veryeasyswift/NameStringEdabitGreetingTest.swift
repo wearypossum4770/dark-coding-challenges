ba625b5be4dfe1eeb6b0aef4b8df179cabdfcf4f
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:18:55 -0500

    test(swift): complete challenge for 4c518ff

---
+++ test/veryeasyswift/NameStringEdabitGreetingTest.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ("Matt", "Hello, Matt!"),
+  ("Helen", "Hello, Helen!"),
+  ("Mubashir", "Hello, my Love!"),
+
+])
+func nameStringEdabitGreeting(name: String, outcome: String) {
+  #expect(nameStringEdabitGreeting(name) == outcome)
+}
