d346af2394cfdc6521ed18c9cc83700743d4fa15
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:19:01 -0500

    test(swift): complete challenge for 48be571

---
+++ test/veryeasyswift/CombineIntegerArraysTest.swift
@@ -0,0 +1,12 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ([1, 3, 5], [2, 6, 8], [1, 3, 5, 2, 6, 8]),
+  ([7, 8], [10, 9, 1, 1, 2], [7, 8, 10, 9, 1, 1, 2]),
+  ([4, 5, 1], [3, 3, 3, 3, 3], [4, 5, 1, 3, 3, 3, 3, 3]),
+])
+func combineIntegerArrays(a: [Int], b: [Int], outcome: [Int]) {
+  #expect(combineIntegerArrays(a, b) == outcome)
+}
