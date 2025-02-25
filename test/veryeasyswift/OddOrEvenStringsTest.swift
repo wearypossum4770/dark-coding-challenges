a7f4fa71b6e9ee4567ba9f71f1c6aa3e2865487b
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:18:55 -0500

    test(swift): complete challenge for 4ce7b53

---
+++ test/veryeasyswift/OddOrEvenStringsTest.swift
@@ -0,0 +1,15 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ("apples", true),
+  ("banana", true),
+  ("cherry", true),
+  ("mango", false),
+  ("peach", false),
+  ("pears", false),
+])
+func oddOrEvenStrings(text: String, outcome: Bool) {
+  #expect(oddOrEvenStrings(text) == outcome)
+}
