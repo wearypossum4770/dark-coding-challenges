2e428270df550961b1f8babe05e76331d91b0a7e
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:18:53 -0500

    test(swift): complete challenge for ed24d53

---
+++ test/veryeasyswift/StringHasSpacesTest.swift
@@ -0,0 +1,15 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ("Foo", false),
+  ("Foo bar", true),
+  ("Foo ", true),
+  (" Foo", true),
+  (" ", true),
+  (",./;'[]-=", false),
+])
+func stringHasSpaces(text: String, outcome: Bool) {
+  #expect(stringHasSpaces(text) == outcome)
+}
