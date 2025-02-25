dce1704df5c6ac5f83a15a9467c902fcf2041648
Your Name <wearypossum4770@yahoo.com>
2025-02-25 18:18:57 -0500

    test(swift): complete challenge for a0d2246

---
+++ test/veryeasyswift/IsStringEmptyTest.swift
@@ -0,0 +1,15 @@
+import Testing
+
+@testable import very_easy
+
+@Test(arguments: [
+  ("", true),
+  (" ", false),
+  ("            ", false),
+  ("38215", false),
+  ("afjabsdf", false),
+  ("!?@&", false),
+])
+func isStringEmpty(input: String, outcome: Bool) {
+  #expect(isStringEmpty(input) == outcome)
+}
