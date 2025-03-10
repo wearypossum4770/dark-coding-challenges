39c882e6f731cda592c43171bcda543075f23cf0
Your Name <wearypossum4770@yahoo.com>
2025-03-10 16:55:12 -0400

    feat(kt): implement armstrong number challenge solution
     # Current Challenge
    
    | Challenge Title        | Challenge Provider                                                                                |
    | ---------------------- | ------------------------------------------------------------------------------------------------- |
    | Armstrong Number       | [Hacker Earth](https://www.hackerearth.com/problem/algorithm/armstrong-number-12-862cf0ed/)       |
    | Armstrong Number       | [Leet Code](https://leetcode.com/problems/armstrong-number/)                                      |
    
     # Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | ? ms     | ?%       |
    | Memory    | ? MB     | ?%       |
    
     # Summary
    
    An Armstrong number (also known as a Narcissistic number) is a number that is equal to the sum of its digits raised to the power of the number of digits.
    
     # Strategy
    
    - Check if integer is less than zero, negative numbers cannot be Armstrong numbers.
    - Cast integer to string, iterate string cast Char to integer.
    - Store size of integer collection (array).
    - Pass collection to the `sumOf` aggregation function.
    - Use the lambda function to transform the element to double, calculate its power raised to list size, cast to integer.
    - Check equality of the result to the original input.
    - Return 1 (true) if the values are the same 0 (false) if the values are not equal.
    
     # Constraints
    
    - The input number is a positive integer.
    - The number length can range from 1 to 10^6.
    
    relates-to: #1134

---
+++ src/main/kotlin/easy/KodeeArmstrongNumbers.kt
@@ -0,0 +1,11 @@
+package easy
+import kotlin.math.pow
+
+fun kodeeArmstrongNumbers(numberToCheck: Int): Int {
+    if (numberToCheck < 0) return 0
+    val digits = numberToCheck.toString().map { it.toString().toInt() }
+    val power = digits.size
+    val sum = digits.sumOf { it.toDouble().pow(power).toInt() }
+    if (sum == numberToCheck) return 1
+    return 0
+}
