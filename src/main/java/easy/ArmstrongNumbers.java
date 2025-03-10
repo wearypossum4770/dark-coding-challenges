5112c5471736f9fa5dfe1fdb23718f465cb0b5d4
Your Name <wearypossum4770@yahoo.com>
2025-03-10 16:27:06 -0400

    feat(java): implement armstrong number challenge solution
     # Current Challenge
    
    | Challenge Title        | Challenge Provider                                                                                |
    | ---------------------- | ------------------------------------------------------------------------------------------------- |
    | Armstrong Number       | [Hacker Earth](https://www.hackerearth.com/problem/algorithm/armstrong-number-12-862cf0ed/)       |
    | Armstrong Number       | [Leet Code](https://leetcode.com/problems/armstrong-number/)                                      |
    
     # Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | ? ms     | ?%       |
    | Memory    | ? MB     | ?%       |
    
     # Summary
    
    An Armstrong number (also known as a Narcissistic number) is a number that is equal to the sum of its digits raised to the power of the number of digits.
    
     # Strategy
    
    - Check if integer is less than zero, negative integers cannot be Armstrong Numbers.
    - Create total pointer initialized as "zero" and index pointer set to start of the string.
    - Iterate while the index is less than the length of the string.
    - Cast `char` to `int` for calculation raising it to the power of the string length.
    - If the computed sum matches the original number, it is an Armstrong number.
    
     # Constraints
    
    - The input number is a positive integer.
    - The number length can range from 1 to 10^6.
    
    relates-to: #1134

---
+++ src/main/java/easy/ArmstrongNumbers.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class ArmstrongNumbers {
+  public int isArmstrongNumber(int input) {
+    int total = 0;
+    int index = 0;
+    String target = Integer.toString(input);
+    int end = target.length();
+    while (index < end) {
+      int base = target.charAt(index) - '0';
+      total += Math.pow(base, end);
+      index += 1;
+    }
+    return total == input ? 1 : 0;
+  }
+
+  public int solve(int input) {
+    return this.isArmstrongNumber(input);
+  }
+}
