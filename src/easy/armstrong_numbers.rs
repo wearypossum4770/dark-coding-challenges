9899b4428055618f3da88a1defad47ba1899a5b3
Your Name <wearypossum4770@yahoo.com>
2025-03-17 22:45:21 -0400

    test(rs): include test for challenge dfaa7d4
    relates-to: #1134

--- src/easy/armstrong_numbers.rs
+++ src/easy/armstrong_numbers.rs
@@ -12,3 +12,34 @@ pub fn armstrong_numbers(num: i32) -> u8 {
         false => 0,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::armstrong_numbers;
+    use rstest::rstest;
+    #[rstest]
+    #[case(2, 1)]
+    #[case(89, 0)]
+    #[case(0, 1)]
+    #[case(39, 0)]
+    #[case(555, 0)]
+    #[case(9475, 0)]
+    #[case(9926315, 1)]
+    #[case(93084, 1)]
+    #[case(548834, 1)]
+    #[case(2147483647, 0)]
+    #[case(365, 0)]
+    #[case(120, 0)]
+    #[case(1253, 0)]
+    #[case(153, 1)]
+    #[case(-153, 0)]
+    #[case(407, 1)]
+    #[case(370, 1)]
+    #[case(371, 1)]
+    #[case(1634, 1)]
+    #[case(8208, 1)]
+    #[case(9474, 1)]
+    fn test_armstrong_numbers(#[case] num: i32, #[case] expected: u8) {
+        assert_eq!(expected, armstrong_numbers(num));
+    }
+}
