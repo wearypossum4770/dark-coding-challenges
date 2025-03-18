65e7fabda2042ea4d6f26f456027cc8e93ced509
Your Name <wearypossum4770@yahoo.com>
2025-03-18 00:08:36 -0400

    feat(go): implement two sum challenge solution
     # Current Challenge
    
    | Challenge Title  | Challenge Provider                                     |
    | ---------------- | ------------------------------------------------------ |
    | Two Sum          | [Leet Code](https://leetcode.com/problems/two-sum/)    |
    
     # Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 5.97 MB  | 40.94 %  |
    
     # Summary
    
    Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     # Strategy
    
    - Initialize a hashmap/dictionary/table to memoize previously visited values.
    - Iterate through integer array, for each value, calculate the complement.
    - Check hashmap for presence of the complement, immediately return the complement and index of the current iteration.
    - Otherwise store complement in the hashmap
    - Consume iterator, return empty array to ensure type safety.
    
     # Constraints
    
    - Only one valid answer exists.
    - The target is between -10^9^ and 10^9^.
    - Elements in the nums array are between -10^9^ and 10^9^.
    - the array nums is no less than 2 elements and no more than 10^4^.
    - The number length can range from 1 to 10^6^.
    
    relates-to: #1

---
+++ easy/two_sum.go
@@ -0,0 +1,14 @@
+package easy
+
+import ()
+
+func TwoSum(nums []int, target int) []int {
+	seen := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := seen[target-num]; ok {
+			return []int{j, i}
+		}
+		seen[num] = i
+	}
+	return []int{}
+}
