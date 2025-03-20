72477f64f76b86c7d4c0751a2c57e21bdf53481e
Your Name <wearypossum4770@yahoo.com>
2025-03-20 15:09:44 -0400

    refactor(rs/test): change test to be parameterized
    relates-to: #13
    refactors: 2ea7f80

--- src/easy/roman_to_int.rs
+++ src/easy/roman_to_int.rs
@@ -20,80 +20,29 @@ pub fn roman_to_int(roman: String) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::roman_to_int;
-    #[test]
-    fn test_roman_numeral_IV() {
-        assert_eq!(roman_to_int("IV".to_string()), 4);
-    }
-    #[test]
-    fn test_roman_numeral_XC() {
-        assert_eq!(roman_to_int("XC".to_string()), 90);
-    }
-    #[test]
-    fn test_roman_numeral_CM() {
-        assert_eq!(roman_to_int("CM".to_string()), 900);
-    }
-    #[test]
-    fn test_roman_numeral_MMM() {
-        assert_eq!(roman_to_int("MMM".to_string()), 3_000);
-    }
-    #[test]
-    fn test_roman_numeral_III() {
-        assert_eq!(roman_to_int("III".to_string()), 3);
-    }
-    #[test]
-    fn test_roman_numeral_XXVII() {
-        assert_eq!(roman_to_int("XXVII".to_string()), 27);
-    }
-    #[test]
-    fn test_roman_numeral_LVIII() {
-        assert_eq!(roman_to_int("LVIII".to_string()), 58);
-    }
-    #[test]
-    fn test_roman_numeral_MCMXCIV() {
-        assert_eq!(roman_to_int("MCMXCIV".to_string()), 1_994);
-    }
-    #[test]
-    fn test_roman_numeral_MMMCMXCIV() {
-        assert_eq!(roman_to_int("MMMCMXCIV".to_string()), 3_994);
-    }
-    #[test]
-    fn test_roman_numeral_I() {
-        assert_eq!(roman_to_int("I".to_string()), 1);
-    }
-    #[test]
-    fn test_roman_numeral_VIII() {
-        assert_eq!(roman_to_int("VIII".to_string()), 8);
-    }
-    #[test]
-    fn test_roman_numeral_XXIX() {
-        assert_eq!(roman_to_int("XXIX".to_string()), 29);
-    }
-    #[test]
-    fn test_roman_numeral_LIV() {
-        assert_eq!(roman_to_int("LIV".to_string()), 54);
-    }
-    #[test]
-    fn test_roman_numeral_CCV() {
-        assert_eq!(roman_to_int("CCV".to_string()), 205);
-    }
-    #[test]
-    fn test_roman_numeral_CDXLIV() {
-        assert_eq!(roman_to_int("CDXLIV".to_string()), 444);
-    }
-    #[test]
-    fn test_roman_numeral_CMXCIX() {
-        assert_eq!(roman_to_int("CMXCIX".to_string()), 999);
-    }
-    #[test]
-    fn test_roman_numeral_M() {
-        assert_eq!(roman_to_int("M".to_string()), 1_000);
-    }
-    #[test]
-    fn test_roman_numeral_MMMDCCCLXXXVIII() {
-        assert_eq!(roman_to_int("MMMDCCCLXXXVIII".to_string()), 3_888);
-    }
-    #[test]
-    fn test_roman_numeral_MMMCMX() {
-        assert_eq!(roman_to_int("MMMCMX".to_string()), 3_910);
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("IV".to_string(), 4)]
+    #[case("XC".to_string(), 90)]
+    #[case("CM".to_string(), 900)]
+    #[case("MMM".to_string(), 3_000)]
+    #[case("III".to_string(), 3)]
+    #[case("XXVII".to_string(), 27)]
+    #[case("LVIII".to_string(), 58)]
+    #[case("MCMXCIV".to_string(), 1_994)]
+    #[case("MMMCMXCIV".to_string(), 3_994)]
+    #[case("I".to_string(), 1)]
+    #[case("VIII".to_string(), 8)]
+    #[case("XXIX".to_string(), 29)]
+    #[case("LIV".to_string(), 54)]
+    #[case("CCV".to_string(), 205)]
+    #[case("CDXLIV".to_string(), 444)]
+    #[case("CMXCIX".to_string(), 999)]
+    #[case("M".to_string(), 1_000)]
+    #[case("MMMDCCCLXXXVIII".to_string(), 3_888)]
+    #[case("MMMCMX".to_string(), 3_910)]
+    fn test_roman_numeral_to_arabic_number(#[case] roman: String, #[case] arabic: i32) {
+        assert_eq!(roman_to_int(roman), arabic);
     }
 }
