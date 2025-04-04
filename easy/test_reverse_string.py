4b5e97ab5f0910ef29730abf6234cf59aee29749
Your Name <wearypossum4770@yahoo.com>
2025-04-04 00:27:53 -0400

    test(py): include tests for challenge 357b6f1
    approximately-relates-to: #344

---
+++ easy/test_reverse_string.py
@@ -0,0 +1,11 @@
+import pytest
+from easy.reverse_string import reverse_string
+@pytest.mark.parametrize("s, expected",[
+	(["h", "e", "l", "l", "o"],["o", "l", "l", "e", "h"]),
+	(["a", "b", "c", "d"], ["d", "c", "b", "a"]),
+	(["z"], ["z"]),
+	([], []),
+])
+def test_reverse_string(s, expected):
+	reverse_string(s)
+	assert s == expected
