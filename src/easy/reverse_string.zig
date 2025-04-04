10b1a67b092cd8f6d5c1b46a63b0d552195e6cd0
Your Name <wearypossum4770@yahoo.com>
2025-04-04 00:50:34 -0400

    test(zig): include tests for challenge 5a36f50
    approximately-relates-to: #344

--- src/easy/reverse_string.zig
+++ src/easy/reverse_string.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const expectEqualSlices = std.testing.expectEqualSlices;
 
 pub fn reverseString(s: []u8) void {
     if (s.len == 0) return;
@@ -11,4 +12,29 @@ pub fn reverseString(s: []u8) void {
         left += 1;
         right -= 1;
     }
+}
+
+test "reverse normal word" {
+    var input = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
+    const output = [_]u8{ 'o', 'l', 'l', 'e', 'h' };
+    reverseString(&input);
+    try expectEqualSlices(u8, &output, &input);
+}
+test "ordered characters are reversed" {
+    var input = [_]u8{ 'a', 'b', 'c', 'd' };
+    const output = [_]u8{ 'd', 'c', 'b', 'a' };
+    reverseString(&input);
+    try expectEqualSlices(u8, &output, &input);
+}
+test "single character" {
+    var input = [_]u8{'z'};
+    const output = [_]u8{'z'};
+    reverseString(&input);
+    try expectEqualSlices(u8, &output, &input);
+}
+test "empty array/slice" {
+    var input = [_]u8{};
+    const output = [_]u8{};
+    reverseString(&input);
+    try expectEqualSlices(u8, &output, &input);
 }
