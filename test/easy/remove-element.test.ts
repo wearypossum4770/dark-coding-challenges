a6de5e867a8b306c6f8a0920684260ab4c765e5f
Your Name <wearypossum4770@yahoo.com>
2025-04-10 01:40:34 -0400

    test(ts): include tests for challenge c872ec9
    relates-to: #27

---
+++ test/easy/remove-element.test.ts
@@ -0,0 +1,19 @@
+import { describe, test, expect } from "bun:test";
+import { removeElement } from "@/src/easy/remove-element";
+describe("Feature: Remove Element in Place", () => {
+    test.each([
+        [[3, 2, 2, 3], 3, 2, [null, 2, 2, null]],
+        [[0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, 4, 0, 3, null, null, null]],
+    ])(
+        "\n\t\tGiven the array %p\n\t\tAnd: a removal values of %p\n\t\tThen: there are %i values left.",
+        (
+            nums: number[],
+            val: number,
+            expected: number,
+            _actual: Array<number | null>,
+        ) => {
+            const result = removeElement(nums, val);
+            expect(result).toBe(expected);
+        },
+    );
+});
