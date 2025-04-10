245ddbb4ffc7007b5be989491f9ae5fb8cc98ca0
Your Name <wearypossum4770@yahoo.com>
2025-04-10 01:43:40 -0400

    test(php): include tests for challenge adb4ff3
    relates-to: #27

---
+++ test/easy/RemoveElementTest.php
@@ -0,0 +1,18 @@
+
+<?php
+
+use Src\Easy\RemoveElement;
+
+describe('TestValue', function () {
+
+    test('Testing', function ($nums, $target, $expected, $output) {
+        $instance = new RemoveElement;
+        $result = $instance->solve($nums, $target);
+        expect($result)->toBe($expected);
+
+    })->with([
+        [[3, 2, 2, 3], 3, 2, [2, 2]],
+        [[0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, 3, 0, 4]],
+    ]);
+
+});
