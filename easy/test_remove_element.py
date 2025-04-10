067b9265bf567ea019915dc2cc0c0bdb2b654ac3
Your Name <wearypossum4770@yahoo.com>
2025-04-10 01:38:39 -0400

    test(py): include tests for challenge fb50413
    relates-to: #27

---
+++ easy/test_remove_element.py
@@ -0,0 +1,16 @@
+import pytest
+from easy.remove_element import remove_element
+
+
+
+@pytest.mark.parametrize(
+    "nums, subject, expected, actual",
+    [
+        ([3, 2, 2, 3], 3, 2, [None, 2, 2, None]),
+        ([0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, None, None, 3, 0, 4, None]),
+    ],
+)
+def test_remove_element(nums, subject, expected, actual):
+    result = remove_element(nums, subject)
+    assert nums == actual
+    assert result == expected
