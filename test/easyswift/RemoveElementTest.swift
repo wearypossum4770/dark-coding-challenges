cfb4a4b1c0dfe825bc23a62b3d4a61fdf4e2c207
Your Name <wearypossum4770@yahoo.com>
2025-04-10 01:39:03 -0400

    test(swift): include tests for challenge c2b490a
    relates-to: #27

---
+++ test/easyswift/RemoveElementTest.swift
@@ -0,0 +1,13 @@
+// import Testing
+
+// @testable import easy
+
+// @Test(arguments: [
+//   ([3, 2, 2, 3], 3, [nil, 2, 2, nil], 2),
+//   ([0, 1, 2, 2, 3, 0, 4, 2], 2, [0, 1, 4, 0, 3, nil, nil, nil], 5),
+// ])
+// func removeElementTest(nums: [Int], val: Int, actual: [Int?], expected: Int) {
+//   var mutable = nums
+//   var result = removeElement(mutable, val)
+//   #expect(result == expected)
+// }
