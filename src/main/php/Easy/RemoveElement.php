adb4ff307689a4f45de52947a2f215c5be3d3ef6
Your Name <wearypossum4770@yahoo.com>
2025-04-10 00:28:07 -0400

    feat(php): implement remove-element challenge solution
     # 🎯 Current Challenge
    
    | Challenge Title           | Challenge Provider                                                                  |
    | ------------------------- | ----------------------------------------------------------------------------------- |
    | Remove Element            | [LeetCode](https://leetcode.com/problems/remove-element/)                           |
    | Remove Element from Array | [InterviewBit](https://www.interviewbit.com/problems/remove-element-from-array/)    |
    
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 20.67 MB | 22.02 %  |
    
     # 📝 Summary
    
    Given an array `nums` and a value `val`, remove all instances of `val` **in-place** and return the new length. The order of elements may change. Extra space should not be used.
    
      # ♟️ Strategy
    
     1. Initialize a pointer to the count of non-matches.
     2. Create range iterator over the slice of integers.
     3. Check if the value of the current index.
        1. Value at current index is not the target.
        2. Set the value at index of current iteration to the index at the counter
        3. Advance the counter
     4. Once iterator is exhausted return the counter result.
    
     # 🚨 Special Considerations:
    
     - Empty Array -> No change.
    
     # 🔒 Constraints
    
     - 0 <= nums.length <= 100
     - 0 <= nums[i] <= 50
     - 0 <= val <= 100
    
    relates-to: #27

---
+++ src/main/php/Easy/RemoveElement.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Easy;
+
+class RemoveElement
+{
+    public function transformRemoveElement(array &$nums, int $tar): int
+    {
+        $i = 0;
+        foreach ($nums as $num) {
+            if ($num != $tar) {
+                $nums[$i] = $num;
+                $i++;
+            }
+        }
+
+        return $i;
+    }
+
+    public function solve(array &$nums, int $tar): int
+    {
+        return $this->transformRemoveElement($nums, $tar);
+    }
+}
