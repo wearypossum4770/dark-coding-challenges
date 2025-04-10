395c71767eac20e87439a0091d6e1ac3860b6acf
Your Name <wearypossum4770@yahoo.com>
2025-04-10 01:39:37 -0400

    test(rs): include tests for challenge 72d961f
    relates-to: #27

--- src/easy/remove_element.rs
+++ src/easy/remove_element.rs
@@ -21,3 +21,28 @@ pub fn remove_element(nums: &mut Vec<i32>, target: i32) -> i32 {
     }
     count as i32
 }
+#[cfg(test)]
+mod tests {
+    use super::{remove_optional_element, remove_element};
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(&mut vec![3, 2, 2, 3], 3, vec![2, 2], 2)]
+    fn test_remove_element(#[case] input: &mut Vec<i32>, #[case] target: i32, #[case] _result: Vec<i32>, #[case] count: i32) {
+       let output = remove_element(input, target);
+       assert_eq!(output, count);
+    }
+    #[rstest]
+    #[case(&mut vec![Some(3), Some(2), Some(2), Some(3)], 3, 2, &mut vec![None, Some(2), Some(2), None])]
+    #[case(&mut vec![Some(0), Some(1), Some(2), Some(2), Some(3), Some(0), Some(4), Some(2)], 2, 5, &mut vec![Some(0), Some(1), None, None, Some(3), Some(0), Some(4), None])]
+    fn test_remove_optional_element(
+        #[case] input: &mut Vec<Option<i32>>,
+        #[case] target: i32,
+        #[case] expected: i32,
+        #[case] mutated: &mut Vec<Option<i32>>,
+    ) {
+        let result: i32 = remove_optional_element(input, target);
+        assert_eq!(input, mutated);
+        assert_eq!(result, expected);
+    }
+}
