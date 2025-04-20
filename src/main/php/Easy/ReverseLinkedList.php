e5e8735cb2ecef826567380d0393fc638dda6147
Your Name <wearypossum4770@yahoo.com>
2025-04-19 22:11:43 -0400

    feat(php): implement reverse linked list challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 22.15 MB | 38.24 %  |
    
    relates-to: #206

---
+++ src/main/php/Easy/ReverseLinkedList.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Easy;
+
+class ReverseLinkedList
+{
+    public function solve(int $n): array
+    {
+        return $this->transformReverseLinkedList($n);
+    }
+}
