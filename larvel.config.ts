5209ec81c3d7c69d35144940506c12df1011750e
Your Name <wearypossum4770@yahoo.com>
2025-05-26 17:02:26 -0400

    chore(format): format, lint and refactor syntax

--- larvel.config.ts
+++ larvel.config.ts
@@ -1,6 +1,6 @@
 import laravel from "laravel-vite-plugin";
 
 export default laravel({
-	input: ["resources/css/app.css", "resources/js/app.js"],
-	refresh: true,
-})
+    input: ["resources/css/app.css", "resources/js/app.js"],
+    refresh: true,
+});
