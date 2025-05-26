144c72a4596597967c9c0a59b43b19f5af7c937d
Your Name <wearypossum4770@yahoo.com>
2025-05-26 18:15:49 -0400

    chore(format): format, lint and refactor syntax

--- src/very_easy/TriArea.swift
+++ src/very_easy/TriArea.swift
@@ -1,5 +1,3 @@
 // https://edabit.com/challenge/zSmjiQX9jMgh3Kpjr
 
-func triArea(_ base: Int, _ height: Int) -> Int {
-  return (base * height) / 2
-}
+func triArea(_ base: Int, _ height: Int) -> Int { return (base * height) / 2 }
