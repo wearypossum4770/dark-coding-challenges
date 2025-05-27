d03d555ae65c94a8a9705fa79c763df50a22ed4d
Your Name <wearypossum4770@yahoo.com>
2025-05-27 00:39:15 -0400

    test(ts): add test to challenge 72d961f
    relates-to: #27

--- test/easy/remove-element.test.ts
+++ test/easy/remove-element.test.ts
@@ -4,6 +4,7 @@ describe("Feature: Remove Element in Place", () => {
     test.each([
         [[3, 2, 2, 3], 3, 2, [null, 2, 2, null]],
         [[0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, 4, 0, 3, null, null, null]],
+        [[4, 1, 1, 2, 1, 3], 1, 3, [4, null, null, 2, null, 3]],
     ])(
         "\n\t\tGiven the array %p\n\t\tAnd: a removal values of %p\n\t\tThen: there are %i values left.",
         (
