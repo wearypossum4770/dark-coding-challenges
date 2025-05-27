7e697bceb9561ecd1cd28e91410e879bc48b7aa5
Your Name <wearypossum4770@yahoo.com>
2025-05-27 00:25:53 -0400

    refactor(type-safety): increase type safety
    relates-to: #206

--- easy/reverse_linked_list_test.go
+++ easy/reverse_linked_list_test.go
@@ -1,30 +1,30 @@
 package easy
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestReverseLinkedList(t *testing.T) {
 	cases := []struct {
-		nums     *ListNode[int]
-		expected *ListNode[int]
+		nums     []int
+		expected []int
 	}{
-		{BuildList([]int{1, 2, 3, 4, 5}), BuildList([]int{5, 4, 3, 2, 1})},
-		{BuildList([]int{1, 2}), BuildList([]int{2, 1})},
-		{BuildList([]int{}), BuildList([]int{})},
+		{[]int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}},
+		{[]int{1, 2}, []int{2, 1}},
+		{[]int{}, []int{}}, // handles nil properly
 	}
-	for _, c := range cases {
-		t.Run("", func(t *testing.T) {
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("#%02d", i), func(t *testing.T) {
 			t.Parallel()
-			input, output := ListToSlice(c.nums), ListToSlice(c.expected)
+			head := ListNodeFromArray(c.nums)
+			reversed := ReverseLinkedList(head)
+			outcome := ArrayFromListNode(reversed)
 
-			result := ReverseLinkedList(c.nums)
-			if !reflect.DeepEqual(input, output) {
-				t.Errorf("ReverseLinkedList(%v) == %v, expected %v", c.nums, result, c.expected)
+			if !reflect.DeepEqual(outcome, c.expected) {
+				t.Errorf("ReverseLinkedList(%v) == %v, expected %v", c.nums, outcome, c.expected)
 			}
-
 		})
-
 	}
 }
