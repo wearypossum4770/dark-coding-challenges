67fcf5df827d3092d65f7842dd105da9bb2be504
Your Name <wearypossum4770@yahoo.com>
2025-05-27 00:31:06 -0400

    refactor(lint): fix type safety
    relates-to: #1

--- src/easy/two-sum.ts
+++ src/easy/two-sum.ts
@@ -18,9 +18,9 @@
 export const twoSum = (nums: number[], target: number): number[] => {
     const mapper = new Map<number, number>();
     for (let i = 0; i < nums.length; i++) {
-        const complement = mapper.get(target - nums[i]);
-        if (complement) {
-            return [complement, i];
+        const complement = target - nums[i];
+        if (mapper.has(complement)) {
+            return [mapper.get(complement) ?? 0, i];
         }
         mapper.set(nums[i], i);
     }
