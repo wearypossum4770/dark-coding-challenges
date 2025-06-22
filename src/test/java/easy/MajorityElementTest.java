b3551417ad142edeb9477a8356979c23e773748a
Your Name <wearypossum4770@yahoo.com>
2025-06-22 03:41:49 -0400

    test(java): add test to challenge 010253d
    relates-to: #169

---
+++ src/test/java/easy/MajorityElementTest.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class MajorityElementTest {
+  MajorityElement instance = new MajorityElement();
+
+  @ParameterizedTest
+  @MethodSource("majorityElementData")
+  public void testMajorityElement(int[] nums, int expected) {
+    int result = instance.solve(nums);
+
+    assertEquals(expected, result);
+  }
+
+  public static Stream<Arguments> majorityElementData() {
+    return Stream.of(
+        Arguments.of(new int[] {2, 1, 2}, 2),
+        Arguments.of(new int[] {100}, 100),
+        Arguments.of(new int[] {2, 1, 1}, 1),
+        Arguments.of(new int[] {3,2,3}, 3),
+        Arguments.of(new int[] {2,2,1,1,1,2,2}, 2));
+
+  }
+}
