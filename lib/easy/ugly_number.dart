935da82ef59357c24c6c8f5bbe7658d439bbc48a
Your Name <wearypossum4770@yahoo.com>
2025-07-29 14:33:00 -0400

    feat(dart): implement Ugly Number challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 2 ms     | 17.88 %  |
    | Memory    | 151.14 MB| 35.71 %  |
    
    relates-to: #263

---
+++ lib/easy/ugly_number.dart
@@ -0,0 +1,10 @@
+bool uglyNumber(num n) {
+  if (n < 1) return false;
+  List<num> primes = [2, 3, 5];
+  for (var prime in primes) {
+    while (n % prime == 0) {
+      n /= prime;
+    }
+  }
+  return n == 1;
+}
