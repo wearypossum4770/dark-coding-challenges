f8eec95a9879457a4ecc609801fa9eca5f93e31e
Your Name <wearypossum4770@yahoo.com>
2025-07-29 14:20:11 -0400

    feat(php): implement Ugly Number challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 20.39 MB | 76.47 %  |
    
    relates-to: #263

---
+++ src/main/php/Easy/UglyNumber.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Src\Easy;
+class UglyNumber {
+public function transformUglyNumber(int $n): bool {
+	if($n < 1) return false;
+	$primes = [2,3,5];
+	for ($i=0; $i < count($primes); $i++) { 
+		$num = $primes[$i];
+		while ($n % $num === 0) {
+			$n /=$num;
+		}
+	}
+	return $n === 1;
+}
+public function solve(int $n): bool
+{
+return $this->transformUglyNumber($n);
+}
+
+}
