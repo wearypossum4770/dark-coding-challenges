c6853003719f51b25b09bd50e60a4bd4a40e5e00
Your Name <wearypossum4770@yahoo.com>
2025-07-29 19:50:46 -0400

    feat(ts): implement Find the Difference challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 13 ms    | 6.08 %   |
    | Memory    | 58.33 MB | 23.20 %  |
    
    	XOr approach (AI)
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 1 ms     | 95.58 %  |
    | Memory    | 53.54 MB | 99.72 %  |
    
    relates-to: #389

--- src/easy/ugly-number.ts
+++ src/easy/ugly-number.ts
@@ -1,13 +1,11 @@
 // https://leetcode.com/problems/ugly-number
 export const uglyNumber = (n: number): boolean => {
 	if (n <= 0) return false;
-	for(let index)
+	const primes = [2,3,5]
+	for(let prime of primes) {
+	while(n % prime === 0) {
+		n /= prime;
+	}
+	}
+	return n === 1
 }
-
-
-console.log(uglyNumber(6), true)
-console.log(uglyNumber(1), true)
-console.log(uglyNumber(14), false)
-console.log(uglyNumber(30), true)
-console.log(uglyNumber(0), false)
-console.log(uglyNumber(-1), false)
