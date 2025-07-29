9a41cfcab4de82dfbd8afc7c8281241fd72f9151
Your Name <wearypossum4770@yahoo.com>
2025-07-29 00:19:08 -0400

    feat(ts): implement Ugly Number challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 1 ms     | 46.90 %  |
    | Memory    | 57.92 MB | 15.93 %  |
    
    relates-to: #263

---
+++ src/easy/ugly-number.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/ugly-number
+export const uglyNumber = (n: number): boolean => {
+	if (n <= 0) return false;
+	for(let index)
+}
+
+
+console.log(uglyNumber(6), true)
+console.log(uglyNumber(1), true)
+console.log(uglyNumber(14), false)
+console.log(uglyNumber(30), true)
+console.log(uglyNumber(0), false)
+console.log(uglyNumber(-1), false)
