0415b3c42b3fb74a8f94a3e5248d75c8f585f8f8
Your Name <wearypossum4770@yahoo.com>
2025-07-30 03:11:03 -0400

    test(swift): add test to challenge f8013f6
    relates-to: #389

---
+++ test/easyswift/FindTheDifferenceTest.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import easy
+
+@Test(arguments: [
+	("abcd", "abcde", "e"),
+	("", "y", "y"),
+	("a", "aa", "a"),
+	("aabbcc", "aabbccd", "d"),
+])
+func findTheDifferenceTest(s: String, t: String, expected: Character?) {
+  #expect(findTheDifference(s, t) == expected)
+}
