bf2327667ae4f4014dfbc0858c7c06a4a5778b05
Your Name <wearypossum4770@yahoo.com>
2025-07-30 03:10:03 -0400

    test(java): add test to challenge b877977
    relates-to: #389

---
+++ src/test/java/easy/FindTheDifferenceTest.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class FindTheDifferenceTest {
+  FindTheDifference instance = new FindTheDifference();
+
+  @ParameterizedTest
+  @MethodSource("dataFindTheDifference")
+  public void testFindTheDifference(String s, String t, char expected) {
+
+    char result = instance.solve(s, t);
+
+    assertEquals(expected, result);
+  }
+
+  public static Stream<Arguments> dataFindTheDifference() {
+    return Stream.of(
+      Arguments.of("abcd", "abcde", 'e'),
+      Arguments.of("", "y", 'y'),
+      Arguments.of("a", "aa", 'a'),
+      Arguments.of("aabbcc", "aabbccd", 'd')
+      );
+  }
+}
