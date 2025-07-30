587d166d1f1f51df95ad002dbbec223c6120e578
Your Name <wearypossum4770@yahoo.com>
2025-07-30 03:11:41 -0400

    test(kt): add test to challenge 7ffe150
    relates-to: #389

---
+++ src/test/kotlin/easy/KodeeFindTheDifferenceTest.kt
@@ -0,0 +1,31 @@
+
+package easy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+
+class KodeeFindTheDifferenceTest {
+    @ParameterizedTest
+    @MethodSource("findTheDifferenceData")
+    fun kodeeFindTheDifferenceTest(
+        s: String,
+        t: String,
+        expected: Char,
+    ) {
+        val result = kodeeFindTheDifference(s, t)
+        assertEquals(expected, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun findTheDifferenceData(): Stream<Arguments> =
+            Stream.of(
+            Arguments.of("abcd", "abcde", 'e'),
+            Arguments.of("", "y", 'y'),
+            Arguments.of("a", "aa", 'a'),
+            Arguments.of("aabbcc", "aabbccd", 'd'),
+            )
+    }
+}
