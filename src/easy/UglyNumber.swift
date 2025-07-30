6f6e1152602cca5fafbde95685067b04ab4f48d4
Your Name <wearypossum4770@yahoo.com>
2025-07-30 02:27:32 -0400

    chore(format): format, lint and refactor syntax

--- src/easy/UglyNumber.swift
+++ src/easy/UglyNumber.swift
@@ -1,12 +1,12 @@
-func uglyNumber(_ n: Int)-> Bool {
-	if n <= 0 { return false }
-	var primes = [2,3,5]
-	var num = n
-	for prime in primes {
-		while num % prime == 0 {
-			num/=prime
-		}
-		
-	}
-	return num == 1
+func uglyNumber(_ n: Int) -> Bool {
+  if n <= 0 { return false }
+  var primes = [2, 3, 5]
+  var num = n
+  for prime in primes {
+    while num % prime == 0 {
+      num /= prime
+    }
+
+  }
+  return num == 1
 }
