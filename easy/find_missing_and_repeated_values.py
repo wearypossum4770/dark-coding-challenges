6a6ca4a7ae7242c00b98001b16a9ccce9f49552b
Your Name <wearypossum4770@yahoo.com>
2025-08-01 17:10:24 -0400

    feat(py): implement Find Missing and Repeated challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 7 ms     | 68.10 %  |
    | Memory    | 18.15 MB | 63.20 %  |
    
    relates-to: #2965

---
+++ easy/find_missing_and_repeated_values.py
@@ -0,0 +1,14 @@
+def find_missing_and_repeated_values(grid: list[list[int]], )->list[int]:
+	store = set()
+	n = len(grid)
+	repeated = 0
+	expected =( pow(n, 2) * (pow(n, 2) + 1) ) // 2
+	for row in grid:
+		for cell in row:
+			if cell in store:
+				repeated = cell
+			else:
+				store.add(cell)
+				expected -= cell
+
+	return [repeated, expected]
