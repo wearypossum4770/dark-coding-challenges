c9ba90559849979bd9394008d06fdd1889dce7b7
Your Name <wearypossum4770@yahoo.com>
2025-08-06 19:50:47 -0400

    feat(go): implement Anagram Additional challenge solution
    relates-to: #242

--- easy/is_anagram.go
+++ easy/is_anagram.go
@@ -1,19 +1,35 @@
 package easy
 
+import (
+	"strings"
+)
+
 func IsAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
 	freq := make(map[rune]int)
-	for _, v := range s {
+	for _, v := range strings.ToLower(s) {
 		freq[v]++
 	}
 
-	for _, ch := range t {
+	for _, ch := range strings.ToLower(t) {
 		if freq[ch] == 0 {
 			return false
 		}
 		freq[ch]--
 	}
 	return true
+}
+
+func DetectAnagrams(word string, candidates []string) []string {
+	lowerWord := strings.ToLower(word)
+	valid := []string{}
+	for _, candidate := range candidates {
+		lowerCandidate := strings.ToLower(candidate)
+		if lowerWord != lowerCandidate && IsAnagram(lowerWord, lowerCandidate) {
+			valid = append(valid, candidate)
+		}
+	}
+	return valid
 }
