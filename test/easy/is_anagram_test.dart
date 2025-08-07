3182a0466dc52221ea5ad7527e2da010ea03ce1c
Your Name <wearypossum4770@yahoo.com>
2025-08-06 22:37:53 -0400

    test(dart): add test to challenge 4b04e75
    relates-to: #242

--- test/easy/is_anagram_test.dart
+++ test/easy/is_anagram_test.dart
@@ -44,4 +44,34 @@ void main() {
       });
     }
   });
+  group("\n\tFeature: Detect Anagram from list", () {
+    final testCases = [
+      DetectAnagramTestCases(["gallery", "regally", "largely"], "allergy", ["gallery", "ballerina", "regally", "clergy", "largely", "leading"], "detects three anagrams"),
+      DetectAnagramTestCases(["inlets"], "listen", ["enlists", "google", "inlets", "banana"], "detects anagram"),
+      DetectAnagramTestCases([], "good", ["dog", "goody"], "does not detect anagram subsets"),
+      DetectAnagramTestCases(["lemons", "melons"], "solemn", ["lemons", "cherry", "melons"], "detects two anagrams"),
+      DetectAnagramTestCases([], "diaper", ["hello", "world", "zombies", "pants"], "no matches"),
+      DetectAnagramTestCases(["Eons", "ONES"], "nose", ["Eons", "ONES"], "detects multiple anagrams with different case"),
+      DetectAnagramTestCases([], "mass", ["last"], "does not detect non-anagrams with identical checksum"),
+      DetectAnagramTestCases(["Carthorse"], "Orchestra", ["cashregister", "Carthorse", "radishes"], "detects anagrams case-insensitively"),
+      DetectAnagramTestCases(["carthorse"], "Orchestra", ["cashregister", "carthorse", "radishes"], "detects anagrams using case-insensitive subject"),
+      DetectAnagramTestCases(["Carthorse"], "orchestra", ["cashregister", "Carthorse", "radishes"], "detects anagrams using case-insensitive possible matches"),
+      DetectAnagramTestCases([], "go", ["goGoGO"], "does not detect an anagram if the original word is repeated"),
+      DetectAnagramTestCases([], "tapper", ["patter"], "anagrams must use all letters exactly once"),
+      DetectAnagramTestCases([], "BANANA", ["BANANA"], "words are not anagrams of themselves"),
+      DetectAnagramTestCases([], "BANANA", ["Banana"], "words are not anagrams of themselves even if letter case is partially different"),
+      DetectAnagramTestCases([], "BANANA", ["banana"], "words are not anagrams of themselves even if letter case is completely different"),
+      DetectAnagramTestCases(["Silent"], "LISTEN", ["LISTEN", "Silent"], "words other than themselves can be anagrams"),
+    ];
+    for (final step in testCases) {
+      final expected = step.expected;
+      final word = step.word;
+      final candidates = step.candidates;
+      final criteria = step.criteria;
+      test("\n\tGiven: the string \x1b[38;5;221m'$word'\x1b[0m\nAnd: array $candidates\n\tWhen feed to detectAnagrams\n\tThen: it returns $expected", () {
+        List<String> result = detectAnagram(word, candidates);
+        expect(result, equals(expected));
+      });
+    }
+  });
 }
