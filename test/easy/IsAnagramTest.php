1d209cd6cbb1ee5edadeaaffafe710972e6905d8
Your Name <wearypossum4770@yahoo.com>
2025-08-06 23:10:43 -0400

    test(php): add test to challenge 9c6291b
    relates-to: #242

--- test/easy/IsAnagramTest.php
+++ test/easy/IsAnagramTest.php
@@ -25,4 +25,45 @@ describe('\n\tFeature: Anagram detection', function () {
         ['aaaaa', 'aaaaa', true],
         ['abcd', 'abdc', true],
     ]);
+    test("\n\tDetecting anagrams from a list", function (array $expected, string $word, array $candidates, string $criteria) {
+        $instance = new IsAnagram;
+        expect($instance->detectAnagram($word, $candidates))->toBe($expected);
+    })->with([
+        [
+            ['gallery', 'regally', 'largely'], 'allergy',
+            ['gallery', 'ballerina', 'regally', 'clergy', 'largely', 'leading'], 'detects three anagrams',
+        ],
+        [['inlets'], 'listen', ['enlists', 'google', 'inlets', 'banana'], 'detects anagram'],
+        [[], 'good', ['dog', 'goody'], 'does not detect anagram subsets'],
+        [
+            ['lemons', 'melons'], 'solemn', ['lemons', 'cherry', 'melons'], 'detects two anagrams',
+        ],
+        [[], 'diaper', ['hello', 'world', 'zombies', 'pants'], 'no matches'],
+        [['Eons', 'ONES'], 'nose', ['Eons', 'ONES'], 'detects multiple anagrams with different case'],
+        [[], 'mass', ['last'], 'does not detect non-anagrams with identical checksum'],
+        [
+            ['Carthorse'], 'Orchestra', ['cashregister', 'Carthorse', 'radishes'],
+            'detects anagrams case-insensitively',
+        ],
+        [
+            ['carthorse'], 'Orchestra', ['cashregister', 'carthorse', 'radishes'],
+            'detects anagrams using case-insensitive subject',
+        ],
+        [
+            ['Carthorse'], 'orchestra', ['cashregister', 'Carthorse', 'radishes'],
+            'detects anagrams using case-insensitive possible matches',
+        ],
+        [[], 'go', ['goGoGO'], 'does not detect an anagram if the original word is repeated'],
+        [[], 'tapper', ['patter'], 'anagrams must use all letters exactly once'],
+        [[], 'BANANA', ['BANANA'], 'words are not anagrams of themselves'],
+        [
+            [], 'BANANA', ['Banana'],
+            'words are not anagrams of themselves even if letter case is partially different',
+        ],
+        [
+            [], 'BANANA', ['banana'],
+            'words are not anagrams of themselves even if letter case is completely different',
+        ],
+        [['Silent'], 'LISTEN', ['LISTEN', 'Silent'], 'words other than themselves can be anagrams'],
+    ]);
 });
