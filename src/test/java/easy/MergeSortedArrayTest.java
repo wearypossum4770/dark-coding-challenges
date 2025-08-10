3271592e96b690c5ada906daee8f482f19e76b20
Your Name <wearypossum4770@yahoo.com>
2025-08-09 22:38:10 -0400

    test(java): add test to challenge 9b7dafd
    relates-to: #88

---
+++ src/test/java/easy/MergeSortedArrayTest.java
@@ -0,0 +1,26 @@
+package easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class MergeSortedArrayTest {
+  MergeSortedArray instance = new MergeSortedArray();
+  @ParameterizedTest
+  @MethodSource("dataMergeSortedArray")
+  public void testMergeSortedArrayInPlace( int[] first, int firstSize, int[] second, int secondSize, int[] expected) {
+    instance.mergeSortedArrayInPlace(first, firstSize, second, secondSize);
+    assertArrayEquals(expected, first);
+  }
+
+  public static Stream<Arguments> dataMergeSortedArray() {
+    return Stream.of(
+        Arguments.of(
+            new int[] {1, 2, 3, 0, 0, 0}, 3, new int[] {2, 5, 6}, 3, new int[] {1, 2, 2, 3, 5, 6}),
+        Arguments.of(new int[]{0}, 0, new int[]{1}, 1, new int[]{1}),
+        Arguments.of(new int[] {1}, 1, new int[] {}, 0, new int[] {1}));
+  }
+}
