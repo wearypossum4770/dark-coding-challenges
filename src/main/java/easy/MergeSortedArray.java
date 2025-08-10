83647ac7532f901be2bb96a9035e1edb48e2b003
Your Name <wearypossum4770@yahoo.com>
2025-08-09 22:35:42 -0400

    feat(java): merge sort with mutation
    related-to: #88

--- src/main/java/easy/MergeSortedArray.java
+++ src/main/java/easy/MergeSortedArray.java
@@ -21,4 +21,31 @@ public class MergeSortedArray {
       k--;
     }
   }
- }
+  public int[] solve(int[] nums1, int m, int[] nums2, int n) {
+    int[] result = new int[m + n];
+    int k = 0;
+    int i = 0;
+    int j = 0;
+    while( i < m && j < n) {
+      if (nums1[i] <= nums2[j]) {
+        result[k] = nums1[i];
+        i++;
+      }else {
+        result[k] = nums2[j];
+        j++;
+      }
+      k++;
+    }
+    while(i < m) {
+      result[k] = nums1[i];
+      i++;
+      k++;
+    }
+    while(j < n) {
+      result[k] = nums2[j];
+      j++;
+      k++;
+    }
+    return result;
+  }
+}
