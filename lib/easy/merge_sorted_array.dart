c390e482de7717376a14da9004b09e17f1446b92
Your Name <wearypossum4770@yahoo.com>
2025-08-10 13:22:18 -0400

    feat(dart): merge sort with mutation
    related-to: #88

--- lib/easy/merge_sorted_array.dart
+++ lib/easy/merge_sorted_array.dart
@@ -19,3 +19,33 @@ void mergeSortedArrayInPlace(List<int> nums1, int m, List<int> nums2, int n) {
 	}
 
 }
+
+List<int> mergeSortedArray(List<int> nums1, int m, List<int> nums2, int n) {
+	var result = List.filled(m + n, 0, growable: false);
+	int i = 0; 
+	int j = 0;
+	int k = 0;
+  while (i < m && j < n) {
+    if (nums1[i] <= nums2[j]) {
+      result[k] = nums1[i];
+      i++;
+    } else {
+      result[k] = nums2[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < m) {
+    result[k] = nums1[i];
+    i++;
+    k++;
+  }
+  while (j < n) {
+    result[k] = nums2[j];
+    j++;
+    k++;
+  }
+
+  return result;
+}
+
