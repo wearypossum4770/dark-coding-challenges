1c61aa2c843ad2a001dd985b8d81c8563934857a
Your Name <wearypossum4770@yahoo.com>
2025-08-09 22:55:32 -0400

    chore(format): format, lint and refactor syntax

--- src/main/java/easy/MergeSortedArray.java
+++ src/main/java/easy/MergeSortedArray.java
@@ -5,43 +5,44 @@ public class MergeSortedArray {
     int k = m + n - 1;
     m--;
     n--;
-    while(m >= 0 && n >= 0) {
+    while (m >= 0 && n >= 0) {
       if (nums1[m] >= nums2[n]) {
         nums1[k] = nums1[m];
         m--;
-      }else {
+      } else {
         nums1[k] = nums2[n];
         n--;
       }
       k--;
     }
-    while(n >= 0) {
+    while (n >= 0) {
       nums1[k] = nums2[n];
       n--;
       k--;
     }
   }
+
   public int[] solve(int[] nums1, int m, int[] nums2, int n) {
     int[] result = new int[m + n];
     int k = 0;
     int i = 0;
     int j = 0;
-    while( i < m && j < n) {
+    while (i < m && j < n) {
       if (nums1[i] <= nums2[j]) {
         result[k] = nums1[i];
         i++;
-      }else {
+      } else {
         result[k] = nums2[j];
         j++;
       }
       k++;
     }
-    while(i < m) {
+    while (i < m) {
       result[k] = nums1[i];
       i++;
       k++;
     }
-    while(j < n) {
+    while (j < n) {
       result[k] = nums2[j];
       j++;
       k++;
