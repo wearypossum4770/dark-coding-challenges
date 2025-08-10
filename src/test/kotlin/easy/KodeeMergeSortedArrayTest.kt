40fa76e5c341b1e506b30183cc926621dfa783d0
Your Name <wearypossum4770@yahoo.com>
2025-08-09 23:00:14 -0400

    test(kt): add test to challenge e83631e
    relates-to: #88

---
+++ src/test/kotlin/easy/KodeeMergeSortedArrayTest.kt
@@ -0,0 +1,32 @@
+
+package easy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.test.assertContentEquals
+
+class KodeeMergeSortedArrayTest {
+    @ParameterizedTest
+    @MethodSource("mergeSortedArrayData")
+    fun kodeeMergeSortedArrayTest(
+        first: IntArray,
+        firstSize: Int,
+        second: IntArray,
+        secondSize: Int,
+        expected: IntArray,
+    ) {
+        val result = kodeeMergeSortedArray(first, firstSize, second, secondSize)
+        assertContentEquals(expected, result)
+    }
+    
+    companion object {
+        @JvmStatic
+        fun mergeSortedArrayData(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(intArrayOf(1, 2, 3, 0, 0, 0), 3, intArrayOf(2, 5, 6), 3, intArrayOf(1, 2, 2, 3, 5, 6)),
+                Arguments.of(intArrayOf(1), 1, intArrayOf(), 0, intArrayOf(1)),
+                Arguments.of(intArrayOf(0), 0, intArrayOf(1), 1, intArrayOf(1)),
+            )
+    }
+}
