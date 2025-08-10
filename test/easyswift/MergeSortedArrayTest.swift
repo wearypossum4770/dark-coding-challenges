ceaaa0e6059440b5f81187348c9a200b11753a0c
Your Name <wearypossum4770@yahoo.com>
2025-08-10 00:13:54 -0400

    test(swift): add test to challenge bf8cac1
    relates-to: #88

--- test/easyswift/MergeSortedArrayTest.swift
+++ test/easyswift/MergeSortedArrayTest.swift
@@ -2,6 +2,17 @@ import Testing
 
 @testable import easy
 
+@Test("Merge Sorted Arrays as functional programming", arguments: [
+  ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
+  ([1], 1, [], 0, [1]),
+  ([0], 0, [1], 1, [1]),
+])
+func mergeSortedArrayTest(
+  first: [Int], firstSize: Int, second: [Int], secondSize: Int, outcome: [Int]
+) {
+  #expect(mergeSortedArray(first, firstSize, second, secondSize) == outcome)
+}
+
 @Test("Merge Sorted Arrays In Place", arguments: [
   ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
   ([1], 1, [], 0, [1]),
