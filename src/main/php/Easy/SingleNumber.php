e0bf8a827d75faff840f1ce28dca2ee1a9668608
Your Name <wearypossum4770@yahoo.com>
2025-08-11 15:21:11 -0400

    feat(php): implement Single Number challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 21.50 MB | 97.89 %  |
    
    relates-to: #136

---
+++ src/main/php/Easy/SingleNumber.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Src\Easy;
+class SingleNumber {
+public function transformSingleNumber(array $nums): int{
+	$result = 0;
+	foreach ($nums as $val) {
+		$result ^= $val;
+	}
+	return $result;
+}
+public function solve(array $n): int
+{
+return $this->transformSingleNumber($n);
+}
+
+}
