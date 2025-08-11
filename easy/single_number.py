eec210973266c5008d4ff1731f3b48670ad0063e
Your Name <wearypossum4770@yahoo.com>
2025-08-11 15:46:46 -0400

    chore(format): format, lint and refactor syntax

--- easy/single_number.py
+++ easy/single_number.py
@@ -1,3 +1,5 @@
 from functools import reduce
-def single_number(nums: list[int])-> int:
-	return reduce(lambda x, y: x ^y, nums)
+
+
+def single_number(nums: list[int]) -> int:
+    return reduce(lambda x, y: x ^ y, nums)
