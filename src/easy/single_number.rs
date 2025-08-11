1c4285291016dde49c6d5ed55d699587a808f674
Your Name <wearypossum4770@yahoo.com>
2025-08-11 15:49:38 -0400

    test(rs): add test to challenge ea25b09
    relates-to: #136

--- src/easy/single_number.rs
+++ src/easy/single_number.rs
@@ -1,3 +1,15 @@
 pub fn single_number(nums: Vec<i32>) -> i32 {
         nums.into_iter().fold(0, |acc, x| acc ^ x)
 }
+#[cfg(test)]
+mod tests {
+    use super::single_number;
+    use rstest::rstest;
+    #[rstest]
+    #[case(vec![2,2,1], 1)]
+    #[case(vec![4,1,2,1,2], 4)]
+    #[case(vec![1], 1)]
+    fn test_single_number(#[case] nums: Vec<i32>, #[case] expected: i32) {
+        assert_eq!(single_number(nums), expected);
+    }
+}
