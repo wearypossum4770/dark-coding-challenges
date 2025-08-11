437e46ef61655d61fbd9c5a1936b393a5a440161
wearypossum4770 <50812322+wearypossum4770@users.noreply.github.com>
2025-08-11 19:49:09 +0000

    Dusting

--- src/main/php/Easy/SingleNumber.php
+++ src/main/php/Easy/SingleNumber.php
@@ -1,17 +1,23 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Src\Easy;
-class SingleNumber {
-public function transformSingleNumber(array $nums): int{
-	$result = 0;
-	foreach ($nums as $val) {
-		$result ^= $val;
-	}
-	return $result;
-}
-public function solve(array $n): int
-{
-return $this->transformSingleNumber($n);
-}
 
+class SingleNumber
+{
+    public function transformSingleNumber(array $nums): int
+    {
+        $result = 0;
+        foreach ($nums as $val) {
+            $result ^= $val;
+        }
+
+        return $result;
+    }
+
+    public function solve(array $n): int
+    {
+        return $this->transformSingleNumber($n);
+    }
 }
