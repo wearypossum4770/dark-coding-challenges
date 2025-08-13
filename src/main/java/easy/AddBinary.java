3a61a5631c3f0b8bcf2708fb8d5ce2b9143cb16a
Your Name <wearypossum4770@yahoo.com>
2025-08-13 12:56:34 -0400

    feat(java): implement Add Binary challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 1 ms     | 99.85 %  |
    | Memory    | 42.74 MB | 18.28 %  |
    
    relates-to: #67

---
+++ src/main/java/easy/AddBinary.java
@@ -0,0 +1,20 @@
+package easy;
+public class AddBinary {
+public String solve(String a, String b) {
+	char[] result = new char[Math.max(a.length(), b.length()) + 1];
+	int i = a.length() - 1;
+	int j = b.length() - 1;
+	int k = result.length - 1;
+	int carry = 0;
+
+	while( i >= 0 || j >= 0|| carry > 0) {
+		int sum = carry;
+		if (i >= 0) sum += a.charAt(i--) - '0';
+		if (j >= 0) sum += b.charAt(j--)  - '0';
+		
+		result[k--] = (char)('0' + (sum % 2));
+
+		carry = sum / 2;
+	}
+	return new String(result).trim();
+}}
