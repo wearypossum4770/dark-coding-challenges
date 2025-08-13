ddaf7ee141252f21425544b5d582495db1e99ca9
Your Name <wearypossum4770@yahoo.com>
2025-08-13 13:32:08 -0400

    feat(py): implement Add Binary challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 17.70 MB | 91.63 %  |
    
    relates-to: #67

---
+++ easy/add_binary.py
@@ -0,0 +1,15 @@
+def add_binary(a: str, b: str) -> str:
+    res = ["0"] * (max(len(a), len(b)) + 1)
+    i, j, k, carry = len(a) - 1, len(b) - 1, len(res) - 1, 0
+    while i >= 0 or j >= 0 or carry > 0:
+        s = carry
+        if i >= 0:
+            s += ord(a[i]) - ord("0")
+            i -= 1
+        if j >= 0:
+            s += ord(b[j]) - ord("0")
+            j -= 1
+        res[k] = str(s % 2)
+        carry = s // 2
+        k -= 1
+    return "".join(res[k+1:])
