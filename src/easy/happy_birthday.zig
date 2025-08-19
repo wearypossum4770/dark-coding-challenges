2dd1fd4a74930bb779a4122b4584500e76637b2a
Your Name <wearypossum4770@yahoo.com>
2025-08-18 21:54:03 -0400

    test(zig): add test to challenge a64c3eb

--- src/easy/happy_birthday.zig
+++ src/easy/happy_birthday.zig
@@ -4,3 +4,16 @@ pub fn happyBirthday(date: []const u8) []const u8 {
     if (std.mem.eql(u8, date, "18")) return "Happy Birthday!";
     return "Just a normal day!";
 }
+
+test "Happy Birthday!" {
+    const date = "18";
+    const expected = "Happy Birthday!";
+    const actual = happyBirthday(date);
+    try std.testing.expectEqualSlices(u8, expected, actual);
+}
+test "Just a normal day!" {
+    const date = "";
+    const expected = "Just a normal day!";
+    const actual = happyBirthday(date);
+    try std.testing.expectEqualSlices(u8, expected, actual);
+}
