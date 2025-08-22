d63bea3ddff9adaa19760c6c2a401b544de67a3f
Your Name <wearypossum4770@yahoo.com>
2025-08-22 15:11:04 -0400

    feat(ts): implement Distribute Elements Into Two Arrays II challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 332 ms   |  25.00 % |
    | Memory    | 100.51MB |  12.50 % |
    
    relates-to: #3072
    
    Classification is only possible for natural numbers.
    Classification is only possible for natural numbers

--- src/easy/two-ways-distribution.ts
+++ src/easy/two-ways-distribution.ts
@@ -1,21 +1,35 @@
 import { FenwickTree } from '../helpers/data-structures'
+
 export const twoWaysDistribution = (nums: number[]): number[] => {
-	const [first, second] = [[nums[0]], [nums[1]]]
-	for (const num of nums.slice(2)) {
-		let  f = first.filter(v=> v > num), s = second.filter(v=> v > num)
-		if (f.length === s.length) {
-			if( second.length < first.length) {
-				second.push(num)
-			}else {
-			first.push(num)
-			}
-		}
-		if (f.length > s.length) {
-			first.push(num)
-		}
-		else if (f.length < s.length) {
-			second.push(num)
-		}
-	}
+	const sorted = [...new Set(nums)].sort((a, b) => a - b);
+const index = new Map(sorted.map((v, i) => [v, i + 1]));
+	const firstTree = new FenwickTree(sorted.length), secondTree = new FenwickTree(sorted.length)
+const first: number[] = [nums[0]];
+const second: number[] = [nums[1]];
+firstTree.update(index.get(nums[0])!, 1);
+secondTree.update(index.get(nums[1])!, 1);
+
+for (const num of nums.slice(2)) {
+    const rank = index.get(num)!;
+    const fCount = firstTree.countGreater(rank);
+    const sCount = secondTree.countGreater(rank);
+
+    if (fCount === sCount) {
+        if (second.length < first.length) {
+            second.push(num);
+            secondTree.update(rank, 1);
+        } else {
+            first.push(num);
+            firstTree.update(rank, 1);
+        }
+    } else if (fCount > sCount) {
+        first.push(num);
+        firstTree.update(rank, 1);
+    } else {
+        second.push(num);
+        secondTree.update(rank, 1);
+    }
+}
+
 	return first.concat(second)
 }
