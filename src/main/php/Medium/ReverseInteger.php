5dbfbfe042beda6005b4a1fd1ead50024ce42b27
Your Name <wearypossum4770@yahoo.com>
2025-08-27 14:47:21 -0400

    feat(php): implement Reverse Integer challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 16 ms    | 8.97 %   |
    | Memory    | 20.36 MB | 79.06 %  |
    
    approximately-related-to: #7

---
+++ src/main/php/Medium/ReverseInteger.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Src\Medium;
+class ReverseInteger {
+public function transformReverseInteger(int $x): int {
+	$res = 0;
+	$sign = $x >= 0 ? 1 : -1;
+	$x = abs($x);
+	while($x != 0) {
+		$res = $res * 10 + $x  % 10;
+		$x = intdiv($x, 10);
+		
+	}
+	if($res > 0x7FFFFFFF || $res < -0x80000000) return 0;
+	return $res * $sign;
+	
+}
+public function solve(int $n): array
+{
+return $this->transformReverseInteger($n);
+}
+
+}
