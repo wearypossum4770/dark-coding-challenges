d9bf01806fea78696ae31da6de94f834b074e5ff
Your Name <wearypossum4770@yahoo.com>
2025-08-27 14:59:12 -0400

    feat(kt): implement Reverse Integer challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 107 ms   | 38.57 %  |
    | Memory    | 40.42 MB | 42.53 %  |
    
    approximately-related-to: #7

---
+++ src/main/kotlin/medium/KodeeReverseInteger.kt
@@ -0,0 +1,19 @@
+package medium;
+import kotlin.math.abs
+
+fun kodeeReverseInteger(x: Int): Int  {
+	var num: Long = 0
+		var sign: Long  = 1
+    if( x < 0 ) {
+        sign *= -1
+    }
+
+	var y = abs(x)
+	while( y != 0) {
+
+		num = num * 10 + (y % 10)
+		y /= 10
+	}
+	if(num > 0x7FFFFFFF || num < -0x80000000 ) return 0
+	return num.toInt() * sign.toInt()
+}
