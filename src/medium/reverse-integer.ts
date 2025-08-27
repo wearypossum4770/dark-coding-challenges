5a35fb84430d735daa4b1e8dcd4594bf1b32f899
Your Name <wearypossum4770@yahoo.com>
2025-08-27 14:38:00 -0400

    feat(ts): implement Reverse Integer challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 55 ms    | 43.76 %  |
    | Memory    | 57.75 MB | 46.11 %  |
    
    approximately-related-to: #7

---
+++ src/medium/reverse-integer.ts
@@ -0,0 +1,9 @@
+export const reverseInteger = (x: number): number => {
+	let num = 0;
+	while( x != 0) {
+		num = num * 10 + (x % 10);
+		x = Math.trunc(x / 10);
+	}
+	if (num > 0x7FFFFFFF || num < -0x80000000) return 0;    
+	return num;
+}
