957e9c90607546030dc2058e4b9928926144c2f1
Your Name <wearypossum4770@yahoo.com>
2025-08-29 20:12:22 -0400

    feat(kt): implement (Binary) Search Insertion challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 43.48 MB | 47.24 %  |
    
    approximately-related-to: #35

---
+++ src/main/kotlin/easy/KodeeSearchInsertPosition.kt
@@ -0,0 +1,20 @@
+package easy
+
+fun kodeeSearchInsertPosition(
+    nums: IntArray,
+    target: Int,
+): Int {
+    var left = 0
+    var right = nums.size - 1
+		while(left <= right) {
+			val mid = left + (right - left) / 2
+			if (nums[mid] == target) return mid
+			if (nums[mid] < target) {
+				left = mid + 1
+			}else {
+				right = mid - 1
+			}
+		}
+
+    return left
+}
