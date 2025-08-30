9077931d8be7900a0e50852e092123f760c8470c
wearypossum4770 <50812322+wearypossum4770@users.noreply.github.com>
2025-08-30 00:18:36 +0000

    Dusting

--- src/main/php/Easy/SearchInsertPosition.php
+++ src/main/php/Easy/SearchInsertPosition.php
@@ -6,21 +6,24 @@ namespace Src\Easy;
 
 class SearchInsertPosition
 {
-    public function transformSearchInsertPosition(array $nums, int $target): int {
-			$left= 0;
-			$right =  count($nums);
-			while($left <= $right) {
-				$mid = intdiv($left + ($right - $left), 2);
-				if($nums[$mid] == $target) return $mid;
-				if($nums[$mid] < $target) {
-					$left = $mid + 1;
-				}else {
-					$right = $mid - 1;
-				}
-			}
+    public function transformSearchInsertPosition(array $nums, int $target): int
+    {
+        $left = 0;
+        $right = count($nums);
+        while ($left <= $right) {
+            $mid = intdiv($left + ($right - $left), 2);
+            if ($nums[$mid] == $target) {
+                return $mid;
+            }
+            if ($nums[$mid] < $target) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid - 1;
+            }
+        }
 
-			return $left;
-		}
+        return $left;
+    }
 
     public function solve(array $nums, int $target): int
     {
