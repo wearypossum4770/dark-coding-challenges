9968ad247fd2f60d0003af5a47ea386fa33221f3
Your Name <wearypossum4770@yahoo.com>
2025-08-29 20:04:23 -0400

    feat(swift): implement (Binary) Search Insertion challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 19.36 MB | 91.81 %  |
    
    approximately-related-to: #35

---
+++ src/easy/SearchInsertPosition.swift
@@ -0,0 +1,16 @@
+func searchInsertPosition(_ nums: [Int], _ target: Int) -> Int {
+  var left = 0
+  var right = nums.count - 1
+	while left <= right {
+		let mid = left + (right - left) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			left = mid + 1
+		}else {
+			right = mid - 1
+		}
+	}
+  return left
+}
