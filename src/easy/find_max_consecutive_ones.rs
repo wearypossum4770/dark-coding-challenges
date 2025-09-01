76ba1d39b3d0fa4dda11722342487b19698cbd33
Your Name <wearypossum4770@yahoo.com>
2025-09-01 10:01:34 -0400

    test(rs): add test to challenge d94291e
    relates-to: #485

--- src/easy/find_max_consecutive_ones.rs
+++ src/easy/find_max_consecutive_ones.rs
@@ -13,3 +13,17 @@ pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
 	}
     m.max(count)
 }
+#[cfg(test)]
+mod tests {
+    use super::find_max_consecutive_ones;
+    use rstest::rstest;
+    #[rstest]
+    #[case(vec![1,1,0,1,1,1], 3)]
+    #[case(vec![1,0,1,1,0,1], 2)]
+		#[case(vec![0], 0)]
+		#[case(vec![1], 1)]
+		#[case(vec![1, 1, 1, 1], 4)]
+    fn test_find_max_consecutive_ones(#[case] nums: Vec<i32>, #[case] expected: i32) {
+        assert_eq!(find_max_consecutive_ones(nums), expected);
+    }
+}
