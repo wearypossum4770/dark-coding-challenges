37d9205f7a492b934c8835197887dc9a2a705195
Your Name <wearypossum4770@yahoo.com>
2025-09-01 10:05:06 -0400

    feat(kt): implement Max Consecutive challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 3 ms     | 71.27 %  |
    | Memory    | 53.56 MB | 62.31 %  |
    
    related-to: #485

---
+++ src/main/kotlin/easy/KodeeFindMaxConsecutiveOnes.kt
@@ -0,0 +1,19 @@
+package easy;
+import kotlin.math.*
+
+fun kodeeFindMaxConsecutiveOnes(nums: IntArray): Int  {
+	var count = 0
+	var m = 0
+	for( n in nums) {
+		if( m < count) {
+			m = count
+		}
+		if (n == 1) {
+			count++
+		}else {
+			count = 0
+		}
+	}
+
+	return max(m, count)
+}
