8ac17ef0eb7d56506b7a77ab51df037aea0327dd
wearypossum4770 <50812322+wearypossum4770@users.noreply.github.com>
2025-09-01 15:00:32 +0000

    Dusting

--- src/main/php/Easy/FindMaxConsecutiveOnes.php
+++ src/main/php/Easy/FindMaxConsecutiveOnes.php
@@ -1,26 +1,32 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Src\Easy;
-class FindMaxConsecutiveOnes {
-public function transformFindMaxConsecutiveOnes(array $nums): int {
-	$count = 0;
-	$max = 0;
-	foreach ($nums as $_ => $value) {
-		if($max < $count) {
-			$max = $count;
-		}
-		if ($value === 1) {
-			$count++;
-		}else {
 
-			$count = 0;
-		}
-	}
-	return max($count, $max);
-}
-public function solve(array $nums): int
+class FindMaxConsecutiveOnes
 {
-return $this->transformFindMaxConsecutiveOnes($nums);
-}
+    public function transformFindMaxConsecutiveOnes(array $nums): int
+    {
+        $count = 0;
+        $max = 0;
+        foreach ($nums as $_ => $value) {
+            if ($max < $count) {
+                $max = $count;
+            }
+            if ($value === 1) {
+                $count++;
+            } else {
 
+                $count = 0;
+            }
+        }
+
+        return max($count, $max);
+    }
+
+    public function solve(array $nums): int
+    {
+        return $this->transformFindMaxConsecutiveOnes($nums);
+    }
 }
