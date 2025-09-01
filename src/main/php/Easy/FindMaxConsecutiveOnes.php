63457c37a5e996bd5052d3d6d512144fb93391d0
Your Name <wearypossum4770@yahoo.com>
2025-09-01 10:07:24 -0400

    feat(php): implement Max Consecutive challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 5 ms     | 84.62 %  |
    | Memory    | 25.74 MB | 25.64 %  |
    
    related-to: #485

---
+++ src/main/php/Easy/FindMaxConsecutiveOnes.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Src\Easy;
+class FindMaxConsecutiveOnes {
+public function transformFindMaxConsecutiveOnes(array $nums): int {
+	$count = 0;
+	$max = 0;
+	foreach ($nums as $_ => $value) {
+		if($max < $count) {
+			$max = $count;
+		}
+		if ($value === 1) {
+			$count++;
+		}else {
+
+			$count = 0;
+		}
+	}
+	return max($count, $max);
+}
+public function solve(array $nums): int
+{
+return $this->transformFindMaxConsecutiveOnes($nums);
+}
+
+}
