2d9c6fa8d7bdde68ea83f28066a86662b1758d7e
Your Name <wearypossum4770@yahoo.com>
2025-09-03 15:43:45 -0400

    test(rs): add test to challenge 03e50be
    relates-to: #1089

--- src/easy/remove_duplicate_zeros.rs
+++ src/easy/remove_duplicate_zeros.rs
@@ -13,3 +13,21 @@ pub fn remove_duplicate_zeros(arr: &mut Vec<i32>) {
         left += 1;
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::remove_duplicate_zeros;
+    use rstest::rstest;
+    #[rstest]
+    #[case(vec![1, 0, 2, 3, 0, 4, 5, 0 ], vec![ 1, 0, 0, 2, 3, 0, 0, 4 ])]
+    #[case(vec![1, 2, 3 ], vec![ 1, 2, 3 ])]
+    #[case(vec![0, 0, 0 ], vec![ 0, 0, 0 ])]
+    #[case(vec![0 ], vec![ 0 ])]
+    #[case(vec![7 ], vec![ 7 ])]
+    #[case(vec![1, 2, 0 ], vec![ 1, 2, 0 ])]
+    #[case(vec![0, 1, 0, 2, 0, 3 ], vec![ 0, 0, 1, 0, 0, 2 ])]
+    fn test_remove_duplicate_zeros(#[case] nums: Vec<i32>, #[case] expected: Vec<i32>) {
+        let mutation = &mut nums.clone();
+        remove_duplicate_zeros(mutation);
+        assert_eq!(mutation, &expected);
+    }
+}
