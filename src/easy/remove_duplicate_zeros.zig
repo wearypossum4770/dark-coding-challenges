1955aaea423c52f2b14fca2006da5f84142319ae
Your Name <wearypossum4770@yahoo.com>
2025-09-03 15:49:51 -0400

    test(zig): add test to challenge 844f888
    relates-to: #1089

--- src/easy/remove_duplicate_zeros.zig
+++ src/easy/remove_duplicate_zeros.zig
@@ -17,3 +17,52 @@ pub fn removeDuplicateZeros(comptime T: type, arr: []T) void {
         }
     }
 }
+
+test "remove duplicate zeros in place [1,0,2,3,0,4,5,0]" {
+    var nums = [_]i32{ 1, 0, 2, 3, 0, 4, 5, 0 };
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{ 1, 0, 0, 2, 3, 0, 0, 4 };
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
+
+test "remove duplicate zeros in place [1,2,3]" {
+    var nums = [_]i32{ 1, 2, 3 };
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{ 1, 2, 3 };
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
+
+test "remove duplicate zeros in place all zeros [0,0,0]" {
+    var nums = [_]i32{ 0, 0, 0 };
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{ 0, 0, 0 };
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
+
+test "remove duplicate zeros in place single element zero" {
+    var nums = [_]i32{0};
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{0};
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
+
+test "remove duplicate zeros in place single element nonzero" {
+    var nums = [_]i32{7};
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{7};
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
+
+test "remove duplicate zeros in place zero at end [1,2,0]" {
+    var nums = [_]i32{ 1, 2, 0 };
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{ 1, 2, 0 };
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
+
+test "remove duplicate zeros in place scattered zeros [0,1,0,2,0,3]" {
+    var nums = [_]i32{ 0, 1, 0, 2, 0, 3 };
+    removeDuplicateZeros(@TypeOf(nums[0]), &nums);
+    const expected = [_]i32{ 0, 0, 1, 0, 0, 2 };
+    try expectEqualSlices(@TypeOf(nums[0]), &expected, &nums);
+}
