62ebb07b7d6250417f3b7b71ba8c7fd448b5a12d
Your Name <wearypossum4770@yahoo.com>
2025-09-05 15:20:01 -0400

    test(rs): add test to challenge 4ad3d92
    relates-to: #1299

--- src/easy/replace_greatest_elements.rs
+++ src/easy/replace_greatest_elements.rs
@@ -12,3 +12,31 @@ pub fn replace_greatest_elements(arr: &mut Vec<i32>) -> Vec<i32> {
 
     arr.to_vec()
 }
+#[cfg(test)]
+mod tests {
+    use super::replace_greatest_elements;
+    use rstest::rstest;
+    #[rstest]
+    #[case("normal case", vec![17, 18, 5, 4, 6, 1], vec![18, 6, 6, 6, 1, -1])]
+    #[case("normal case", vec![400, 300, 200, 100], vec![300, 200, 100, -1])]
+    #[case("single element array", vec![400], vec![-1])]
+    #[case("single element array", vec![0], vec![-1])]
+    #[case("two elements array", vec![1, 2], vec![2, -1])]
+    #[case("two elements array", vec![2, 1], vec![1, -1])]
+    #[case("empty array", vec![], vec![])]
+    #[case("all elements equal", vec![5, 5, 5, 5], vec![5, 5, 5, -1])]
+    #[case("strictly increasing array", vec![1, 2, 3, 4, 5], vec![5, 5, 5, 5, -1])]
+    #[case("strictly decreasing array", vec![5, 4, 3, 2, 1], vec![4, 3, 2, 1, -1])]
+    #[case("array with zeros", vec![0, 0, 0, 0], vec![0, 0, 0, -1])]
+    #[case("array with zeros", vec![0, 1, 0, 1], vec![1, 1, 1, -1])]
+    #[case("with large numbers", vec![100000, 50000, 100001], vec![100001, 100001, -1])]
+    #[case("with large numbers", vec![1, 2147395600, 2], vec![2147395600, 2, -1])]
+    fn test_replace_greatest_elements(
+        #[case] _criteria: &str,
+        #[case] arr: Vec<i32>,
+        #[case] expected: Vec<i32>,
+    ) {
+        let num = &mut arr.clone();
+        assert_eq!(replace_greatest_elements(num), expected);
+    }
+}
