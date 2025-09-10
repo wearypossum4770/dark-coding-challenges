3a945942a3ace648499ca3a0a847022a7b3a0070
Your Name <wearypossum4770@yahoo.com>
2025-09-09 21:00:55 -0400

    test(swift): add test to challenge 837fd28
    relates-to: #1051

---
+++ test/easyswift/HeightCheckerTest.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import easy
+
+@Test(arguments: [
+  ([1, 1, 4, 2, 1, 3], 3),
+  ([5, 1, 2, 3, 4], 5),
+  ([1, 2, 3, 4, 5], 0),
+  ([10, 6, 6, 10, 10, 9, 8, 8, 3, 3, 8, 2, 1, 5, 1, 9, 5, 2, 7, 4, 7, 7], 22),
+])
+func heightCheckerTest(nums: [Int], outcome: Int) {
+  #expect(heightChecker(nums) == outcome)
+}
