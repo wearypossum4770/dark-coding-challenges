217f8c74f1499d2b98749e93950dfdd7f97938c4
Your Name <wearypossum4770@yahoo.com>
2025-09-09 20:29:50 -0400

    test(zig): add test to challenge 00e972e
    relates-to: #1051

--- src/easy/height_checker.zig
+++ src/easy/height_checker.zig
@@ -17,3 +17,40 @@ pub fn heightChecker(comptime T: type, allocator: Allocator, heights: []const T)
     }
     return count;
 }
+
+test "count the number of elements out of order [1, 1, 4, 2, 1, 3]" {
+    const nums = [_]i32{ 1, 1, 4, 2, 1, 3 };
+    const expected: usize = 3;
+    const allocator = std.testing.allocator;
+    const result = heightChecker(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "count the number of elements out of order [5, 1, 2, 3, 4]" {
+    const nums = [_]i32{ 5, 1, 2, 3, 4 };
+    const expected: usize = 5;
+    const allocator = std.testing.allocator;
+    const result = heightChecker(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "count the number of elements out of order [1, 2, 3, 4, 5]" {
+    const nums = [_]i32{ 1, 2, 3, 4, 5 };
+    const expected: usize = 0;
+    const allocator = std.testing.allocator;
+    const result = heightChecker(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "count the number of elements out of order [10, 6, 6, 10, 10, 9, 8, 8, 3, 3, 8, 2, 1, 5, 1, 9, 5, 2, 7, 4, 7, 7]" {
+    const nums = [_]i32{ 10, 6, 6, 10, 10, 9, 8, 8, 3, 3, 8, 2, 1, 5, 1, 9, 5, 2, 7, 4, 7, 7 };
+    const expected: usize = 22;
+    const allocator = std.testing.allocator;
+    const result = heightChecker(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
