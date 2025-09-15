a81ff794c182b6cebff4c17422e2a284821f4cd5
wearypossum4770 <50812322+wearypossum4770@users.noreply.github.com>
2025-09-15 23:02:01 +0000

    Dusting

--- src/main/php/Easy/AddTwoIntegers.php
+++ src/main/php/Easy/AddTwoIntegers.php
@@ -1,13 +1,18 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Src\Easy;
-class AddTwoIntegers {
-public function transformAddTwoIntegers(int $num1, int $num2): int {
-	return $num1 + $num2;
-}
-public function solve(int $a, int $b): int
-{
-return $this->transformAddTwoIntegers($a, $b);
-}
 
+class AddTwoIntegers
+{
+    public function transformAddTwoIntegers(int $num1, int $num2): int
+    {
+        return $num1 + $num2;
+    }
+
+    public function solve(int $a, int $b): int
+    {
+        return $this->transformAddTwoIntegers($a, $b);
+    }
 }
