565ead914b7007a8668d11db370017d1f66a69b1
Your Name <wearypossum4770@yahoo.com>
2025-09-15 22:53:28 -0400

    refactor(perf): remove if-else for 43f7a3d
    relates-to: #1480

--- lib/easy/running_array_sum.dart
+++ lib/easy/running_array_sum.dart
@@ -1,11 +1,9 @@
 List<int> runningArraySum(List<int> nums) {
-  List<int> result = List.filled(nums.length, 0);
-  for (int i = 0; i < nums.length; i++) {
-    if (i == 0) {
-      result[i] = nums[i];
-    } else {
-      result[i] = result[i - 1] + nums[i];
-    }
-  }
-  return result;
+	if( nums.isEmpty) return nums;
+	List<int> result = List.filled(nums.length, 0);
+	result[0] = nums[0];
+	for(int i = 1; i < nums.length; i++) {
+		result[i] = result[i - 1] + nums[i];
+	}
+return result;
 }
