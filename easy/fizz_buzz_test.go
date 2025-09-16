ce2e13f3ed8973b01efa41efada57f4b4af6f78d
Your Name <wearypossum4770@yahoo.com>
2025-09-16 17:44:18 -0400

    test(go): add test to challenge ac0e7ff
    relates-to: #412

--- easy/fizz_buzz_test.go
+++ easy/fizz_buzz_test.go
@@ -2,26 +2,26 @@ package easy
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
 func TestFizzBuzz(t *testing.T) {
 	cases := []struct {
-		candidate int
-		expected  []string
+		num      int
+		expected []string
 	}{
-		{5, []string{"1", "2", "Fizz", "4", "Buzz"}},
-		{3, []string{"1", "2", "Fizz"}},
-		{16, []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16"}},
-		{15, []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"}},
-		{35, []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz", "31", "32", "Fizz", "34", "Buzz"}},
+		{3, strings.Split("1,2,Fizz", ",")},
+		{5, strings.Split("1,2,Fizz,4,Buzz", ",")},
+		{10, strings.Split("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz", ",")},
+		{100, strings.Split("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32,Fizz,34,Buzz,Fizz,37,38,Fizz,Buzz,41,Fizz,43,44,FizzBuzz,46,47,Fizz,49,Buzz,Fizz,52,53,Fizz,Buzz,56,Fizz,58,59,FizzBuzz,61,62,Fizz,64,Buzz,Fizz,67,68,Fizz,Buzz,71,Fizz,73,74,FizzBuzz,76,77,Fizz,79,Buzz,Fizz,82,83,Fizz,Buzz,86,Fizz,88,89,FizzBuzz,91,92,Fizz,94,Buzz,Fizz,97,98,Fizz,Buzz", ",")},
+		{15, strings.Split("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz", ",")},
 	}
 	for _, c := range cases {
-		t.Run("Give any array of string representing as fizzbuzz", func(t *testing.T) {
-			t.Parallel()
-			result := FizzBuzz(c.candidate)
+		t.Run("", func(t *testing.T) {
+			result := FizzBuzz(c.num)
 			if !reflect.DeepEqual(result, c.expected) {
-				t.Errorf("FizzBuzz(%d) == %v, expected %v", c.candidate, result, c.expected)
+				t.Errorf("FizzBuzz(%d) == %v, expected %v", c.num, result, c.expected)
 			}
 
 		})
