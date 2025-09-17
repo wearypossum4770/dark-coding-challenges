f91fadc822eb7adfec4e5462d4366f495e139883
Your Name <wearypossum4770@yahoo.com>
2025-09-17 18:37:02 -0400

    test(zig): add test to challenge adaf656
    relates-to: #383

--- src/easy/can_construct_ransom_note.zig
+++ src/easy/can_construct_ransom_note.zig
@@ -21,3 +21,138 @@ pub fn canConstructRansomNote(allocator: Allocator, ransomNote: []const u8, maga
     }
     return true;
 }
+
+test "the ransomNote: a can be constructed from b" {
+    const ransomNote = "a";
+    const magazine = "b";
+    const expected = false;
+    const allocator = std.testing.allocator;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+
+test "the ransomNote: aa can be constructed from ab" {
+    const ransomNote = "aa";
+    const magazine = "ab";
+    const expected = false;
+    const allocator = std.testing.allocator;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+
+test "the ransomNote: aa can be constructed from aab" {
+    const ransomNote = "aa";
+    const magazine = "aab";
+    const expected = true;
+    const allocator = std.testing.allocator;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+
+test "the ransomNote: a can be constructed from a" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "a";
+    const magazine = "a";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+
+test "the ransomNote: abc can be constructed from cbad" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "abc";
+    const magazine = "cbad";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: '' can be constructed from anything" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "";
+    const magazine = "anything";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: a can be constructed from " {
+    const allocator = std.testing.allocator;
+    const ransomNote = "a";
+    const magazine = "";
+    const expected = false;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: '' can be constructed from ''" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "";
+    const magazine = "";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: aA can be constructed from aa" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "aA";
+    const magazine = "aa";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: Aa can be constructed from aA" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "Aa";
+    const magazine = "aA";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: zzz can be constructed from zzzzzz" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "zzz";
+    const magazine = "zzzzzz";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: zzzz can be constructed from zzz" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "zzzz";
+    const magazine = "zzz";
+    const expected = false;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "the ransomNote: hello can be constructed from ld" {
+    const allocator = std.testing.allocator;
+    const ransomNote = "hello world";
+    const magazine = "dlrow olleh";
+    const expected = true;
+    const result = canConstructRansomNote(allocator, ransomNote, magazine);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
