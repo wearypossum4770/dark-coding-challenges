9dc80d5e2c187b8615a28a8198e75795b50962ea
Your Name <wearypossum4770@yahoo.com>
2025-09-17 15:09:36 -0400

    test(zig): add test to challenge 2d3d49b
    relates-to: #1342

--- src/easy/number_of_steps_to_zero.zig
+++ src/easy/number_of_steps_to_zero.zig
@@ -16,3 +16,59 @@ pub fn numberOfStepsToZero(num: i32) usize {
     }
     return steps;
 }
+
+test "number of steps to reduce 14" {
+    const num: i32 = 14;
+    const expected: usize = 6;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce 8" {
+    const num: i32 = 8;
+    const expected: usize = 4;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce 123" {
+    const num: i32 = 123;
+    const expected: usize = 12;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce 0" {
+    const num: i32 = 0;
+    const expected: usize = 0;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce 1" {
+    const num: i32 = 1;
+    const expected: usize = 1;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce 1000000" {
+    const num: i32 = 1_000_000;
+    const expected: usize = 26;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce negative input" {
+    const num: i32 = -1;
+    const expected: usize = 0;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
+
+test "number of steps to reduce 999999" {
+    const num: i32 = 999_999;
+    const expected: usize = 31;
+    const actual = numberOfStepsToZero(num);
+    try expectEqual(expected, actual);
+}
