6dc830d32c12cfc016b1c51660cce27a23885e7a
Your Name <wearypossum4770@yahoo.com>
2025-09-17 15:08:05 -0400

    feat(go): implement Steps to Reduce Number to Zero challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 3.92 MB  | 27.47 %  |
    
    relates-to: #1342

---
+++ easy/number_of_steps_to_zero.go
@@ -0,0 +1,14 @@
+package easy
+
+func NumberOfStepsToZero(num int) int {
+	steps := 0
+	for num > 0 {
+		if num%2 == 0 {
+			num /= 2
+		} else {
+			num -= 1
+		}
+		steps += 1
+	}
+	return steps
+}
