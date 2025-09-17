e1e1c694c7d112744ca252f69621940785649794
Your Name <wearypossum4770@yahoo.com>
2025-09-17 15:15:05 -0400

    feat(ts): implement Steps to Reduce Number to Zero challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 55.11 MB | 67.51 %  |
    
    relates-to: #1342

---
+++ src/easy/number-of-steps-to-zero.ts
@@ -0,0 +1,13 @@
+export const numberOfStepsToZero = (num: number): number => {
+    let steps = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num--;
+        }
+        steps++;
+    }
+
+    return steps;
+};
