39acd1671c82826a2b9ef308a3712ed58a4ec3b0
Your Name <wearypossum4770@yahoo.com>
2025-09-17 15:15:42 -0400

    feat(py): implement Steps to Reduce Number to Zero challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 17.66 MB | 87.97 %  |
    
    relates-to: #1342

---
+++ easy/number_of_steps_to_zero.py
@@ -0,0 +1,9 @@
+def number_of_steps_to_zero(num: int) -> int:
+    steps = 0
+    while num > 0:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num -= 1
+        steps += 1
+    return steps
