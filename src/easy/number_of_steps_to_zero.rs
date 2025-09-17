374ecd98113cd8dabd83008aa9a4f41ea94e4e21
Your Name <wearypossum4770@yahoo.com>
2025-09-17 15:07:52 -0400

    test(rs): add test to challenge b71ed7a
    relates-to: #1342

--- src/easy/number_of_steps_to_zero.rs
+++ src/easy/number_of_steps_to_zero.rs
@@ -14,4 +14,21 @@ pub fn number_of_steps_to_zero(num: i32) -> i32 {
     }
     steps
 }
-	
+#[cfg(test)]
+mod tests {
+    use super::number_of_steps_to_zero;
+    use rstest::rstest;
+    #[rstest]
+    #[case(2, 2)]
+    #[case(0, 0)]
+    #[case(1, 1)]
+    #[case(8, 4)]
+    #[case(14, 6)]
+    #[case(123, 12)]
+    #[case(1_000_000, 26)]
+    #[case(-1, 0)]
+    #[case(999_999, 31)]
+    fn test_number_of_steps_to_zero(#[case] n: i32, #[case] expected: i32) {
+        assert_eq!(number_of_steps_to_zero(n), expected);
+    }
+}
