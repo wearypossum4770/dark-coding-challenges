cd92e59e55180ce28ef7c716e18f165f7b002ea3
Your Name <wearypossum4770@yahoo.com>
2025-09-16 20:47:21 -0400

    chore(format): format and lint 175c814

--- lib/easy/fizz_buzz.dart
+++ lib/easy/fizz_buzz.dart
@@ -1,14 +1,14 @@
 String intToFizzBuzz(int n) {
-	if (n % 15 == 0) return "FizzBuzz";
-	if (n % 5 == 0) return "Buzz";
-	if (n % 3 == 0) return "Fizz";
-	return n.toString();
+  if (n % 15 == 0) return "FizzBuzz";
+  if (n % 5 == 0) return "Buzz";
+  if (n % 3 == 0) return "Fizz";
+  return n.toString();
 }
 
 List<String> fizzBuzz(int n) {
-	List<String> result = List.filled(n, "");
-	for(int i = 0; i < n; i++) {
-		result[i] = intToFizzBuzz(i + 1);
-	}
-	return result;
+  List<String> result = List.filled(n, "");
+  for (int i = 0; i < n; i++) {
+    result[i] = intToFizzBuzz(i + 1);
+  }
+  return result;
 }
