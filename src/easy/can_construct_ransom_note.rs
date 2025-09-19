ee496e96ec2d2b366aefaa6b7e687c17033090db
Your Name <wearypossum4770@yahoo.com>
2025-09-19 10:50:46 -0400

    test(rs): add test to challenge df91457
    relates-to: #383

--- src/easy/can_construct_ransom_note.rs
+++ src/easy/can_construct_ransom_note.rs
@@ -18,3 +18,29 @@ pub fn can_construct_ransom_note(ransom_note: String, magazine: String) -> bool 
     true
 }
 
+#[cfg(test)]
+mod tests {
+    use super::can_construct_ransom_note;
+    use rstest::rstest;
+    #[rstest]
+    #[case("a", "b", false)]
+    #[case("aa", "aab", true)]
+    #[case("a", "a", true)]
+    #[case("aa", "ab", false)]
+    #[case("abc", "cbad", true)]
+    #[case("", "anything", true)]
+    #[case("a", "", false)]
+    #[case("", "", true)]
+    #[case("aA", "aa", true)]
+    #[case("Aa", "aA", true)]
+    #[case("zzz", "zzzzzz", true)]
+    #[case("zzzz", "zzz", false)]
+    #[case("hello world", "dlrowolleh", true)]
+    fn test_can_construct_ransom_note(
+        #[case] note: String,
+        #[case] magazine: String,
+        #[case] expected: bool,
+    ) {
+        assert_eq!(can_construct_ransom_note(note, magazine), expected);
+    }
+}
