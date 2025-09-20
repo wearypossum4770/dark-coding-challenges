dc1d85111ca437c7f6daa07845cc8bcdea195a77
Your Name <wearypossum4770@yahoo.com>
2025-09-19 22:18:03 -0400

    feat(py): implement Is Subsequence challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 3 ms     | 24.99 %  |
    | Memory    | 17.51 MB | 98.94 %  |
    
    relates-to: #392

---
+++ easy/string_is_subsequence.py
@@ -0,0 +1,8 @@
+def string_is_subsequence(s: str, t: str) -> bool:
+    left, right = 0, 0
+    while left < len(s) and right < len(t):
+        if s[left] == t[right]:
+            left += 1
+        right += 1
+    return left == len(s)
+
