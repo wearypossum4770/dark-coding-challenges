38019668240724c38564e15acd0f8ddcefd15527
Your Name <wearypossum4770@yahoo.com>
2025-09-22 20:44:21 -0400

    chore(swift): cleanup String Is Subsequence challenge 6ffcb4a
    relates-to: #392

--- src/easy/StringIsSubsequence.swift
+++ src/easy/StringIsSubsequence.swift
@@ -1,13 +1,13 @@
 func stringIsSubsequence(_ s: String, _ t: String) -> Bool {
-      var i = s.startIndex
-        var j = t.startIndex
-        
-        while i < s.endIndex && j < t.endIndex {
-            if s[i] == t[j] {
-                i = s.index(after: i)
-            }
-            j = t.index(after: j)
-        }
-        
-        return i == s.endIndex
+  var i = s.startIndex
+  var j = t.startIndex
+
+  while i < s.endIndex && j < t.endIndex {
+    if s[i] == t[j] {
+      i = s.index(after: i)
+    }
+    j = t.index(after: j)
+  }
+
+  return i == s.endIndex
 }
