75270d9b180ae8b788e789ca0b8f7f055a0cecfe
Your Name <wearypossum4770@yahoo.com>
2025-09-22 20:44:25 -0400

    chore(kt): cleanup Kodee Search Insert Position challenge 957e9c9
    relates-to: #35

--- src/main/kotlin/easy/KodeeSearchInsertPosition.kt
+++ src/main/kotlin/easy/KodeeSearchInsertPosition.kt
@@ -6,15 +6,15 @@ fun kodeeSearchInsertPosition(
 ): Int {
     var left = 0
     var right = nums.size - 1
-		while(left <= right) {
-			val mid = left + (right - left) / 2
-			if (nums[mid] == target) return mid
-			if (nums[mid] < target) {
-				left = mid + 1
-			}else {
-				right = mid - 1
-			}
-		}
+    while (left <= right) {
+        val mid = left + (right - left) / 2
+        if (nums[mid] == target) return mid
+        if (nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
 
     return left
 }
