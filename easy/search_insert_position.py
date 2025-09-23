7b403aeceafef192c8660057d11ba963ca52ca0b
Your Name <wearypossum4770@yahoo.com>
2025-09-22 20:44:19 -0400

    chore(py): cleanup Search Insert Position challenge d769e8c
    relates-to: #35

--- easy/search_insert_position.py
+++ easy/search_insert_position.py
@@ -1,11 +1,11 @@
 def search_insert_position(nums: list[int], target: int) -> int:
-left, right = 0, len(nums) - 1
-while left <= right:
-mid = left + (right - left) // 2
-if nums[mid] == target:
-return mid
-if nums[mid] < target:
-left = mid + 1
-else:
-right = mid -1
-return left
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
