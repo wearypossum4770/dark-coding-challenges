495982b1964613de004b7cff7634a2e072059c95
Your Name <wearypossum4770@yahoo.com>
2025-09-24 10:55:27 -0400

    feat(java): implement Power of 3 challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 8 ms     | 81.44 %  |
    | Memory    | 44.01 MB | 68.38 %  |
    
    relates-to: #326

---
+++ src/main/java/easy/IsPowerOfThree.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class IsPowerOfThree {
+  public boolean solve(int n) {
+    if (n < 1) return false;
+    while (n % 3 == 0) {
+      n /= 3;
+    }
+    return n == 1;
+  }
+}
