25e3c33691a9c8345434bd50233a37618918a281
Your Name <wearypossum4770@yahoo.com>
2025-09-25 18:19:41 -0400

    feat(kt): implement Delete Linked List Node challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 130 ms   | 15.25 %  |
    | Memory    | 43.82 MB | 7.63 %   |
    
    relates-to: #237

---
+++ src/main/kotlin/medium/KodeeDeleteLinkedListNode.kt
@@ -0,0 +1,9 @@
+package medium
+import easy.KodeeListNode
+
+fun <T>kodeeDeleteLinkedListNode(node: KodeeListNode<T>?)  {
+  if (node == null || node.next == null) return
+    node.`val` = node.next?.`val`
+    node.next = node.next?.next
+
+}
