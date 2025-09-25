d55f116e8c46318d5c3c324583b4d9e9a04637fb
Your Name <wearypossum4770@yahoo.com>
2025-09-25 18:22:12 -0400

    feat(java): implement Delete Linked List Node challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 44.31 MB | 18.38 %  |
    
    relates-to: #237

---
+++ src/main/java/medium/DeleteLinkedListNode.java
@@ -0,0 +1,9 @@
+package medium;
+
+import easy.DukeListNode;
+
+public class DeleteLinkedListNode {
+public void solve(DukeListNode node) {
+	node.val = node.next.val;
+	node.next = node.next.next;
+}}
