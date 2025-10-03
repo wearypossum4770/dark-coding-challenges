4639dfb6dde70b96b5de8c08a5b0b6646ab7b5b8
Your Name <wearypossum4770@yahoo.com>
2025-10-03 07:59:03 -0400

    feat(go): improve challenge 10b936d solution
    similar-to: #2469

--- very_easy/temperature_converter.go
+++ very_easy/temperature_converter.go
@@ -1,7 +1,10 @@
 package very_easy
 
 import (
+	"fmt"
 	"math"
+	"strconv"
+	"strings"
 )
 
 func ToKelvin(celsius float64) float64 {
@@ -13,7 +16,27 @@ func ToFahrenheit(celsius float64) float64 {
 func ToCelsius(fahrenheit float64) float64 {
 	return (fahrenheit - 32.0) * 5 / 9
 }
+func TemperatureConverter(deg string) string {
+	var sb strings.Builder
+	for _, c := range deg {
+		if (c >= '0' && c <= '9') || c == '-' {
+			sb.WriteRune(c)
+		}
+	}
+	temp, err := strconv.Atoi(sb.String())
+	if err != nil {
+		return "Error"
+	}
+	if strings.HasSuffix(deg, "°C") {
+		return fmt.Sprintf("%d°F", int(math.Round(ToFahrenheit(float64(temp)))))
+	}
+	if strings.HasSuffix(deg, "°F") {
+		return fmt.Sprintf("%d°C", int(math.Round(ToCelsius(float64(temp)))))
 
+	}
+
+	return "Error"
+}
 func ConvertTemperature(celsius float64) []float64 {
 	return []float64{
 		math.Round(ToKelvin(celsius)),
