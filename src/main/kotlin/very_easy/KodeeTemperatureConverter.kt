4372bcf88f6c887429d5f23c45e7cae8bd862296
Your Name <wearypossum4770@yahoo.com>
2025-10-03 08:15:40 -0400

    feat(kt): improve challenge 117884d solution
    similar-to: #2469

--- src/main/kotlin/very_easy/KodeeTemperatureConverter.kt
+++ src/main/kotlin/very_easy/KodeeTemperatureConverter.kt
@@ -7,4 +7,23 @@ fun kodeeToFahrenheit(temperature: Double): Double = (temperature * 1.8) + 32.0
 
 fun kodeeToKelvin(temperature: Double): Double = temperature + 273.15
 
+    fun kodeeTemperatureConverter(deg: String): String {
+        val temp =
+            buildString {
+                for (i in deg) {
+                    if (i in '0'..'9' || i == '-' || i == '.') {
+                        append(i)
+                    } else {
+                        break
+                    }
+                }
+            }.toDoubleOrNull() ?: return "Error"
+
+        return when {
+            deg.endsWith("°F") -> "${kodeeToCelsius(temp).roundToInt().toInt()}°C"
+            deg.endsWith("°C") -> "${kodeeToFahrenheit(temp).roundToInt().toInt()}°F"
+            else -> "Error"
+        }
+    }
+
 fun kodeeConvertTemperature(celsius: Double): DoubleArray = doubleArrayOf(kodeeToKelvin(celsius), kodeeToFahrenheit(celsius))
