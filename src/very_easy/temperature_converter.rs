f90c08d51579adb0db515dc0b8dcf659e96a7823
Your Name <wearypossum4770@yahoo.com>
2025-10-03 07:51:27 -0400

    feat(rs): improve solution 55342e6
    similar-to: #2469

--- src/very_easy/temperature_converter.rs
+++ src/very_easy/temperature_converter.rs
@@ -10,6 +10,18 @@ pub fn to_kelvin(celsius: f64) -> f64 {
 pub fn to_celsius(fahrenheit: f64) -> f64 {
 	(fahrenheit - 32.0) *( 5.0 / 9.0)
 }
+pub fn temperature_converter(deg: String) -> String {
+  let temperature = deg.chars().filter(|&t| t.is_ascii_digit() || t == '.' || t == '-').collect::<String>().parse::<f64>().unwrap_or(0.0);
+	if deg.ends_with("°C") {
+		return format!("{}°F", to_fahrenheit(temperature).round() as i32);
+	}
+	else if deg.ends_with("°F") {
+		return format!("{}°C", to_celsius(temperature).round() as i32);
+		}else {
+			return "Error".to_string();
+		}
+
+}
 
 pub fn convert_temperature(celsius: f64) -> Vec<f64> {
 	Vec::from([
