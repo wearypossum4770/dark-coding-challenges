6f43393401b5b0b0276645acf049506ffc75be77
Your Name <wearypossum4770@yahoo.com>
2025-10-03 07:57:14 -0400

    feat(swift): improve challenge 22ab77e solution
     # 📊 Metrics
    
    similar-to: #2469

--- src/very_easy/TemperatureConverter.swift
+++ src/very_easy/TemperatureConverter.swift
@@ -8,6 +8,28 @@ func toFahrenheit(_ celsius: Double) -> Double {
   return celsius * 1.8 + 32
 }
 
+func temperatureConverter(_ deg: String) -> String {
+  var temp = ""
+  for ch in deg {
+    switch ch {
+      case "0"..."9", "-", ".":
+        temp.append(ch)
+      default:
+        break
+    }
+  }
+  guard let temperature = Double(temp) else {
+    return "Error"
+  }
+  if deg.hasSuffix("°F") {
+    return String(format: "%.0f°C", toCelsius(temperature))
+  } else if deg.hasSuffix("°C") {
+
+    return String(format: "%.0f°F", toFahrenheit(temperature))
+  }
+  return "Error"
+}
+
 func convertTemperature(_ celsius: Double) -> [Double] {
   return [
     toKelvin(celsius),
