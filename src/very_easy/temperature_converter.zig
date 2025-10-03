7e3dfdb8de70e73e6370b09cd468ab8aa0dba261
Your Name <wearypossum4770@yahoo.com>
2025-10-03 08:25:46 -0400

    feat(zig): improve challenge 46c965a solution
    relates-to: #2469

--- src/very_easy/temperature_converter.zig
+++ src/very_easy/temperature_converter.zig
@@ -24,4 +24,27 @@ pub fn convertTemperature(celsius: f64) []f64 {
     return &[_]f64{ kelvin, fahrenheit };
 }
 
+pub fn temperatureConverter(allocator: Allocator, deg: []const u8) ?[]const u8 {
+    const buf = allocator.alloc(u8, 64) catch return null;
+    var index: usize = 0;
+    for (deg) |char| {
+        switch (char) {
+            '0'...'9' | '-' => index += 1,
+            else => break,
+        }
+    }
+    const temp = std.fmt.parseFloat(f32, deg[0..index]) catch {
+        return allocator.dupe(u8, "Error") catch return null;
+    };
+    if (std.mem.endsWith(u8, deg, "°F")) {
+        const celsius = @round((temp - 32.0) * 5.0 / 9.0);
+        return bufPrint(buf, "{d:.0}°C", .{celsius}) catch return null;
+    }
+    if (std.mem.endsWith(u8, deg, "°C")) {
+        const fahrenheit = @round((temp * 9.0 / 5.0) + 32.0);
+        return bufPrint(buf, "{d:.0}°F", .{fahrenheit}) catch return null;
+    }
+    return allocator.dupe(u8, "Error") catch return null;
+}
+
 const tolerance: f64 = 1e-2;
