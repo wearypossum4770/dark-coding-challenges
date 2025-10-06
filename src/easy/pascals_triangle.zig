87ad61a3f169aab8208cfa2a7f7e5def020c7083
Your Name <wearypossum4770@yahoo.com>
2025-10-06 19:58:02 -0400

    test(zig) to challenge d1e534e
    relates-to: #118

--- src/easy/pascals_triangle.zig
+++ src/easy/pascals_triangle.zig
@@ -21,3 +21,98 @@ pub fn pascalsTriangle(comptime T: type, allocator: Allocator, numRows: i32) ?[]
     }
     return result;
 }
+
+test "creating a pascal triangle of size 1" {
+    const numRows: i32 = 1;
+    const expectedMatrix = [_][]const i32{
+        &[_]i32{1},
+    };
+    const allocator = std.testing.allocator;
+    const matrix = pascalsTriangle(@TypeOf(expectedMatrix[0][0]), allocator, numRows);
+    if (matrix) |result| {
+        for (result, 0..) |actual, i| {
+            const expected = expectedMatrix[i];
+            defer allocator.free(actual);
+            try expectEqualSlices(@TypeOf(expected[i]), expected, actual);
+        }
+        defer allocator.free(result);
+    } else return error.SkipZigTest;
+}
+test "creating a pascal triangle of size 2" {
+    const numRows: i32 = 2;
+    const expectedMatrix = [_][]const i32{ &[_]i32{1}, &[_]i32{ 1, 1 } };
+    const allocator = std.testing.allocator;
+    const matrix = pascalsTriangle(@TypeOf(expectedMatrix[0][0]), allocator, numRows);
+    if (matrix) |result| {
+        for (result, 0..) |actual, i| {
+            const expected = expectedMatrix[i];
+            defer allocator.free(actual);
+            try expectEqualSlices(@TypeOf(expected[i]), expected, actual);
+        }
+        defer allocator.free(result);
+    } else return error.SkipZigTest;
+}
+test "creating a pascal triangle of size 3" {
+    const numRows: i32 = 3;
+    const expectedMatrix = [_][]const i32{ &[_]i32{1}, &[_]i32{ 1, 1 }, &[_]i32{ 1, 2, 1 } };
+    const allocator = std.testing.allocator;
+    const matrix = pascalsTriangle(@TypeOf(expectedMatrix[0][0]), allocator, numRows);
+    if (matrix) |result| {
+        for (result, 0..) |actual, i| {
+            const expected = expectedMatrix[i];
+            defer allocator.free(actual);
+            try expectEqualSlices(@TypeOf(expected[i]), expected, actual);
+        }
+        defer allocator.free(result);
+    } else return error.SkipZigTest;
+}
+test "creating a pascal triangle of size 4" {
+    const numRows: i32 = 4;
+    const expectedMatrix = [_][]const i32{ &[_]i32{1}, &[_]i32{ 1, 1 }, &[_]i32{ 1, 2, 1 }, &[_]i32{ 1, 3, 3, 1 } };
+    const allocator = std.testing.allocator;
+    const matrix = pascalsTriangle(@TypeOf(expectedMatrix[0][0]), allocator, numRows);
+    if (matrix) |result| {
+        for (result, 0..) |actual, i| {
+            const expected = expectedMatrix[i];
+            defer allocator.free(actual);
+            try expectEqualSlices(@TypeOf(expected[i]), expected, actual);
+        }
+        defer allocator.free(result);
+    } else return error.SkipZigTest;
+}
+
+test "creating a pascal triangle of size 5" {
+    const numRows: i32 = 5;
+    const expectedMatrix = [_][]const i32{
+        &[_]i32{1},
+        &[_]i32{ 1, 1 },
+        &[_]i32{ 1, 2, 1 },
+        &[_]i32{ 1, 3, 3, 1 },
+        &[_]i32{ 1, 4, 6, 4, 1 },
+    };
+    const allocator = std.testing.allocator;
+    const matrix = pascalsTriangle(@TypeOf(expectedMatrix[0][0]), allocator, numRows);
+    if (matrix) |result| {
+        for (result, 0..) |actual, i| {
+            const expected = expectedMatrix[i];
+            defer allocator.free(actual);
+            try expectEqualSlices(@TypeOf(expected[i]), expected, actual);
+        }
+        defer allocator.free(result);
+    } else return error.SkipZigTest;
+}
+
+test "creating a pascal triangle of size 6" {
+    const numRows: i32 = 6;
+    const expectedMatrix = [_][]const i32{ &[_]i32{1}, &[_]i32{ 1, 1 }, &[_]i32{ 1, 2, 1 }, &[_]i32{ 1, 3, 3, 1 }, &[_]i32{ 1, 4, 6, 4, 1 }, &[_]i32{ 1, 5, 10, 10, 5, 1 } };
+    const allocator = std.testing.allocator;
+    const matrix = pascalsTriangle(@TypeOf(expectedMatrix[0][0]), allocator, numRows);
+    if (matrix) |result| {
+        for (result, 0..) |actual, i| {
+            const expected = expectedMatrix[i];
+            defer allocator.free(actual);
+            try expectEqualSlices(@TypeOf(expected[i]), expected, actual);
+        }
+        defer allocator.free(result);
+    } else return error.SkipZigTest;
+}
