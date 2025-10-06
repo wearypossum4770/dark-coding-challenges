4efeb4ecf59b06d8f86b5539e3f67c5e8e4b2668
Your Name <wearypossum4770@yahoo.com>
2025-10-06 16:27:31 -0400

    feat(ts): implement Pascal's Triangle challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 56.17 MB | 78.84 %  |
    
    relates-to: #118

---
+++ src/easy/pascals-triangle.ts
@@ -0,0 +1,13 @@
+const allocateArray = (length: number): number[] =>  Array.from({ length }, () => 1);
+const createMatrix = (length: number): number[][] => Array.from({length}, (_, i) => allocateArray(i+ 1)) 
+
+export const pascalsTriangle = (numRows: number): number[][] => {
+ const result = createMatrix(numRows);
+    if (numRows < 0) return [];
+    for (let i = 1; i < numRows; i++) {
+        for (let j = 1; j < i; j++) {
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+    }
+    return result;
+};
