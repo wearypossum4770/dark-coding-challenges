a3ee86762aec989ef1818ce3f2d1038ef4304061
Your Name <wearypossum4770@yahoo.com>
2025-10-06 15:49:32 -0400

    feat(dart): implement Pascal's Triangle challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 148.83 MB| 48.57 %  |
    
    relates-to: #118

---
+++ lib/easy/pascals_triangle.dart
@@ -0,0 +1,10 @@
+List<List<int>> pascalsTriangle(int numRows) {
+  if (numRows < 0) return [];
+  List<List<int>> result = List.generate(numRows, (i) => List.filled(i + 1, 1));
+  for (int i = 2; i < numRows; i++) {
+    for (int j = 1; j < i; j++) {
+      result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+    }
+  }
+  return result;
+}
