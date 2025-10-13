e3c3e974cb2c41de360d669e98b34c8d948b9760
Your Name <wearypossum4770@yahoo.com>
2025-10-13 19:32:08 -0400

    test(rs) challenge 87982ed
    relates-to: #3340

--- src/easy/string_integer_is_balanced.rs
+++ src/easy/string_integer_is_balanced.rs
@@ -13,3 +13,27 @@ pub fn string_integer_is_balanced(num: String) -> bool {
 
     even == odd
 }
+#[cfg(test)]
+mod tests {
+    use super::string_integer_is_balanced;
+    use rstest::rstest;
+    #[rstest]
+    #[case("12", false)]
+    #[case("11", true)]
+    #[case("1234", false)]
+    #[case("24123", true)]
+    #[case("00", true)]
+    #[case("10", false)]
+    #[case("1010", false)]
+    #[case("909", false)]
+    #[case("1234567890", false)]
+    #[case("111111", true)]
+    #[case("123321", true)]
+    #[case("99999999", true)]
+    #[case("5005", true)]
+    #[case("1234567", false)]
+    #[case("1", false)]
+    fn test_string_integer_is_balanced(#[case] num: &str, #[case] expected: bool) {
+        assert_eq!(string_integer_is_balanced(num.to_string()), expected);
+    }
+}
