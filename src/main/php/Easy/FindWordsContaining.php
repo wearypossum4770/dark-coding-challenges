5d841ec116781231183f92c1da58d4d671bd0583
wearypossum4770 <50812322+wearypossum4770@users.noreply.github.com>
2025-10-15 12:31:16 +0000

    Dusting

--- src/main/php/Easy/FindWordsContaining.php
+++ src/main/php/Easy/FindWordsContaining.php
@@ -1,20 +1,26 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Src\Easy;
-class FindWordsContaining {
-public function transformFindWordsContaining(array $words, string $x): array {
-	$result = [];
-	for($i= 0; $i < count($words); $i++) {
-				if(str_contains($words[$i], $x)) {
-			$result[] = $i;
-		}
 
-	}
-	return $result;
-}
-public function solve(array $words, string $x): array
+class FindWordsContaining
 {
-return $this->transformFindWordsContaining($words, $x);
-}
+    public function transformFindWordsContaining(array $words, string $x): array
+    {
+        $result = [];
+        for ($i = 0; $i < count($words); $i++) {
+            if (str_contains($words[$i], $x)) {
+                $result[] = $i;
+            }
 
+        }
+
+        return $result;
+    }
+
+    public function solve(array $words, string $x): array
+    {
+        return $this->transformFindWordsContaining($words, $x);
+    }
 }
