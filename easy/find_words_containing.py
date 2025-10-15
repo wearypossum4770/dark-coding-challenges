5d711f31e157395a0ea32a41fceaea1c1769a394
Your Name <wearypossum4770@yahoo.com>
2025-10-15 08:50:31 -0400

    refactor(py) fix missing variable 607f81e
    relates-to: #2942

--- easy/find_words_containing.py
+++ easy/find_words_containing.py
@@ -1,2 +1,2 @@
 def find_words_containing(words: list[str], x: str) -> list[int]:
-    return [index for index, _ in enumerate(words) if x in word]
+    return [index for index, word in enumerate(words) if x in word]
