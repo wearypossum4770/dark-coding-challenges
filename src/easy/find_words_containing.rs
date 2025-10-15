d627ff6480ac44d25f402f31cee1b81322b499be
Your Name <wearypossum4770@yahoo.com>
2025-10-15 08:33:48 -0400

    test(rs) to challenge dd1c3ad
    relates-to: #2942

--- src/easy/find_words_containing.rs
+++ src/easy/find_words_containing.rs
@@ -6,3 +6,20 @@ pub fn find_words_containing(words: Vec<String>, c: char) -> Vec<i32> {
         .map(|(i, _)| i as i32)
         .collect()
 }
+#[cfg(test)]
+mod tests {
+    use super::find_words_containing;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&["abc", "bcd", "aaaa", "cbc"], 'a', &[0, 2])]
+    #[case(&["leet", "code"], 'e', &[0, 1])]
+    #[case(&["abc", "bcd", "aaaa", "cbc"], 'z', &[])]
+    fn test_find_words_containing(#[case] arr: &[&str], #[case] x: char, #[case] expected: &[i32]) {
+        let words = arr
+            .iter()
+            .map(|word| word.to_string())
+            .collect::<Vec<String>>();
+
+        assert_eq!(find_words_containing(words, x), expected.to_vec());
+    }
+}
