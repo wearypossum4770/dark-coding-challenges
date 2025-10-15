fb81a54e2199adc9087f465715548c3e7d661249
Your Name <wearypossum4770@yahoo.com>
2025-10-15 08:18:00 -0400

    feat(php): Words Containing Character challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 20.41 MB | 66.67 %  |
    
    relates-to: #2942

---
+++ src/main/php/Easy/FindWordsContaining.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Src\Easy;
+class FindWordsContaining {
+public function transformFindWordsContaining(array $words, string $x): array {
+	$result = [];
+	for($i= 0; $i < count($words); $i++) {
+				if(str_contains($words[$i], $x)) {
+			$result[] = $i;
+		}
+
+	}
+	return $result;
+}
+public function solve(array $words, string $x): array
+{
+return $this->transformFindWordsContaining($words, $x);
+}
+
+}
