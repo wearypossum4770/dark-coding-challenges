8c7221e8dd8f16d2e2bc5106cb0b1fbe4733e53e
Your Name <wearypossum4770@yahoo.com>
2025-10-15 20:44:33 -0400

    feat(py): Hello World challenge solution
    relates-to: #2667

---
+++ src/main/java/very_easy/CreateHelloWorld.java
@@ -0,0 +1,8 @@
+package very_easy;
+
+import java.util.function.Function;
+
+public class CreateHelloWorld {
+public Function<Object[], String> createHelloWorld() {
+return args -> "Hello World";
+}}
