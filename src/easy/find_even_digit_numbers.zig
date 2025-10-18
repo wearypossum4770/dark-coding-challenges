35611e80cbdf538afca184c6e470b26a1e56d7a4
Your Name <wearypossum4770@yahoo.com>
2025-10-18 11:00:56 -0400

    test(zig) to challenge a74df9f
    relates-to: #1295

--- src/easy/find_even_digit_numbers.zig
+++ src/easy/find_even_digit_numbers.zig
@@ -16,3 +16,16 @@ pub fn findEvenDigitNumbers(comptime T: type, nums: []const T) usize {
     }
     return result;
 }
+
+test "tally the even length numbers in [12,345,2,6,7896]" {
+    const nums = [_]i32{ 12, 345, 2, 6, 7896 };
+    const expected: usize = 2;
+    const actual = findEvenDigitNumbers(@TypeOf(nums[0]), &nums);
+    try expectEqual(expected, actual);
+}
+test "tally the even length numbers in [555,901,482,1771]" {
+    const nums = [_]i32{ 555, 901, 482, 1771 };
+    const expected: usize = 1;
+    const actual = findEvenDigitNumbers(@TypeOf(nums[0]), &nums);
+    try expectEqual(expected, actual);
+}
