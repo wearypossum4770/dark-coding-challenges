8b8316f500c749aebe47bc32679805fb5bf523df
Your Name <wearypossum4770@yahoo.com>
2025-10-18 10:55:49 -0400

    test(rs) to challenge 9794f8
    relates-to: #1295

--- src/easy/find_even_digit_numbers.rs
+++ src/easy/find_even_digit_numbers.rs
@@ -3,3 +3,14 @@ pub fn find_even_digit_numbers(nums: Vec<i32>) -> i32 {
         .filter(|&num| num.to_string().len() % 2 == 0)
         .count() as i32
 }
+#[cfg(test)]
+mod tests {
+    use super::find_even_digit_numbers;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[12,345,2,6,7896], 2)]
+    #[case(&[555,901,482,1771], 1)]
+    fn test_find_even_digit_numbers(#[case] nums: &[i32], #[case] expected: i32) {
+        assert_eq!(find_even_digit_numbers(nums.to_vec()), expected);
+    }
+}
