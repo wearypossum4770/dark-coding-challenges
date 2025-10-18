4f7e9afd23e1568bbebaef6e8add5437ef84b44b
Your Name <wearypossum4770@yahoo.com>
2025-10-17 20:23:48 -0400

    test(rs) to challenge 14b34f7
    relates-to: #66

--- src/easy/plus_one.rs
+++ src/easy/plus_one.rs
@@ -24,3 +24,26 @@ pub fn plus_one(digits: &mut Vec<i32>) -> Vec<i32> {
     }
     result.to_vec()
 }
+#[cfg(test)]
+mod tests {
+    use super::plus_one;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[4, 3, 2, 1], &[4, 3, 2, 2])]
+    #[case(&[1, 2, 3], &[1, 2, 4])]
+    #[case(&[9], &[1, 0])]
+    #[case(&[9, 9], &[1, 0, 0])]
+    #[case(&[0], &[1])]
+    #[case(&[5], &[6])]
+    #[case(&[9, 9, 9, 9], &[1, 0, 0, 0, 0])]
+    #[case(&[9, 9, 8], &[9, 9, 9])]
+    #[case(&[1, 0, 0], &[1, 0, 1])]
+    #[case(&[8, 9, 9], &[9, 0, 0])]
+    #[case(&[0, 9], &[1, 0])]
+    // #[case(&[1, 9, 9], &[2, 0, 0])]
+    // #[case(&[9, 9, 9, 9, 9], &[1, 0, 0, 0, 0, 0])]
+    fn test_plus_one(#[case] digits: &[i32], #[case] expected: &[i32]) {
+        let mut arr = digits.to_vec();
+        assert_eq!(plus_one(&mut arr), expected);
+    }
+}
