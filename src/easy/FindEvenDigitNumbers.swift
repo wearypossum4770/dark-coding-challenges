837c61d1d4f448064f3eff533b8fd7dfc6fcda82
Your Name <wearypossum4770@yahoo.com>
2025-10-18 08:46:30 -0400

    feat(swift): implements Find Even Digit Numbers challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 19.94 MB | 13.04 %  |
    
    relates-to: #1295

--- src/easy/FindEvenDigitNumbers.swift
+++ src/easy/FindEvenDigitNumbers.swift
@@ -1,3 +1,9 @@
-func findEvenDigitNumbers(_ nums: [Int])-> Int {
-	return nums.filter({ String($0).count % 2 == 0}).count
+func findEvenDigitNumbers(_ nums: [Int]) -> Int {
+  var result = 0
+  for num in nums {
+    if String(num).count % 2 == 0 {
+      result += 1
+    }
+  }
+  return result
 }
