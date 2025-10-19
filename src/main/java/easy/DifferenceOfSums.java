25ae39c7548707774f3346f5655aa5a2a16d5416
Your Name <wearypossum4770@yahoo.com>
2025-10-19 09:44:21 -0400

    feat(java): implements Difference of divisibility sums challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 40.81 MB | 56.36 %  |
    
    relates-to: #2894

---
+++ src/main/java/easy/DifferenceOfSums.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class DifferenceOfSums {
+  public int solve(int n, int m) {
+    int lhs = 0;
+    int rhs = 0;
+    for (int i = 1; i < n + 1; i++) {
+      if (i % m == 0) {
+        rhs += i;
+      } else {
+        lhs += i;
+      }
+    }
+    return lhs - rhs;
+  }
+}
