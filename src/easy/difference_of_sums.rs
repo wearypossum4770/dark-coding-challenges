9d5b8ddc3774bc74f3405528dc8f7b75404b7ddf
Your Name <wearypossum4770@yahoo.com>
2025-10-19 09:48:01 -0400

    test(rs) to challenge 717cc42
    relates-to: #2894

--- src/easy/difference_of_sums.rs
+++ src/easy/difference_of_sums.rs
@@ -10,3 +10,16 @@ pub fn difference_of_sums(n: i32, m: i32) -> i32 {
     }
     lhs - rhs
 }
+#[cfg(test)]
+mod tests {
+    use super::difference_of_sums;
+    use rstest::rstest;
+    #[rstest]
+    #[case(10, 3, 19)]
+    #[case(5, 6, 15)]
+    #[case(5, 1, -15)]
+
+    fn test_difference_of_sums(#[case] n: i32, #[case] m: i32, #[case] expected: i32) {
+        assert_eq!(difference_of_sums(n, m), expected);
+    }
+}
