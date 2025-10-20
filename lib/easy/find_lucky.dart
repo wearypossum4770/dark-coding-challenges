a86777b4ce2ae8491c428a60ee3534e30c077692
Your Name <wearypossum4770@yahoo.com>
2025-10-20 13:53:53 -0400

    chore(lint): lint and format c009ce8

--- lib/easy/find_lucky.dart
+++ lib/easy/find_lucky.dart
@@ -1,14 +1,14 @@
 import 'package:collection/collection.dart';
 
 int findLucky(List<int> arr) {
-	List<int> nums = List.filled(arr.max + 1, 0);
-	for(final n in arr) {
-		nums[n]++;
-	}
-	int index = nums.length - 1;
-	while(index > 0) {
-		if(nums[index] == index) return index;
-		index--;
-	}
-return -1;
+  List<int> nums = List.filled(arr.max + 1, 0);
+  for (final n in arr) {
+    nums[n]++;
+  }
+  int index = nums.length - 1;
+  while (index > 0) {
+    if (nums[index] == index) return index;
+    index--;
+  }
+  return -1;
 }
