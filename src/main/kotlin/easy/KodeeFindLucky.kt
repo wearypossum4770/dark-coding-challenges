9073d35c496beb07338074353a3e66c92cafa7fe
Your Name <wearypossum4770@yahoo.com>
2025-10-20 13:51:14 -0400

    chore(lint): lint and format fedff41

--- src/main/kotlin/easy/KodeeFindLucky.kt
+++ src/main/kotlin/easy/KodeeFindLucky.kt
@@ -1,13 +1,14 @@
 package easy
-fun kodeeFindLucky(arr: IntArray): Int  {
-	var result = IntArray(arr.max() + 1) { 0 }
-	var i = result.size - 1
-	for(num in arr) {
-		result[num]++
-	} 
-	while( i > 0) {
-		if(result[i] == i) return i
-		i--
-	}
-	return - 1
+
+fun kodeeFindLucky(arr: IntArray): Int {
+    var result = IntArray(arr.max() + 1) { 0 }
+    var i = result.size - 1
+    for (num in arr) {
+        result[num]++
+    }
+    while (i > 0) {
+        if (result[i] == i) return i
+        i--
+    }
+    return -1
 }
