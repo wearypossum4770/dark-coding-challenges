d2f8ec207afbcbd5069971d32114aa1f623e7b56
Your Name <wearypossum4770@yahoo.com>
2025-10-20 13:51:35 -0400

    chore(lint): lint and format 78af276

--- src/main/java/easy/FindLucky.java
+++ src/main/java/easy/FindLucky.java
@@ -3,15 +3,16 @@ package easy;
 import java.util.Arrays;
 
 public class FindLucky {
-public int solve(int[] arr) {
-	int max = Arrays.stream(arr).max().getAsInt();
-	int[] nums = new int[max + 1];
-	for (int i : arr) {
-		nums[i]++;
-	}
-	for(int i = nums.length - 1; i > 0; i--) {
-		if(i == nums[i]) return i;
-	}
-	
-return -1;
-}}
+  public int solve(int[] arr) {
+    int max = Arrays.stream(arr).max().getAsInt();
+    int[] nums = new int[max + 1];
+    for (int i : arr) {
+      nums[i]++;
+    }
+    for (int i = nums.length - 1; i > 0; i--) {
+      if (i == nums[i]) return i;
+    }
+
+    return -1;
+  }
+}
