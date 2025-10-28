2973460abf953fa4ab6e1a99c1d1538522c39efe
Your Name <wearypossum4770@yahoo.com>
2025-10-28 19:57:17 -0400

    test(rs) to challenge 4f395c6
    relates-to: #2108

--- src/easy/find_first_palindrome.rs
+++ src/easy/find_first_palindrome.rs
@@ -8,3 +8,18 @@ pub fn find_first_palindrome(words: Vec<String>) -> String {
     }
     "".to_string()
 }
+#[cfg(test)]
+mod tests {
+    use super::find_first_palindrome;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&["abc","car","ada","racecar","cool"], "ada")]
+    #[case(&["notapalindrome","racecar"], "racecar")]
+    #[case(&["def","ghi"], "")]
+    fn test_find_first_palindrome(#[case] words: &[&str], #[case] expected: &str) {
+        assert_eq!(
+            find_first_palindrome(words.iter().map(|w| w.to_string()).collect()),
+            expected.to_string()
+        );
+    }
+}
