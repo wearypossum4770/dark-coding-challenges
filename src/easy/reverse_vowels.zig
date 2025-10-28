66966b1d2ad446ec5617f7c2e8964ab6d5aef596
Your Name <wearypossum4770@yahoo.com>
2025-10-28 16:14:48 -0400

    test(zig) to challenge 2ac09d7
    relates-to: #345

--- src/easy/reverse_vowels.zig
+++ src/easy/reverse_vowels.zig
@@ -29,3 +29,144 @@ pub fn reverseVowels(allocator: Allocator, s: []const u8) ?[]const u8 {
     }
     return result;
 }
+
+test "reverse the vowels of IceCreAm" {
+    const s = "IceCreAm";
+    const expected = "AceCreIm";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of leetcode" {
+    const s = "leetcode";
+    const expected = "leotcede";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of a" {
+    const s = "a";
+    const expected = "a";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of b" {
+    const s = "b";
+    const expected = "b";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of AEIOU" {
+    const s = "AEIOU";
+    const expected = "UOIEA";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of aeiou" {
+    const s = "aeiou";
+    const expected = "uoiea";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of bcdfgh" {
+    const s = "bcdfgh";
+    const expected = "bcdfgh";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of aA" {
+    const s = "aA";
+    const expected = "Aa";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of a.b,e" {
+    const s = "a.b,e";
+    const expected = "e.b,a";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of hello world" {
+    const s = "hello world";
+    const expected = "hollo werld";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of aEiOu" {
+    const s = "aEiOu";
+    const expected = "uOiEa";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of .,?!" {
+    const s = ".,?!";
+    const expected = ".,?!";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of aaaaaa" {
+    const s = "aaaaaa";
+    const expected = "aaaaaa";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "reverse the vowels of aAaaAa" {
+    const s = "aAaaAa";
+    const expected = "aAaaAa";
+    const allocator = std.testing.allocator;
+    const result = reverseVowels(allocator, s);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualStrings(expected, actual);
+    } else return error.SkipZigTest;
+}
