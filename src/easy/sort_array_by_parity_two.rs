c173fad7324e6c6c652aa5195d448b463e60ad0d
Your Name <wearypossum4770@yahoo.com>
2025-10-29 13:35:38 -0400

    test(rs) add test to challenge 71b3713
    relates-to: #922

--- src/easy/sort_array_by_parity_two.rs
+++ src/easy/sort_array_by_parity_two.rs
@@ -13,3 +13,15 @@ pub fn sort_array_by_parity_two(nums: Vec<i32>) -> Vec<i32> {
     }
     result
 }
+#[cfg(test)]
+mod tests {
+    use super::sort_array_by_parity_two;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[4,2,5,7], &[4,5,2,7])]
+    #[case(&[2,3], &[2,3])]
+
+    fn test_sort_array_by_parity_two(#[case] nums: &[i32], #[case] expected: &[i32]) {
+        assert_eq!(sort_array_by_parity_two(nums.to_vec()), expected.to_vec());
+    }
+}
