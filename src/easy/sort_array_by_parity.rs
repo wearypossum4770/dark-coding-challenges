e12ccfe52269cbb7789dd80dd64d4491d88bebda
Your Name <wearypossum4770@yahoo.com>
2025-10-29 14:52:53 -0400

    feat(rs): implements Sort Array by Parity challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 2.14 MB  | 100.00 % |
    
    relates-to: #905

--- src/easy/sort_array_by_parity.rs
+++ src/easy/sort_array_by_parity.rs
@@ -1,16 +1,16 @@
-pub fn sort_array_by_parity(nums: &mut Vec<i32>) -> Vec<i32> {
-    let mut left: usize = 0;
-    let mut right: usize = nums.len() - 1;
-    while left < right {
-        if nums[left] % 2 == 0 {
-            left += 1;
-        } else if nums[right] % 2 == 1 {
-            right -= 1;
-        } else {
-            nums.swap(left, right);
-            left += 1;
-            right -= 1;
+pub fn sort_array_by_parity(arr:  Vec<i32>) -> Vec<i32> {
+    let mut nums = vec![0; arr.len()];
+    
+    let mut even: usize = 0;
+    let mut odd: usize = nums.len() - 1;
+    for &num in arr.iter(){
+        if num % 2 == 0 {
+            nums[even] = num;
+            even += 1;
+        }else {
+            nums[odd] = num;
+            odd -= 1;
         }
     }
-    nums.to_vec()
+    nums
 }
