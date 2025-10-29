6f3462b082e2dee9cb9b5d4939a2744723645068
Your Name <wearypossum4770@yahoo.com>
2025-10-28 20:22:38 -0400

    test(zig) add test to challenge 2df0d5e
    relates-to: #2108

--- src/easy/find_first_palindrome.zig
+++ src/easy/find_first_palindrome.zig
@@ -10,3 +10,22 @@ pub fn findFirstPalindrome(words: []const []const u8) []const u8 {
     }
     return "";
 }
+
+test "['zabc','car','ada','racecar','cool']" {
+    const words = [_][]const u8{ "abc", "car", "ada", "racecar", "cool" };
+    const expected = "ada";
+    const actual = findFirstPalindrome(&words);
+    try expectEqualStrings(expected, actual);
+}
+test "['notapalindrome','racecar']" {
+    const words = [_][]const u8{ "notapalindrome", "racecar" };
+    const expected = "racecar";
+    const actual = findFirstPalindrome(&words);
+    try expectEqualStrings(expected, actual);
+}
+test "['def','ghi']" {
+    const words = [_][]const u8{ "def", "ghi" };
+    const expected = "";
+    const actual = findFirstPalindrome(&words);
+    try expectEqualStrings(expected, actual);
+}
