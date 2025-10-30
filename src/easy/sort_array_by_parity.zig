ce0adfc6942c72c77c97c487c261739c4158d2ef
Your Name <wearypossum4770@yahoo.com>
2025-10-29 20:28:15 -0400

    perf(zig): improve algorithm for  challenge solution 4078582
    relates-to: #905

--- src/easy/sort_array_by_parity.zig
+++ src/easy/sort_array_by_parity.zig
@@ -1,43 +1,27 @@
 const std = @import("std");
-const expectEqualSlices = std.testing.expectEqualSlices;
-const expectEqual = std.testing.expectEqual;
 
-pub fn sortArrayByParity(comptime T: type, nums: []T) []T {
-    var left: usize = 0;
-    var right: usize = nums.len - 1;
-    while (left < right) {
-        if (@mod(nums[left], 2) == 0) {
-            left += 1;
-        } else if (@mod(nums[right], 2) == 1) {
-            right -= 1;
+const expectEqualSlices = std.testing.expectEqualSlices;
+
+const Allocator = std.mem.Allocator;
+
+pub fn sortArrayByParity(comptime T: type, allocator: Allocator, nums: []const T) ?[]const T {
+    var result = allocator.alloc(T, nums.len) catch return null;
+    if (result.len < 2) {
+        std.mem.copyBackwards(T, result, nums);
+        return result;
+    }
+    var even: usize = 0;
+    var odd: usize = nums.len - 1;
+    for (nums) |n| {
+        if (@mod(n, 2) == 0) {
+            result[even] = n;
+            even += 1;
         } else {
-            const temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left += 1;
-            right -= 1;
+            result[odd] = n;
+            if (odd > 0) {
+                odd -= 1;
+            }
         }
     }
-    return nums;
-}
-
-test "array [3, 1, 2, 4] sorted by even/odd parity" {
-    var nums = [_]i32{ 3, 1, 2, 4 };
-    const expected = [_]i32{ 4, 2, 1, 3 };
-    const actual = sortArrayByParity(@TypeOf(nums[0]), &nums);
-    try expectEqualSlices(i32, &expected, actual);
-}
-
-test "array [0, 1, 2] sorted by even/odd parity" {
-    var nums = [_]i32{ 0, 1, 2 };
-    const expected = [_]i32{ 0, 2, 1 };
-    const actual = sortArrayByParity(@TypeOf(nums[0]), &nums);
-    try expectEqualSlices(i32, &expected, actual);
-}
-
-test "array [0] sorted by even/odd parity" {
-    var nums = [_]i32{0};
-    const expected = [_]i32{0};
-    const actual = sortArrayByParity(@TypeOf(nums[0]), &nums);
-    try expectEqualSlices(i32, &expected, actual);
+    return result;
 }
