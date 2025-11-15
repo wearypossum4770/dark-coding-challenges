0b0e58ec9811daa32d3cedd415d62d8be2a3d4e9
Your Name <wearypossum4770@yahoo.com>
2025-11-15 14:48:44 -0500

    test(zig) add test to challenge 57e3024
    relates-to: #2652

--- src/easy/sum_of_multiples.zig
+++ src/easy/sum_of_multiples.zig
@@ -12,3 +12,94 @@ pub fn sumOfMultiples(comptime T: type, n: T) T {
     }
     return total;
 }
+
+test "sum of multiples of 3/5/7 until 1" {
+    const num: usize = 1;
+    const expected: u32 = 0;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 2" {
+    const num: usize = 2;
+    const expected: u32 = 0;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 3" {
+    const num: usize = 3;
+    const expected: u32 = 3;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 5" {
+    const num: usize = 5;
+    const expected: u32 = 8;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 7" {
+    const num: usize = 7;
+    const expected: u32 = 21;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 9" {
+    const num: usize = 9;
+    const expected: u32 = 30;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 10" {
+    const num: usize = 10;
+    const expected: u32 = 40;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 12" {
+    const num: usize = 12;
+    const expected: u32 = 52;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 15" {
+    const num: usize = 15;
+    const expected: u32 = 81;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 20" {
+    const num: usize = 20;
+    const expected: u32 = 119;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 21" {
+    const num: usize = 21;
+    const expected: u32 = 140;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 50" {
+    const num: usize = 50;
+    const expected: u32 = 691;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 100" {
+    const num: usize = 100;
+    const expected: u32 = 2_838;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 1_000" {
+    const num: usize = 1_000;
+    const expected: u32 = 272_066;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
+test "sum of multiples of 3/5/7 until 200" {
+    const num: usize = 200;
+    const expected: u32 = 10_845;
+    const actual = sumOfMultiples(@TypeOf(expected), num);
+    try expectEqual(expected, actual);
+}
