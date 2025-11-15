e3975244b5f30f5b79e06921f6131b75fa8780b5
Your Name <wearypossum4770@yahoo.com>
2025-11-15 14:04:05 -0500

    feat(java): implements Sum Multiples challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 3 ms     | 62.48 %  |
    | Memory    | 42.78 MB | 6.93 %   |
    
    relates-to: #2652

---
+++ src/main/java/easy/SumOfMultiples.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class SumOfMultiples {
+  public int solve(int n) {
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+      if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+        sum += i;
+      }
+    }
+
+    return sum;
+  }
+}
