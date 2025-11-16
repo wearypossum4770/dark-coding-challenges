e3e9a19053c7d8d2759cabba49fc05d647c9feb6
Your Name <wearypossum4770@yahoo.com>
2025-11-16 09:31:14 -0500

    test(rs) add test to challenge a565773
    relates-to: #1929

--- src/easy/get_array_concatenation.rs
+++ src/easy/get_array_concatenation.rs
@@ -7,3 +7,22 @@ pub fn get_array_concatenation(nums: Vec<i32>) -> Vec<i32> {
 
     answer
 }
+#[cfg(test)]
+mod tests {
+    use super::get_array_concatenation;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[1,2,1], &[1,2,1,1,2,1])]
+    #[case(&[1,3,2,1], &[1,3,2,1,1,3,2,1])]
+    #[case(&[1], &[1, 1])]
+    #[case(&[1000], &[1000, 1000])]
+    #[case(&[1, 1, 1], &[1, 1, 1, 1, 1, 1])]
+    #[case(&[2, 4, 6], &[2, 4, 6, 2, 4, 6])]
+    #[case(&[5, 10], &[5, 10, 5, 10])]
+    #[case(&[0, 1, 0], &[0, 1, 0, 0, 1, 0])]
+    #[case(&[1, 2, 3, 2, 1], &[1, 2, 3, 2, 1, 1, 2, 3, 2, 1])]
+
+    fn test_get_array_concatenation(#[case] nums: &[i32], #[case] expected: &[i32]) {
+        assert_eq!(get_array_concatenation(nums.to_vec()), expected.to_vec());
+    }
+}
