be3214987c9635f99837532190655568b8992b0b
Your Name <wearypossum4770@yahoo.com>
2025-11-16 20:27:50 -0500

    test(rs) add test to challenge e9ef2e7
    relates-to: #1470

--- src/easy/shuffle_array_to_coordinates.rs
+++ src/easy/shuffle_array_to_coordinates.rs
@@ -7,3 +7,22 @@ pub fn shuffle_array_to_coordinates(nums: Vec<i32>, n: i32) -> Vec<i32> {
     }
     result
 }
+#[cfg(test)]
+mod tests {
+    use super::shuffle_array_to_coordinates;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[2,5,1,3,4,7],  3, &[2,3,5,4,1,7])]
+    #[case(&[1,2,3,4,4,3,2,1], 4, &[1,4,2,3,3,2,4,1])]
+    #[case(&[1,1,2,2], 2, &[1,2,1,2])]
+    fn test_shuffle_array_to_coordinates(
+        #[case] nums: &[i32],
+        #[case] n: i32,
+        #[case] expected: &[i32],
+    ) {
+        assert_eq!(
+            shuffle_array_to_coordinates(nums.to_vec(), n),
+            expected.to_vec()
+        );
+    }
+}
