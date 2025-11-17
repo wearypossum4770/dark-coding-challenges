f0a96999b4461414a965e162fca0797aaff0826a
Your Name <wearypossum4770@yahoo.com>
2025-11-16 20:31:53 -0500

    test(swift) add test to challenge b5a4d18
    relates-to: #1470

---
+++ test/easyswift/ShuffleArrayToCoordinatesTest.swift
@@ -0,0 +1,12 @@
+import Testing
+
+@testable import easy
+
+@Test(arguments: [
+  ([2, 5, 1, 3, 4, 7], 3, [2, 3, 5, 4, 1, 7]),
+  ([1, 2, 3, 4, 4, 3, 2, 1], 4, [1, 4, 2, 3, 3, 2, 4, 1]),
+  ([1, 1, 2, 2], 2, [1, 2, 1, 2]),
+])
+func shuffleArrayToCoordinatesTest(nums: [Int], n: Int, expected: [Int]) {
+  #expect(shuffleArrayToCoordinates(nums, n) == expected)
+}
