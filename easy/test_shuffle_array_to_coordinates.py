bbb76da96c78ddf62c8853545cb434677075cf85
Your Name <wearypossum4770@yahoo.com>
2025-11-16 20:30:27 -0500

    test(py) add test to challenge 601ac7e
    relates-to: #1470

---
+++ easy/test_shuffle_array_to_coordinates.py
@@ -0,0 +1,16 @@
+import pytest
+from easy.shuffle_array_to_coordinates import shuffle_array_to_coordinates
+
+
+@pytest.mark.parametrize(
+    "nums, n, expected",
+    [
+        ([2, 5, 1, 3, 4, 7], 3, [2, 3, 5, 4, 1, 7]),
+        ([1, 2, 3, 4, 4, 3, 2, 1], 4, [1, 4, 2, 3, 3, 2, 4, 1]),
+        ([1, 1, 2, 2], 2, [1, 2, 1, 2]),
+    ],
+)
+def test_shuffle_array_to_coordinates(
+    nums: list[int], n: int, expected: list[int]
+):
+    assert shuffle_array_to_coordinates(nums, n) == expected
