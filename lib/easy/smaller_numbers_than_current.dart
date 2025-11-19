53f8b32468d6328f22e063265c7fe4fc8bb020d4
Your Name <wearypossum4770@yahoo.com>
2025-11-19 01:10:10 -0500

    feat(dart): implements Numbers Smaller than Current challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 10 ms    | 66.67 %  |
    | Memory    | 148.29 MB| 88.89 %  |
    
    relates-to: #1365

---
+++ lib/easy/smaller_numbers_than_current.dart
@@ -0,0 +1,11 @@
+List<int> smallerNumbersThanCurrent(List<int> nums) {
+  List<int> result = List.filled(nums.length, 0);
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        result[i]++;
+      }
+    }
+  }
+  return result;
+}
