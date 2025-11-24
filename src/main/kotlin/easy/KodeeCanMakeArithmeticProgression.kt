34b9dabca54bf5ca898fb09510f2aa2c9349c6b3
Your Name <wearypossum4770@yahoo.com>
2025-11-24 09:16:13 -0500

    feat(kt): implements Arithmetic Progression challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 10 ms    | 64.29 %  |
    | Memory    | 45.68 MB | 64.29 %  |
    
    Relates-to: #1502

---
+++ src/main/kotlin/easy/KodeeCanMakeArithmeticProgression.kt
@@ -0,0 +1,11 @@
+package easy
+fun kodeeCanMakeArithmeticProgression(arr: IntArray): Boolean  {
+	arr.sort()
+	val distance = arr[0] - arr[1]
+	var index = 1
+	while(index < arr.size - 1) {
+		if(arr[index] - arr[index + 1] != distance) return false
+		index++
+	}
+	return true
+}
