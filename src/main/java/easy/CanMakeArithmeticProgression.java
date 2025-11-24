8e3456e2b068d0249e6595748933f759b07f88c4
Your Name <wearypossum4770@yahoo.com>
2025-11-24 09:17:08 -0500

    feat(java): implements Arithmetic Progression challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 5 ms     | 54.32 %  |
    | Memory    | 44.08 MB | 43.08 %  |
    
    Relates-to: #1502

---
+++ src/main/java/easy/CanMakeArithmeticProgression.java
@@ -0,0 +1,15 @@
+package easy;
+
+import java.util.Arrays;
+
+public class CanMakeArithmeticProgression {
+public boolean solve(int[] arr) {
+	Arrays.sort(arr);
+	int index = 0;
+	int distance = arr[0] - arr[1];
+	while(index < arr.length - 1 ) {
+		if(arr[index] - arr[index + 1] != distance) return false;
+		index++;
+	}
+	return true;
+}}
