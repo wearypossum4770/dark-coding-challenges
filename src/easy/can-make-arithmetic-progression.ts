5f2ac2ddc3353de4684e9d31f81d7562d776e348
Your Name <wearypossum4770@yahoo.com>
2025-11-24 09:21:10 -0500

    feat(ts): implements Arithmetic Progression challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 1 ms     | 77.11 %  |
    | Memory    | 55.75 MB | 65.06 %  |
    
    Relates-to: #1502

---
+++ src/easy/can-make-arithmetic-progression.ts
@@ -0,0 +1,8 @@
+export const canMakeArithmeticProgression = (arr: number[]): boolean => {
+    arr.sort((a, b) => a - b);
+    const distance = arr[0] - arr[1];
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (arr[i] - arr[i + 1] !== distance) return false;
+    }
+    return true;
+};
