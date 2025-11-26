ca9284f6589320598014fb5f9dbf5f3a38d229da
Your Name <wearypossum4770@yahoo.com>
2025-11-26 07:56:30 -0500

    feat(php): implements Kth Frequent Words challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 1 ms     | 100.00 % |
    | Memory    | 20.58 MB | 42.86 %  |
    
    Relates-to: #692

---
+++ src/main/php/Medium/TopFrequentWords.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Medium;
+
+class TopFrequentWords
+{
+	   /**
+     * @param String[] $words
+     * @param Integer $k
+     * @return String[]
+     */
+    public function solve(array $words, int $k): array
+    {
+        $result = [];
+        foreach ($words as $word) {
+            $result[$word] = ($result[$word] ?? 0) + 1;
+        }
+				uksort($result, 'strcmp');
+        uasort($result, fn ($a, $b) => $b - $a);
+				return array_slice(array_keys($result), 0, $k);
+    }
+}
