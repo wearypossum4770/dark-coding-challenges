6554189ac2d8117fa0b987fdb561fb8a2dea3c4f
Your Name <wearypossum4770@yahoo.com>
2025-11-26 20:26:39 -0500

    test(rs) add test to challenge 4df8397
    relates-to: #692

--- src/medium/top_frequent_words.rs
+++ src/medium/top_frequent_words.rs
@@ -13,3 +13,29 @@ pub fn top_frequent_words(words: Vec<String>, k: i32) -> Vec<String> {
         .collect::<Vec<String>>();
     result[0..k as usize].to_vec()
 }
+#[cfg(test)]
+mod tests {
+    use super::top_frequent_words;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&["i","love","leetcode","i","love","coding"], 2, &["i","love"])]
+    #[case(&["i", "love", "leetcode", "i", "love", "coding"], 3, &["i", "love", "coding"])]
+    #[case(&["the","day","is","sunny","the","the","the","sunny","is","is"], 4, &["the","is","sunny","day"])]
+    #[case(&["i","love","leetcode","i","love","coding"], 3, &["i","love","coding"])]
+    #[case(&["hello","hello","hello"], 1, &["hello"])]
+    #[case(&["a","b","c","d"], 4, &["a","b","c","d"])]
+    #[case(&["apple","banana","apple","banana","cherry","cherry"], 3, &["apple","banana","cherry"])]
+    #[case(&["x","y","z"], 1, &["x"])]
+    #[case(&[], 0, &[])]
+    #[case(&["a","aa","aaa","a","aaa","aaa","aa","b","b","b"], 3, &["aaa","b","a"])]
+    #[case(&["one","two"], 2, &["one","two"])]
+    fn test_top_frequent_words(#[case] words: &[&str], #[case] k: i32, #[case] expected: &[&str]) {
+        assert_eq!(
+            top_frequent_words(words.iter().map(|x| x.to_string()).collect(), k),
+            expected
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+        );
+    }
+}
