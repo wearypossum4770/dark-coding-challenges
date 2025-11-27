e48fe8050bbe146c6b3b3fc99d6ab34f514525f8
Your Name <wearypossum4770@yahoo.com>
2025-11-26 20:33:32 -0500

    test(zig) add test to challenge e58b7dd
    relates-to: #692

--- src/medium/top_frequent_words.zig
+++ src/medium/top_frequent_words.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const expectEqualSlices = std.testing.expectEqualSlices;
+const expectEqual = std.testing.expectEqual;
+
 const StringHashMap = std.hash_map.StringHashMap;
 const Allocator = std.mem.Allocator;
 
@@ -53,4 +56,118 @@ pub fn topFrequentWords(allocator: Allocator, words: []const []const u8, k: usiz
         result[i] = target[i].word;
     }
     return result;
+}
+
+test "test concordance" {
+    const words = [_][]const u8{ "apple", "apple", "banana", "grape" };
+    const counts = [_]usize{ 2, 2, 1, 1 };
+    const allocator = std.testing.allocator;
+    var result = createConcordance(allocator, &words);
+    defer result.deinit();
+    for (words, 0..) |word, index| {
+        if (result.get(word)) |concordance| {
+            const actual = concordance.count;
+            const expected = counts[index];
+            try expectEqual(expected, actual);
+        }
+    }
+}
+test "find the top 2 words in ['i','love']" {
+    const words = [_][]const u8{ "i", "love", "leetcode", "i", "love", "coding" };
+    const k: usize = 2;
+    const expected = [_][]const u8{ "i", "love" };
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 4 words in ['the','day','is','sunny','the','the','the','sunny','is','is']" {
+    const words = [_][]const u8{ "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
+    const k: usize = 4;
+    const expected = [_][]const u8{ "the", "is", "sunny", "day" };
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 1 words in ['hello','hello','hello']" {
+    const words = [_][]const u8{ "hello", "hello", "hello" };
+    const k: usize = 1;
+    const expected = [_][]const u8{"hello"};
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 4 words in ['a','b','c','d']" {
+    const words = [_][]const u8{ "a", "b", "c", "d" };
+    const k: usize = 4;
+    const expected = [_][]const u8{ "a", "b", "c", "d" };
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 3 words in ['apple','banana','apple','banana','cherry','cherry']" {
+    const words = [_][]const u8{ "apple", "banana", "apple", "banana", "cherry", "cherry" };
+    const k: usize = 3;
+    const expected = [_][]const u8{ "apple", "banana", "cherry" };
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 1 words in ['x','y','z']" {
+    const words = [_][]const u8{ "x", "y", "z" };
+    const k: usize = 1;
+    const expected = [_][]const u8{"x"};
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 0 words in []" {
+    const words = [_][]const u8{};
+    const k: usize = 0;
+    const expected = [_][]const u8{};
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 3 words in ['a','aa','aaa','a','aaa','aaa','aa','b','b','b']" {
+    const words = [_][]const u8{ "a", "aa", "aaa", "a", "aaa", "aaa", "aa", "b", "b", "b" };
+    const k: usize = 3;
+    const expected = [_][]const u8{ "aaa", "b", "a" };
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
+}
+test "find the top 5 words in ['one','two']" {
+    const words = [_][]const u8{ "one", "two" };
+    const k: usize = 2;
+    const expected = [_][]const u8{ "one", "two" };
+    const allocator = std.testing.allocator;
+    const result = topFrequentWords(allocator, &words, k);
+    if (result) |actual| {
+        defer allocator.free(actual);
+        try expectEqualSlices(@TypeOf(actual[0]), &expected, actual);
+    } else return error.SkipZigTest;
 }
