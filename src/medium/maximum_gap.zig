8a6fd82e1fb1a5a1665f798cc67f15735b82c2a8
Your Name <wearypossum4770@yahoo.com>
2025-11-26 19:22:35 -0500

    test(zig) add test to challenge baabccb
    relates-to: #164

--- src/medium/maximum_gap.zig
+++ src/medium/maximum_gap.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+const expectEqual = std.testing.expectEqual;
 const Allocator = std.mem.Allocator;
 
 pub fn maximumGap(comptime T: type, allocator: Allocator, nums: []const T) ?T {
@@ -16,4 +17,185 @@ pub fn maximumGap(comptime T: type, allocator: Allocator, nums: []const T) ?T {
     }
 
     return result;
+}
+
+test "maximum gap among the sorted array {3, 6, 9, 1}" {
+    const nums = [_]u4{ 3, 6, 9, 1 };
+    const expected: @TypeOf(nums[0]) = 3;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {10}" {
+    const nums = [_]u4{10};
+    const expected: @TypeOf(nums[0]) = 0;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 100}" {
+    const nums = [_]u8{ 1, 100 };
+    const expected: @TypeOf(nums[0]) = 99;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 2, 3, 4, 5}" {
+    const nums = [_]u4{ 1, 2, 3, 4, 5 };
+    const expected: @TypeOf(nums[0]) = 1;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {10, 9, 8, 7}" {
+    const nums = [_]u4{ 10, 9, 8, 7 };
+    const expected: @TypeOf(nums[0]) = 1;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {7, 7, 7, 7}" {
+    const nums = [_]u4{ 7, 7, 7, 7 };
+    const expected: @TypeOf(nums[0]) = 0;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {0, 1_000_000_000}" {
+    const nums = [_]u32{ 0, 1_000_000_000 };
+    const expected: @TypeOf(nums[0]) = 1_000_000_000;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 100, 2, 50_000_000}" {
+    const nums = [_]u32{ 1, 100, 2, 50_000_000 };
+    const expected: @TypeOf(nums[0]) = 49999900;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 1}" {
+    const nums = [_]u4{ 1, 1 };
+    const expected: @TypeOf(nums[0]) = 0;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {2, 2, 2, 3}" {
+    const nums = [_]u4{ 2, 2, 2, 3 };
+    const expected: @TypeOf(nums[0]) = 1;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {100, 3, 2, 1}" {
+    const nums = [_]u7{ 100, 3, 2, 1 };
+    const expected: @TypeOf(nums[0]) = 97;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 3, 1_000, 2_000}" {
+    const nums = [_]u11{ 1, 3, 1_000, 2_000 };
+    const expected: @TypeOf(nums[0]) = 1_000;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {8, 1, 6, 3, 9}" {
+    const nums = [_]u4{ 8, 1, 6, 3, 9 };
+    const expected: @TypeOf(nums[0]) = 3;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {5, 4, 3, 2, 1}" {
+    const nums = [_]u4{ 5, 4, 3, 2, 1 };
+    const expected: @TypeOf(nums[0]) = 1;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 2}" {
+    const nums = [_]u4{ 1, 2 };
+    const expected: @TypeOf(nums[0]) = 1;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1_000_000_000, 0, 999_999_999}" {
+    const nums = [_]u32{ 1_000_000_000, 0, 999_999_999 };
+    const expected: @TypeOf(nums[0]) = 999_999_999;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {1, 4, 7, 13, 20, 21}" {
+    const nums = [_]u5{ 1, 4, 7, 13, 20, 21 };
+    const expected: @TypeOf(nums[0]) = 7;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {5, 5, 5, 10}" {
+    const nums = [_]u4{ 5, 5, 5, 10 };
+    const expected: @TypeOf(nums[0]) = 5;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {0, 1, 2, 3, 100}" {
+    const nums = [_]u7{ 0, 1, 2, 3, 100 };
+    const expected: @TypeOf(nums[0]) = 97;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
+}
+test "maximum gap among the sorted array {100, 200, 300, 1_000}" {
+    const nums = [_]u11{ 100, 200, 300, 1_000 };
+    const expected: @TypeOf(nums[0]) = 700;
+    const allocator = std.testing.allocator;
+    const result = maximumGap(@TypeOf(nums[0]), allocator, &nums);
+    if (result) |actual| {
+        try expectEqual(expected, actual);
+    } else return error.SkipZigTest;
 }
