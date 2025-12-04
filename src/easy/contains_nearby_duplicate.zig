ac932d2b1405b24b6e6e3f3ff01f6c48183714db
Your Name <wearypossum4770@yahoo.com>
2025-12-03 23:48:11 -0500

    test(zig) add test to challenge 503e00e3
    relates-to: #219

--- src/easy/contains_nearby_duplicate.zig
+++ src/easy/contains_nearby_duplicate.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const expectEqual = std.testing.expectEqual;
+
 pub fn containsNearbyDuplicateArrayBacked(comptime T: type, nums: []const T, k: T) bool {
     const maxValue: usize = 100_001;
     var lastSeen = [_]usize{0} ** maxValue;
@@ -9,4 +11,12 @@ pub fn containsNearbyDuplicateArrayBacked(comptime T: type, nums: []const T, k: 
         lastSeen[n] = i;
     }
     return false;
+}
+
+test "distinct indices whose difference is 3 in {1, 2, 3, 1} backed by array" {
+    const nums = [_]i32{ 1, 2, 3, 1 };
+    const k: @TypeOf(nums[0]) = 3;
+    const expected = true;
+    const actual = containsNearbyDuplicateArrayBacked(@TypeOf(nums[0]), &nums, k);
+    try expectEqual(expected, actual);
 }
