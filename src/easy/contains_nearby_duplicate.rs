d95cbb62b3902377f06965a0749d76b7a7d52e4f
Your Name <wearypossum4770@yahoo.com>
2025-12-03 20:10:29 -0500

    feat(rs): implements Contains Duplicate II challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 11 ms    | 55.03 %  |
    | Memory    | 6.59 MB  | 25.40 %  |
    
    Relates-to: #219

--- src/easy/contains_nearby_duplicate.rs
+++ src/easy/contains_nearby_duplicate.rs
@@ -3,13 +3,11 @@ use std::collections::HashMap;
 pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
     let mut seen: HashMap<i32, usize> = HashMap::new();
     for (i, &num) in nums.iter().enumerate() {
-        if let Some(&n) = seen.get(&num) {
+        if let Some(n) = seen.insert(num, i) {
             if i.abs_diff(n) <= k as usize {
                 return true;
             }
         }
-        seen.insert(num, i);
     }
-
     false
 }
