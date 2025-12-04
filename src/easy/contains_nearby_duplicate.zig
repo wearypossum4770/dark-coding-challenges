ef425b761203268812d3f59604dbc63354d14e62
Your Name <wearypossum4770@yahoo.com>
2025-12-03 23:49:01 -0500

    feat(zig): implements Contains Duplicate II challenge solution
    Relates-to: #219

--- src/easy/contains_nearby_duplicate.zig
+++ src/easy/contains_nearby_duplicate.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 
 const expectEqual = std.testing.expectEqual;
 
+const Allocator = std.mem.Allocator;
+const AutoHashmap = std.hash_map.AutoHashMap;
+
 pub fn containsNearbyDuplicateArrayBacked(comptime T: type, nums: []const T, k: T) bool {
     const maxValue: usize = 100_001;
     var lastSeen = [_]usize{0} ** maxValue;
@@ -9,6 +12,17 @@ pub fn containsNearbyDuplicateArrayBacked(comptime T: type, nums: []const T, k: 
         const n: usize = @intCast(num);
         if (lastSeen[n] != -1 and i - lastSeen[n] <= k) return true;
         lastSeen[n] = i;
+    }
+    return false;
+}
+pub fn containsNearbyDuplicate(comptime T: type, allocator: Allocator, nums: []const T, k: T) ?bool {
+    var seen = AutoHashmap(T, usize).init(allocator);
+    defer seen.deinit();
+    for (nums, 0..) |num, i| {
+        if (seen.get(num)) |n| {
+            if (@abs(i - n) <= k) return true;
+        }
+        seen.put(num, i) catch return null;
     }
     return false;
 }
