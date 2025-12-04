9673f6489bca9b04de7879b3e6c598755d041313
Your Name <wearypossum4770@yahoo.com>
2025-12-04 09:54:43 -0500

    fix(swift) addjust failing and add new tests for b3618c0
    relates-to: #819

--- test/easyswift/MostCommonSafeWordTest.swift
+++ test/easyswift/MostCommonSafeWordTest.swift
@@ -7,12 +7,16 @@ import Testing
   ("Hello hello world!", ["world"], "hello"),
   ("apple banana banana apple apple", [], "apple"),
   ("dog cat dog", ["dog", "cat"], ""),
-  ("one two two three three", [], "two"),
+  ("one two two two three three", [], "two"),
   ("", [], ""),
+  ("Bob. hIt, baLl", ["bob", "hit"], "ball"),
+  ("Apple apple APPLE!", ["apple"], ""),
   ("Wow! Such, wow? Very-wow.", ["such"], "wow"),
   ("test test test", ["test"], ""),
-  ("123 123 !@#", [], ""),
   ("echo echo echo echo", [], "echo"),
+  ("Wow! wow? wow. really, really... REALLY!", ["wow"], "really"),
+  ("a.", [], "a"),
+  ("a b.b", [], "b"),
 ])
 func mostCommonSafeWordTest(paragraph: String, banned: [String], expected: String) {
   #expect(mostCommonSafeWord(paragraph, banned) == expected)
