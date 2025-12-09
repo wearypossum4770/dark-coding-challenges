8cc4b2d4f168b8ee03d3f115cf15c5741fe5a65f
Your Name <wearypossum4770@yahoo.com>
2025-12-09 15:18:53 -0500

    test(rs) add test to challenge 205e099
    relates-to: #1700

--- src/easy/count_students.rs
+++ src/easy/count_students.rs
@@ -16,3 +16,33 @@ pub fn count_students(students: Vec<i32>, sandwiches: Vec<i32>) -> i32 {
     }
     lhs.len() as i32
 }
+
+#[cfg(test)]
+mod tests {
+    use super::count_students;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[1,1,0,0], &[0,1,0,1], 0)]
+    #[case(&[1,1,1,0,0,1], &[1,0,0,0,1,1], 3)]
+    #[case(&[0,0,0], &[0,0,0], 0)]
+    #[case(&[1,1,1], &[1,1,1], 0)]
+    #[case(&[1,0,1,0], &[1,0,1,0], 0)]
+    #[case(&[0,1,0,1], &[1,0,1,0], 0)]
+    #[case(&[1], &[0], 1)]
+    #[case(&[0], &[1], 1)]
+    #[case(&[0], &[0], 0)]
+    #[case(&[1], &[1], 0)]
+    #[case(&[0,1,0,1,1,0], &[1,1,0,0,0,1], 0)]
+    #[case(&[1,1,1,1,1], &[0,0,0,0,0], 5)]
+    #[case(&[0,0,0,0,0], &[1,1,1,1,1], 5)]
+    fn test_count_students(
+        #[case] students: &[i32],
+        #[case] sandwiches: &[i32],
+        #[case] expected: i32,
+    ) {
+        assert_eq!(
+            count_students(students.to_vec(), sandwiches.to_vec()),
+            expected
+        );
+    }
+}
