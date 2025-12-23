c366abcb3c5f39cef9505e20297e860fcc8f7823
Your Name <wearypossum4770@yahoo.com>
2025-12-23 17:21:48 -0500

    test(php): correction to 3b4ac2c
    related-to: #206

--- test/easy/ReverseLinkedListTest.php
+++ test/easy/ReverseLinkedListTest.php
@@ -1,15 +1,24 @@
-
 <?php
 
+declare(strict_types=1);
+
+use Src\Easy\ListNode;
 use Src\Easy\ReverseLinkedList;
 
-describe('TestValue', function () {
+dataset('reverseLinkedListData', [
+    [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]],
+    [[1, 2], [2, 1]],
+    [[], []],
+]);
+describe('\n\tFeature: Reverse Linked List', function () {
 
-    test('Testing', function ($candidate, $expected) {
+    test("\n\tGiven: a linked list :candidate.\n\tWhen: it is reversed.\n\tThen: it will be :expected.", function ($candidate, $expected) {
         $instance = new ReverseLinkedList;
+        $head = ListNode::fromArray($candidate);
+        $result = $instance->solve($head);
+        $target = ListNode::toArray($result);
+        expect($target)->toBe($expected);
 
-        expect($instance->solve($candidate))->toBe($expected);
-
-    })->with([]);
+    })->with('reverseLinkedListData');
 
 });
