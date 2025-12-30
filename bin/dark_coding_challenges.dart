7df319efb4666ab257fbc2335e978b7f5f7c5ae8
Your Name <wearypossum4770@yahoo.com>
2025-12-30 14:35:46 -0500

    feat(dart): add entry point for dart lang

---
+++ bin/dark_coding_challenges.dart
@@ -0,0 +1,7 @@
+import 'package:dark_coding_challenges/dark_coding_challenges.dart';
+
+void main() {
+  // Call your library functions here
+  print("\nThe max safe integer (ECMAScript compatible) is ${maxSafeInteger()}");
+  print("Dark Coding Challenges Dart running!");
+}
