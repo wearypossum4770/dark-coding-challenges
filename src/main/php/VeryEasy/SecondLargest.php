b256455a7781e11c13fe5235f0e1d08177fa643b
Your Name <wearypossum4770@yahoo.com>
2025-12-30 19:03:14 -0500

    feat(php): implement Second Max Num challenge solution
    relates-to: #414

---
+++ src/main/php/VeryEasy/SecondLargest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\VeryEasy;
+
+class SecondLargest
+{
+    public function solve(array $nums): int
+    {
+        $first = PHP_INT_MIN;
+        $second = PHP_INT_MIN;
+        foreach ($nums as $num) {
+            if ($first < $num) {
+                $second = $first;
+                $first = $num;
+            } elseif ($second < $num && $first > $second) {
+                $second = $num;
+            }
+        }
+
+        return $second;
+    }
+}
