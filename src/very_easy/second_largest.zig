f7795e37363c6a3e51d6832a329ca7bb78fe2e17
Your Name <wearypossum4770@yahoo.com>
2025-12-30 19:10:41 -0500

    refactor(zig): implement Second Max Num challenge solution
    relates-to: #414

--- src/very_easy/second_largest.zig
+++ src/very_easy/second_largest.zig
@@ -1,8 +1,17 @@
 const std = @import("std");
 const expectEqualSlices = std.testing.expectEqualSlices;
 const expectEqual = std.testing.expectEqual;
-
+const minInt = std.math.minInt;
 pub fn secondLargest(comptime T: type, nums: []T) T {
-    std.mem.sort(T, nums, {}, std.sort.desc(T));
-    return nums[1];
+    var first: T = minInt(T);
+    var second: T = first;
+    for (nums) |n| {
+        if (first < n) {
+            second = first;
+            first = n;
+        } else if (second < n and first > n) {
+            second = n;
+        }
+    }
+    return second;
 }
