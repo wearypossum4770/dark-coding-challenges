5ad55743d6c72911740dc1375302735167ef87f4
Your Name <wearypossum4770@yahoo.com>
2026-01-01 14:27:17 -0500

    chore(dep): clean up unused dependency e5df488
    prerequisite-to: #414

--- src/very_easy/second_largest.zig
+++ src/very_easy/second_largest.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
-const expectEqualSlices = std.testing.expectEqualSlices;
+
 const expectEqual = std.testing.expectEqual;
 const minInt = std.math.minInt;
+
 pub fn secondLargest(comptime T: type, nums: []T) T {
     var first: T = minInt(T);
     var second: T = first;
