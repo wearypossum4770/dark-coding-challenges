d758203ef506db86f6bf24921faec53c740ac8f4
Your Name <wearypossum4770@yahoo.com>
2026-01-05 22:35:33 -0500

    test(go) add test to challenge 1d93f3ee
    relates-to: #1507

---
+++ easy/reformat_date_test.go
@@ -0,0 +1,43 @@
+package easy
+
+import (
+	"testing"
+)
+
+func TestReformatDate(t *testing.T) {
+	cases := []struct {
+		date     string
+		expected string
+	}{
+		{"20th Oct 2052", "2052-10-20"},
+		{"6th Jun 1933", "1933-06-06"},
+		{"26th May 1960", "1960-05-26"},
+		{"1st Jan 1900", "1900-01-01"},
+		{"31st Dec 2100", "2100-12-31"},
+		{"2nd Feb 2000", "2000-02-02"},
+		{"3rd Mar 1999", "1999-03-03"},
+		{"4th Apr 2010", "2010-04-04"},
+		{"9th Sep 2021", "2021-09-09"},
+		{"10th Oct 2020", "2020-10-10"},
+		{"11th Nov 2011", "2011-11-11"},
+		{"12th Dec 2012", "2012-12-12"},
+		{"21st Aug 1987", "1987-08-21"},
+		{"22nd Jul 1975", "1975-07-22"},
+		{"23rd Mar 1965", "1965-03-23"},
+		{"30th Apr 1990", "1990-04-30"},
+		{"28th Feb 2001", "2001-02-28"},
+		{"29th Feb 2000", "2000-02-29"},
+		{"7th Jul 2007", "2007-07-07"},
+		{"8th Aug 1988", "1988-08-08"},
+	}
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			result := ReformatDate(c.date)
+			if result != c.expected {
+				t.Errorf("ReformatDate(%q) == %q, expected %q", c.date, result, c.expected)
+			}
+
+		})
+
+	}
+}
