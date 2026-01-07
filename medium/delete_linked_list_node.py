d417e18638102c0246aa8865e9f5a4a009e05420
Your Name <wearypossum4770@yahoo.com>
2026-01-06 19:26:22 -0500

    doc(py): add challenge 47cfa54 doc-string
    relates-to: #237

--- medium/delete_linked_list_node.py
+++ medium/delete_linked_list_node.py
@@ -2,5 +2,9 @@ from easy.list_node import ListNode
 
 
 def delete_linked_list_node(node: ListNode):
+    """
+    :type node: ListNode
+    :rtype: void Do not return anything, modify node in-place instead.
+    """
     node.val = node.next.val
     node.next = node.next.next
