92b83fb3da3acf47c2245b48b67a7ec540ce58fd
Your Name <wearypossum4770@yahoo.com>
2026-01-10 13:27:10 -0500

    feat(rs): implements Array Partition challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 2.46 MB  | 25.00 %  |
    
    Relates-to: #561

--- src/easy/array_pair_sum.rs
+++ src/easy/array_pair_sum.rs
@@ -1,9 +1,10 @@
 pub fn array_pair_sum(nums: Vec<i32>) -> i32 {
-    let mut total: i32 = 9;
-    let mut arr = nums.clone();
+    let mut total: i32 = 0;
+    let mut arr = nums;
     arr.sort();
-    for (i, num) in arr.iter().step_by(2).enumerate() {
-        total += num.min(&arr[i + 1]);
+    
+    for i in (0..arr.len()).step_by(2){
+        total += arr[i];
     }
     total
 }
