8c9752d4a509c7aa69c9eb7ec6f32d9502417ad8
Your Name <wearypossum4770@yahoo.com>
2026-01-10 13:40:10 -0500

    test(rs) add test to challenge 92b83fb
    relates-to: #561

--- src/easy/array_pair_sum.rs
+++ src/easy/array_pair_sum.rs
@@ -2,9 +2,34 @@ pub fn array_pair_sum(nums: Vec<i32>) -> i32 {
     let mut total: i32 = 0;
     let mut arr = nums;
     arr.sort();
-    
-    for i in (0..arr.len()).step_by(2){
+
+    for i in (0..arr.len()).step_by(2) {
         total += arr[i];
     }
     total
+}
+#[cfg(test)]
+mod tests {
+    use super::array_pair_sum;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[1, 4, 3, 2], 4)]
+    #[case(&[6, 2, 6, 5, 1, 2], 9)]
+    #[case(&[1, 1], 1)]
+    #[case(&[0, 0], 0)]
+    #[case(&[-1, -1], -1)]
+    #[case(&[10_000, 10_000], 10_000)]
+    #[case(&[5, 5, 5, 5], 10)]
+    #[case(&[7, 7, 7, 7, 7, 7], 21)]
+    #[case(&[1, 2, 3, 4, 5, 6], 9)]
+    #[case(&[6, 5, 4, 3, 2, 1], 9)]
+    #[case(&[-5, -4, -3, -2], -8)]
+    #[case(&[-10, -1, 1, 10], -9)]
+    #[case(&[-10_000, 10_000, -9_999, 9_999], -1)]
+    #[case(&[1, 100, 2, 99, 3, 98], 100 + 3)]
+    #[case(&[0, 0, 0, 0, 0, 0], 0)]
+    #[case(&[-10_000, 10_000], -10_000)]
+    fn test_array_pair_sum(#[case] nums: &[i32], #[case] expected: i32) {
+        assert_eq!(array_pair_sum(nums.to_vec()), expected);
+    }
 }
