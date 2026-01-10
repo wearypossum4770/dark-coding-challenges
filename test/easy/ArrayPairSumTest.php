1c44a77c2d88dd18c875091ebb2a6c0c22bacd46
Your Name <wearypossum4770@yahoo.com>
2026-01-10 13:40:25 -0500

    test(php) add test to challenge 53c6eae
    relates-to: #561

---
+++ test/easy/ArrayPairSumTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Src\Easy\ArrayPairSum;
+
+dataset('arrayPairSumData', [
+    [[1, 4, 3, 2], 4],
+    [[6, 2, 6, 5, 1, 2], 9],
+    [[1, 1], 1],
+    [[0, 0], 0],
+    [[-1, -1], -1],
+    [[10_000, 10_000], 10_000],
+    [[5, 5, 5, 5], 10],
+    [[7, 7, 7, 7, 7, 7], 21],
+    [[1, 2, 3, 4, 5, 6], 9],
+    [[6, 5, 4, 3, 2, 1], 9],
+    [[-5, -4, -3, -2], -8],
+    [[-10, -1, 1, 10], -9],
+    [[-10_000, 10_000, -9_999, 9_999], -1],
+    [[1, 100, 2, 99, 3, 98], 100 + 3],
+    [[0, 0, 0, 0, 0, 0], 0],
+    [[-10_000, 10_000], -10_000],
+]);
+it('solve', function (array $nums, int $expected) {
+    $instance = new ArrayPairSum;
+
+    $result = $instance->solve($nums);
+
+    expect($result)->toBe($expected);
+
+})->with('arrayPairSumData');
