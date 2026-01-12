c623808ac76f315543fefb4a39fd218b595a3087
Your Name <wearypossum4770@yahoo.com>
2026-01-12 12:22:20 -0500

    feat(dart): refactors challenge e6d0105
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 1 ms     | 100.00 % |
    | Memory    | 151.89 MB| 40.00 %  |
    
    Relates-to: #1122

--- lib/easy/relative_sort_array.dart
+++ lib/easy/relative_sort_array.dart
@@ -1,27 +1,21 @@
-import 'package:collection/collection.dart';
-
 List<int> relativeSortArray(List<int> arr1, List<int> arr2) {
   List<int> result = List.filled(arr1.length, 0);
-  Map<int, int> freq = {};
+  List<int> freq = List.filled(1_001, 0);
   int index = 0;
-  for (int n in arr1) {
-    freq[n] = (freq[n] ?? 0) + 1;
+  for(int num in arr1) {
+    freq[num]++;
   }
-  for (int n in arr2) {
-    int count = freq[n] ?? 0;
-    while (count > 0) {
-      count--;
-      result[index++] = n;
-    }
-    if (count == 0) freq.removeWhere((key, _) => key == n);
-  }
-  for (int entry in freq.keys.toList().sorted((a, b) => a - b)) {
-    int count = freq[entry] ?? 0;
-    while (count > 0) {
-      result[index++] = entry;
-      count--;
+  for(int num in arr2) {
+    while(freq[num] > 0) {
+      freq[num]--;
+      result[index++] = num;
     }
   }
-
+  for(int i = 0; i < freq.length; i++) {
+    while(freq[i]-- > 0)  {
+      result[index++] = i;
+      
+    }
+  }
   return result;
 }
