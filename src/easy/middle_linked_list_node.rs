2c2e1a9d48f889015d4a75e05a316eed8a5c9acc
Your Name <wearypossum4770@yahoo.com>
2026-01-24 17:14:52 -0500

    test(rs) test to challenge 743a9d4
    relates-to: #876

--- src/easy/middle_linked_list_node.rs
+++ src/easy/middle_linked_list_node.rs
@@ -17,3 +17,19 @@ where
     }
     slow.clone()
 }
+#[cfg(test)]
+mod tests {
+    use crate::helpers::linked_list::ListNode;
+
+    use super::middle_linked_list_node;
+    use rstest::rstest;
+    #[rstest]
+    #[case(vec![1,2,3,4,5], vec![3,4,5])]
+    #[case(vec![1,2,3,4,5,6], vec![4,5,6])]
+    fn test_middle_linked_list_node(#[case] input: Vec<i32>, #[case] output: Vec<i32>) {
+        let head = ListNode::from_vec(&input);
+        let outcome = middle_linked_list_node(head);
+        let expected = ListNode::to_vec(&outcome);
+        assert_eq!(output, expected);
+    }
+}
