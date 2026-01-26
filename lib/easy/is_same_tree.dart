d5e1745983d198a2018f7fadfb5412a9fd244b1f
Your Name <wearypossum4770@yahoo.com>
2026-01-26 00:40:31 -0500

    feat(dart): implement Same Tree challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 149.94 MB| 50.94 %  |
    
    relates-to: #100

---
+++ lib/easy/is_same_tree.dart
@@ -0,0 +1,8 @@
+import 'package:dark_coding_challenges/helpers/data_structures/tree_node.dart';
+
+bool isSameTree(TreeNode? p, TreeNode? q) {
+  if (p == null && q == null) return true;
+  if (p == null || q == null) return false;
+  if (p.val != q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
