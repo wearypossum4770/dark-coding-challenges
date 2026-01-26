3b1c6468dab892199af347da077dab2ebd897f5b
Your Name <wearypossum4770@yahoo.com>
2026-01-26 16:24:04 -0500

    test(ts) test to challenge 6712002
    relates-to: #100

---
+++ test/easy/is-same-tree.test.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect } from "bun:test";
+import { isSameTree } from "@/src/easy/is-same-tree";
+import { TreeNode } from "@/src/helpers/data-structures";
+describe("isSameTree", () => {
+	test.each([
+		["1 2 x x 3 x x", "1 2 x x 3 x x", true],
+		["1 2 x x 3 x x", "1 2 x x x", false],
+		["1 2 x x 3 x x", "1 x 2 x x", false],
+		["1 2 3 x x x x", "1 x 2 x 3 x x", false],
+		["x", "x", true],
+		["1 x x", "x", false],
+		["1 x x", "1 x x", true],
+		["1 x x", "2 x x", false],
+		["1 2 x x x", "1 x 2 x x", false],
+		["1 2 3 x x x x", "1 2 3 x x x x", true],
+		["1 2 3 x x x x", "1 2 4 x x x x", false],
+		["5 3 1 x x 4 x x 7 6 x x 9 x x", "5 3 1 x x 4 x x 7 6 x x 9 x x", true],
+		["1 2 4 x x 5 x x 3 6 x x 7 x x", "1 2 4 x x 5 x x 3 6 x x 8 x x", false],
+		["1 2 x x 3 x x", "1 x 2 x 3 x x", false],
+		[
+			"10 5 2 x x 7 x x 15 12 x x 20 x x",
+			"10 5 2 x x 7 x x 15 12 x x 20 x x",
+			true,
+		],
+		["1 x 2 x 3 x 4 x x", "1 x 2 x 3 x 4 x x", true],
+		["4 3 2 1 x x x x x", "4 3 2 1 x x x x x", true],
+		[
+			"8 4 2 1 x x 3 x x 6 5 x x 7 x x 12 10 9 x x 11 x x 14 13 x x 15 x x",
+			"8 4 2 1 x x 3 x x 6 5 x x 7 x x 12 10 9 x x 11 x x 14 13 x x 15 x x",
+			true,
+		],
+		["3 x 4 x 5 x 6 x x", "3 x 4 x 5 x 7 x x", false],
+		[
+			"100 50 25 x x 75 x x 150 125 x x 175 x x",
+			"100 50 25 x x 75 x x 150 125 x x 175 x x",
+			true,
+		],
+	])("\n\tGiven: the trees $p and $p.\n\tThen they are equal %p.", (a: string, b: string, expected: boolean) => {
+		const p = TreeNode.fromString(a);
+		const q = TreeNode.fromString(b);
+		const result = isSameTree(p, q);
+		expect(result).toStrictEqual(expected);
+	});
+});
