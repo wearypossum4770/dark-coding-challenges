381307f191d7be4b22730ba14452f4764c2ea6e3
Your Name <wearypossum4770@yahoo.com>
2026-01-26 16:26:43 -0500

    test(java) test to challenge d9b72f8
    relates-to: #100

---
+++ src/test/java/easy/IsSameTreeTest.java
@@ -0,0 +1,54 @@
+package easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import helpers.TreeNode;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class IsSameTreeTest {
+  IsSameTree<Integer> instance = new IsSameTree<>();
+
+  @ParameterizedTest
+  @MethodSource("algoMonsterData")
+  public void algoMonsterTest(String a, String b, boolean expected) {
+    TreeNode<Integer> p = TreeNode.fromString(a);
+    TreeNode<Integer> q = TreeNode.fromString(b);
+    boolean result = instance.solve(p, q);
+
+    assertEquals(expected, result);
+  }
+
+  public static Stream<Arguments> algoMonsterData() {
+    return Stream.of(
+        Arguments.of("1 2 x x 3 x x", "1 2 x x 3 x x", true),
+        Arguments.of("1 2 x x 3 x x", "1 2 x x x", false),
+        Arguments.of("1 2 x x 3 x x", "1 x 2 x x", false),
+        Arguments.of("1 2 3 x x x x", "1 x 2 x 3 x x", false),
+        Arguments.of("x", "x", true),
+        Arguments.of("1 x x", "x", false),
+        Arguments.of("1 x x", "1 x x", true),
+        Arguments.of("1 x x", "2 x x", false),
+        Arguments.of("1 2 x x x", "1 x 2 x x", false),
+        Arguments.of("1 2 3 x x x x", "1 2 3 x x x x", true),
+        Arguments.of("1 2 3 x x x x", "1 2 4 x x x x", false),
+        Arguments.of("5 3 1 x x 4 x x 7 6 x x 9 x x", "5 3 1 x x 4 x x 7 6 x x 9 x x", true),
+        Arguments.of("1 2 4 x x 5 x x 3 6 x x 7 x x", "1 2 4 x x 5 x x 3 6 x x 8 x x", false),
+        Arguments.of("1 2 x x 3 x x", "1 x 2 x 3 x x", false),
+        Arguments.of(
+            "10 5 2 x x 7 x x 15 12 x x 20 x x", "10 5 2 x x 7 x x 15 12 x x 20 x x", true),
+        Arguments.of("1 x 2 x 3 x 4 x x", "1 x 2 x 3 x 4 x x", true),
+        Arguments.of("4 3 2 1 x x x x x", "4 3 2 1 x x x x x", true),
+        Arguments.of(
+            "8 4 2 1 x x 3 x x 6 5 x x 7 x x 12 10 9 x x 11 x x 14 13 x x 15 x x",
+            "8 4 2 1 x x 3 x x 6 5 x x 7 x x 12 10 9 x x 11 x x 14 13 x x 15 x x",
+            true),
+        Arguments.of("3 x 4 x 5 x 6 x x", "3 x 4 x 5 x 7 x x", false),
+        Arguments.of(
+            "100 50 25 x x 75 x x 150 125 x x 175 x x",
+            "100 50 25 x x 75 x x 150 125 x x 175 x x",
+            true));
+  }
+}
