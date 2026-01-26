aa64f50f9b742a906f71f9f8093ef8875e395479
Your Name <wearypossum4770@yahoo.com>
2026-01-26 00:38:17 -0500

    feat(go): implement Same Tree challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 4.22 MB  | 11.34 %  |
    
    relates-to: #100

---
+++ easy/is_same_tree.go
@@ -0,0 +1,14 @@
+package easy
+
+func IsSameTree(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return IsSameTree(p.Left, q.Left) && IsSameTree(p.Right, q.Right)
+}
