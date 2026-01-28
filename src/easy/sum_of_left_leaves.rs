5cf49d61461a3325f8ed817593409a0116750835
Your Name <wearypossum4770@yahoo.com>
2026-01-28 09:48:58 -0500

    test(rs) test to challenge b30dce4
    relates-to: #404

--- src/easy/sum_of_left_leaves.rs
+++ src/easy/sum_of_left_leaves.rs
@@ -26,3 +26,29 @@ pub fn sum_of_left_leaves(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> i32 {
     }
     count
 }
+#[cfg(test)]
+mod tests {
+    use crate::helpers::tree_node::TreeNode;
+
+    use super::sum_of_left_leaves;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[], 0)]
+    #[case(&[Some(1)], 0)]
+    #[case(&[Some(1),Some(2)], 2)]
+    #[case(&[Some(1),None,Some(3)], 0)]
+    #[case(&[Some(1),Some(2),Some(3)], 2)]
+    #[case(&[Some(3),Some(9),Some(20),None,None,Some(15),Some(7)], 24)]
+    #[case(&[Some(1),Some(2),None,Some(3),None,Some(4)], 4)]
+    #[case(&[Some(1),None,Some(2),None,Some(3),None,Some(4)], 0)]
+    #[case(&[Some(1),Some(2),Some(3),Some(4),Some(5)], 4)]
+    #[case(&[Some(1),Some(2),Some(3),None,Some(4)], 0)]
+    #[case(&[Some(0),Some(-1),Some(-2)], -1)]
+    #[case(&[Some(1),Some(2),None,None,Some(3)], 0)]
+    #[case(&[Some(1),None,Some(2),Some(3)], 3)]
+    #[case(&[Some(1),Some(2),Some(3),Some(4),None,None,Some(5)], 4)]
+
+    fn test_sum_of_left_leaves(#[case] nums: &[Option<i32>], #[case] expected: i32) {
+        assert_eq!(sum_of_left_leaves(TreeNode::from_slice(nums)), expected);
+    }
+}
