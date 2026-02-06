b25f8bacaa586eed6dd40831365213042ea8c447
Your Name <wearypossum4770@yahoo.com>
2026-02-05 23:26:55 -0500

    test(rs) test to challenge 9f390a4
    relates-to: #2413

--- src/easy/smallest_even_multiple.rs
+++ src/easy/smallest_even_multiple.rs
@@ -4,3 +4,38 @@ pub fn smallest_even_multiple(n: i32) -> i32 {
 	}
 	n
 }
+#[cfg(test)]
+mod tests {
+use super::smallest_even_multiple;
+use rstest::rstest;
+#[rstest]
+	#[case(1, 2)]
+    #[case(2, 2)]
+    #[case(3, 6)]
+    #[case(4, 4)]
+    #[case(5, 10)]
+    #[case(6, 6)]
+    #[case(7, 14)]
+    #[case(8, 8)]
+    #[case(9, 18)]
+    #[case(10, 10)]
+    #[case(11, 22)]
+    #[case(12, 12)]
+    #[case(13, 26)]
+    #[case(14, 14)]
+    #[case(15, 30)]
+    #[case(16, 16)]
+    #[case(17, 34)]
+    #[case(18, 18)]
+    #[case(19, 38)]
+    #[case(20, 20)]
+    #[case(149, 298)]
+    #[case(150, 150)]
+    #[case(1, 2)]      
+    #[case(2, 2)]      
+    #[case(150, 150)]  
+    #[case(149, 298)]  
+fn test_smallest_even_multiple(#[case] n: i32, #[case] expected: i32) {
+assert_eq!(smallest_even_multiple(n), expected);
+}
+}
