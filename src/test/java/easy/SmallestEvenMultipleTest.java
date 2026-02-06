e1246ad54b018de4579bffc68e7db394099ab557
Your Name <wearypossum4770@yahoo.com>
2026-02-05 23:27:41 -0500

    test(java) test to challenge 6d72383
    relates-to: #2413

---
+++ src/test/java/easy/SmallestEvenMultipleTest.java
@@ -0,0 +1,51 @@
+package easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class SmallestEvenMultipleTest {
+  SmallestEvenMultiple instance = new SmallestEvenMultiple();
+
+  @ParameterizedTest
+  @MethodSource("dataSmallestEvenMultiple")
+  public void testSmallestEvenMultiple(int n, int expected) {
+
+    int result = instance.solve(n);
+
+    assertEquals(expected, result);
+  }
+
+  public static Stream<Arguments> dataSmallestEvenMultiple() {
+    return Stream.of(
+        Arguments.of(1, 2),
+        Arguments.of(2, 2),
+        Arguments.of(3, 6),
+        Arguments.of(4, 4),
+        Arguments.of(5, 10),
+        Arguments.of(6, 6),
+        Arguments.of(7, 14),
+        Arguments.of(8, 8),
+        Arguments.of(9, 18),
+        Arguments.of(10, 10),
+        Arguments.of(11, 22),
+        Arguments.of(12, 12),
+        Arguments.of(13, 26),
+        Arguments.of(14, 14),
+        Arguments.of(15, 30),
+        Arguments.of(16, 16),
+        Arguments.of(17, 34),
+        Arguments.of(18, 18),
+        Arguments.of(19, 38),
+        Arguments.of(20, 20),
+        Arguments.of(149, 298),
+        Arguments.of(150, 150),
+        Arguments.of(1, 2),
+        Arguments.of(2, 2),
+        Arguments.of(150, 150),
+        Arguments.of(149, 298));
+  }
+}
