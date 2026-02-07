352af7f5840784208dc71b7feee89231ddec7ea3
Your Name <wearypossum4770@yahoo.com>
2026-02-06 22:21:34 -0500

    feat(rs): implement Remove Duplicate Letters challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 0 ms     | 100.00 % |
    | Memory    | 2.27 MB  | 21.05 %  |
    
    relates-to: #316

--- src/easy/remove_duplicate_zeros.rs
+++ src/easy/remove_duplicate_zeros.rs
@@ -1,4 +1,4 @@
-pub fn remove_duplicate_zeros(arr: &mut Vec<i32>) {
+pub fn remove_duplicate_zeros(arr: &mut [i32]) {
     let mut left = 0;
     let right = arr.len() - 1;
     while left <= right {
