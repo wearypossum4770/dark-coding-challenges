f1744133dff2a4458e37f041c40b727571de89e4
Your Name <wearypossum4770@yahoo.com>
2026-02-11 19:22:17 -0500

    feat(swift): implements Mountain Array challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 54 ms    | 34.43 %  |
    | Memory    | 17.48 MB | 50.82 %  |
    
    Relates-to: #941

---
+++ src/easy/ValidMountainArray.swift
@@ -0,0 +1,17 @@
+func validMountainArray(_ arr: [Int]) -> Bool {
+  let n = arr.count
+  if n < 3 { return false }
+  var i: Int = 0
+
+  while i + 1 < n && arr[i] < arr[i + 1] {
+    i += 1
+  }
+  if i == 0 || i == n - 1 {
+    return false
+  }
+  while i + 1 < n && arr[i] > arr[i + 1] {
+    i += 1
+  }
+
+  return i == n - 1
+}
