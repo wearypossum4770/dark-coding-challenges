24061e4a94f301f1ffdb99af8bfbacce0a9aacea
Your Name <wearypossum4770@yahoo.com>
2026-02-16 01:34:02 -0500

    test(php) test to challenge f526efc6
    relates-to: #101

---
+++ test/easy/IsTreeSymmetricTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use Src\Easy\IsTreeSymmetric;
+use Src\Helpers\TreeNode;
+
+dataset('isTreeSymmetricData', [
+    [[1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5], true],
+    [[1, 2, 2, 3, 4, 4, 3], true],
+    [[], true],
+    [[1], true],
+    [[1, 2, 2], true],
+    [[1, 2, 3], false],
+    [[1, 2, 2, 3, 4, 4, 3], true],
+    [[1, 2, 2, 3, 4, 4, 5], false],
+    [[1, 2, 2, 3, null, null, 3], true],
+    [[1, 2, 2, null, 3, null, 3], false],
+    [[1, 2, 2, 3, 3, null, null], false],
+    [[1, null, null], true],
+    [[1, 2, null], false],
+    [[1, null, 2], false],
+    [[1, 2, 2, 3, 4, null, 4, 5, 6, null, null, 6, 5], false],
+    [[1, 2, 2, 3, 4, null, 4, 5, 6, null, null, 7, 5], false],
+    [[0], true],
+    [[0, 0, 0], true],
+    [[0, 0, 1], false],
+    [[1, 2, 2, 2, 2, 2, 2], true],
+    [[1, 2, 3, 4, 5, 6, 7], false],
+    [[1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4], true],
+    [[5, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2], true],
+    [[1, 2, null, 3, null, null, null], false],
+    [[1, null, null, null, null, null, null], true],
+    [[2, 3, 3, 4, 5, 5, 4, 6, 7, 8, 8, 7, 6], false],
+    [[1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5], true],
+    [[1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 6], false],
+    [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], true],
+]);
+it('solve', function (array $nums, bool $expected) {
+    $instance = new IsTreeSymmetric;
+    $root = TreeNode::fromArray($nums);
+    $result = $instance->solve($root);
+
+    expect($result)->toBe($expected);
+
+})->with('isTreeSymmetricData');
