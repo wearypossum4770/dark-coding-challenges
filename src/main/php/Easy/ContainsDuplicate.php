b05be00b3567f7746b1c823297df9263fb9bd2ce
Your Name <wearypossum4770@yahoo.com>
2026-02-16 21:52:57 -0500

    feat(php): implements Contains Duplicate challenge solution
     # 📊 Metrics
    
    | Resource  | Value    | Stats    |
    | --------- | -------- | -------- |
    | Runtime   | 11 ms    | 37.67 %  |
    | Memory    | 29.52 MB | 81.17 %  |
    
    Relates-to: #217

---
+++ src/main/php/Easy/ContainsDuplicate.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Easy;
+
+class ContainsDuplicate
+{
+    public function transformContainsDuplicate() {}
+
+    public function solve(int $n): array
+    {
+        return $this->transformContainsDuplicate($n);
+    }
+}
