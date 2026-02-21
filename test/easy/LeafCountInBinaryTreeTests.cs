ac0dbc8bd797f8ff6a50c23671b742fa0acbebad
Your Name <wearypossum4770@yahoo.com>
2026-02-20 23:41:07 -0500

    test(dotnet) test to challenge 9d757fd
    relates-to: #404

---
+++ test/easy/LeafCountInBinaryTreeTests.cs
@@ -0,0 +1,43 @@
+namespace Easy;
+
+using DataStructures;
+
+public class LeafCountInBinaryTreeTests
+{
+    public static TheoryData<int?[], int> LeafCountInBinaryTreeData =>
+        new()
+        {
+            { [1, 10, 39, 5], 2 },
+            { [4, 8, 10, 7, null, 5, 1, 3], 3 },
+            { [50, 30, 70, 20, 40, 60, 80, null, null, null, null, 65], 4 },
+            { [30, 25, 35, 20, 28, null, 40, null, null, 27], 3 },
+            { [1, 2, 3, -1, -1, -1, -1], 4 },
+            { [1, 2, 3, null, null, null, null], 2 },
+            { [1, 2, 3, 4, 5, null, null, null, null, null, null], 3 },
+            { [10, 20, 30, 40, 50, 60, 70], 4 },
+            { [null], 0 },
+            { [5], 1 },
+            { [1, null, null], 1 },
+            { [1, 2, null, 3, null, 4, null, 5, null], 1 },
+            { [1, null, 2, null, 3, null, 4, null, 5, null, 6], 1 },
+            { [50, 30, 70, 20, 40, 60, 80, null, null, null, null, null, null, null, null], 4 },
+            // // Difficult / degenerate cases
+            { [1, 2, null, 3, null, 4, null, 5, null, 6, null, 7, null, 8, null, 9, null, 10], 1 },
+            { [-5, -8, -3, -12, -7, null, null, null, null, null, null], 3 },
+            { [100000, 99999, 1, 2, null, null, null, 3, 4, 5, 6], 4 },
+            { [-42, 42, -42, 42, -42, 42, -42], 4 },
+            { [7, null, 8, null, 9, null, 10, null, 11, null, 12, null, 13], 1 },
+            { [1, 2, 3, null, null, null, 4, null, null, null, null, null, null, null, 5], 2 },
+            { [99999, null, null], 1 },
+        };
+
+    [Theory]
+    [MemberData(nameof(LeafCountInBinaryTreeData))]
+    public void ShouldLeafCountInBinaryTree(int?[] input, int expected)
+    {
+        var instance = new LeafCountInBinaryTree();
+        var root = TreeNode.FromIntegerList(input);
+        var result = instance.Solve(root);
+        Assert.Equal(expected, result);
+    }
+}
