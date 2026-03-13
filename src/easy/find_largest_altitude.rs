61549af27977f6f5197b9ee1f79c18757212df24
Your Name <wearypossum4770@yahoo.com>
2026-03-13 18:05:42 -0400

    test(rs): test to challenge c6ffd0a
    relates-to: #1732

--- src/easy/find_largest_altitude.rs
+++ src/easy/find_largest_altitude.rs
@@ -10,3 +10,33 @@ pub fn find_largest_altitude(gain: Vec<i32>) -> i32 {
         None => 0,
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::find_largest_altitude;
+    use rstest::rstest;
+    #[rstest]
+    #[case(&[-5, 1, 5, 0, -7], 1)]
+    #[case(&[-4, -3, -2, -1, 4, 3, 2], 0)]
+    #[case(&[0], 0)]
+    #[case(&[100], 100)]
+    #[case(&[-100], 0)]
+    #[case(&[1], 1)]
+    #[case(&[0, 0, 0, 0], 0)]
+    #[case(&[1, 2, 3, 4, 5], 15)]
+    #[case(&[-1, -2, -3, -4, -5], 0)]
+    #[case(&[5, 5, 5, 5], 20)]
+    #[case(&[-5, -5, -5, -5], 0)]
+    #[case(&[10, -20, 10], 10)]
+    #[case(&[1, 1, 1, 1, -100], 4)]
+    #[case(&[-100, 1, 1, 1, 1], 0)]
+    #[case(&[50, -100, 50, -100, 50], 50)]
+    #[case(&[100, -100, -100, -100], 100)]
+    #[case(&[-1, -1, -1, 1, 1], 0)]
+    #[case(&[1, -1, 1, -1, 1, -1], 1)]
+    #[case(&[1, 1, 1, 1, 1], 5)]
+    #[case(&[-100], 0)]
+    #[case(&[100], 100)]
+    fn test_find_largest_altitude(#[case] gain: &[i32], #[case] expected: i32) {
+        assert_eq!(find_largest_altitude(gain.to_vec()), expected);
+    }
+}
