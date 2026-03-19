392a67fe38e912fa5be62374bd6d140b6d721db4
Your Name <wearypossum4770@yahoo.com>
2026-03-19 01:07:05 -0400

    feat(cpp): implements header file for challenge cfe5d5b7
    Relates-to: #2108

---
+++ src/main/cpp/easy/find_first_palindrome.h
@@ -0,0 +1,9 @@
+#ifndef FIND_FIRST_PALINDROME_H
+#define FIND_FIRST_PALINDROME_H
+#include <iostream>
+#include <string>
+#include <vector>
+bool checkPalindrome(std::string word);
+std::string firstPalindrome(std::vector<std::string>& words);
+
+#endif
