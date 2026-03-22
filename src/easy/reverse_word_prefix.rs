2e4645f70747d7fb83be65e93b574631dc848499
Your Name <wearypossum4770@yahoo.com>
2026-03-22 09:36:37 -0400

    test(rs): validates solution to challenge 4bd1431
    Relates-to: #2000

--- src/easy/reverse_word_prefix.rs
+++ src/easy/reverse_word_prefix.rs
@@ -1,28 +1,28 @@
 pub fn reverse_word_prefix(word: String, ch: char) -> String {
-let mut result = String::new();
+    let mut result = String::new();
 
-for (i, c) in word.char_indices() {
-	if c == ch {
-		let mut right = i+1;
-	for left in (0..=i).rev(){
-			match word.chars().nth(left) {
-				None => {},
-				Some(d) => {
-					result.push(d); 
-				},
-			};
-		}
-		while right <= word.len() {
-				match word.chars().nth(right) {
-					None => {},
-					Some(d) => {
-						result.push(d);
-					},
-				};
-				right += 1;
-		}
-		return result;
-	}
-}
-word
+    for (i, c) in word.char_indices() {
+        if c == ch {
+            let mut right = i + 1;
+            for left in (0..=i).rev() {
+                match word.chars().nth(left) {
+                    None => {}
+                    Some(d) => {
+                        result.push(d);
+                    }
+                };
+            }
+            while right <= word.len() {
+                match word.chars().nth(right) {
+                    None => {}
+                    Some(d) => {
+                        result.push(d);
+                    }
+                };
+                right += 1;
+            }
+            return result;
+        }
+    }
+    word
 }
