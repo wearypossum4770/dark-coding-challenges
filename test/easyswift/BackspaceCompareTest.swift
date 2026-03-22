5e7a4994308329116232e5570787cf7039f3a9b3
Your Name <wearypossum4770@yahoo.com>
2026-03-22 09:20:00 -0400

    test(swift): validates solution to challenge d546179
    Relates-to: #844

---
+++ test/easyswift/BackspaceCompareTest.swift
@@ -0,0 +1,51 @@
+import Testing
+
+@testable import easy
+
+@Test(arguments: [
+  ("y#fo##f", "y#f#o##f", true),
+  ("ab#c", "ad#c", true),
+  ("ab##", "c#d#", true),
+  ("a#c", "b", false),
+  ("a", "a", true),
+  ("a", "b", false),
+  ("#", "#", true),
+  ("#a", "a", true),
+  ("a#", "", true),
+  ("a#", "#", true),
+  ("##", "#", true),
+  ("###", "##", true),
+  ("a##", "#", true),
+  ("a##", "", true),
+  ("a#b", "b", true),
+  ("a#b", "a", false),
+  ("abc", "abc", true),
+  ("abc", "abd", false),
+  ("ab#c", "ab#c", true),
+  ("ab##c", "c", true),
+  ("a#b#c#", "", true),
+  ("a#b#c#d", "d", true),
+  ("a#b#c#d", "e", false),
+  ("#a#b#", "", true),
+  ("bxj##tw", "bxj##tw", true),
+  ("bxj##tw", "bxk##tw", true),
+  ("xywrrmp", "xywrrmu", false),
+  ("xywrrmp", "xywrrmp", true),
+  ("a###b", "b", true),
+  ("a###b", "a", false),
+  ("####", "", true),
+  ("####", "####", true),
+  ("a####", "#", true),
+  ("ab####", "##", true),
+  ("nzp#o#g", "b#nzp#o#g", true),
+  ("bbbextm", "bbb#extm", false),
+  ("ab#c#", "a", true),
+  ("a#a#a#", "", true),
+  ("abc##", "a", true),
+  ("abc###", "", true),
+  ("", "#", true),
+  ("#", "", true),
+])
+func backspaceCompareTest(s: String, t: String, expect: Bool) {
+  #expect(backspaceCompare(s, t) == expect)
+}
