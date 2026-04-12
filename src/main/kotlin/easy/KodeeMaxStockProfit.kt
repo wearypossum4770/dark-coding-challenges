97ab7c9c4ee697cd8489c5637174c129409f263d
Your Name <wearypossum4770@yahoo.com>
2026-04-11 23:22:28 -0400

    feat(kt): implements Max Stock Profit challenge solution
     # 📊 Metrics
    
    | Resource | Value   | Stats    |
    | -------- | ------- | -------- |
    | Runtime  | 5 ms    | 23.92 %  |
    | Memory   | 65.32 MB| 56.88 %  |
    
    Relates-to: #121

---
+++ src/main/kotlin/easy/KodeeMaxStockProfit.kt
@@ -0,0 +1,12 @@
+package easy
+import kotlin.math.max
+import kotlin.math.min
+fun kodeeMaxStockProfit(prices: IntArray): Int  {
+	var top = 0
+	var minPrice = prices[0]
+	for(price in prices) {
+		minPrice = min(price, minPrice)
+		top = max(top, price - minPrice)
+	}
+	return top
+}
