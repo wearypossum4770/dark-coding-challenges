ce5abab026f77d013d0a6858a806a097e97301de
Your Name <wearypossum4770@yahoo.com>
2026-04-11 23:26:08 -0400

    feat(swift): implements Max Stock Profit challenge solution
     # 📊 Metrics
    
    | Resource | Value   | Stats    |
    | -------- | ------- | -------- |
    | Runtime  | 0 ms    | 100.00 % |
    | Memory   | 20.47 MB| 99.65 %  |
    
    Relates-to: #121

---
+++ src/easy/MaxStockProfit.swift
@@ -0,0 +1,9 @@
+func maxStockProfit(_ prices: [Int])-> Int {
+	var minPrice = prices[0]
+	var maxProfit = minPrice - minPrice
+	for price in prices {
+		minPrice = min(minPrice, price)
+		maxProfit = max(maxProfit, price - minPrice)
+	}
+	return maxProfit
+}
