421016ed4754479e516f3043fadedd5c180fc285
Your Name <wearypossum4770@yahoo.com>
2026-04-13 18:23:50 -0400

    chore(kt): lint and format challenge 97ab7c9
    Relates-to: #121

--- src/main/kotlin/easy/KodeeMaxStockProfit.kt
+++ src/main/kotlin/easy/KodeeMaxStockProfit.kt
@@ -1,12 +1,13 @@
 package easy
 import kotlin.math.max
 import kotlin.math.min
-fun kodeeMaxStockProfit(prices: IntArray): Int  {
-	var top = 0
-	var minPrice = prices[0]
-	for(price in prices) {
-		minPrice = min(price, minPrice)
-		top = max(top, price - minPrice)
-	}
-	return top
+
+fun kodeeMaxStockProfit(prices: IntArray): Int {
+    var top = 0
+    var minPrice = prices[0]
+    for (price in prices) {
+        minPrice = min(price, minPrice)
+        top = max(top, price - minPrice)
+    }
+    return top
 }
