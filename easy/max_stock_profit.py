98d5b7cbd50e2aacb3291e38c5f66b22dcd7f419
Your Name <wearypossum4770@yahoo.com>
2026-04-13 21:06:55 -0400

    chore(py): lints & formats challenge 377d978
    Relates-to: #121

--- easy/max_stock_profit.py
+++ easy/max_stock_profit.py
@@ -1,7 +1,7 @@
 def max_stock_profit(prices: list[int]) -> int:
-	mn = prices[0]
-	mx = mn - mn
-	for price in prices:
-		mn = min(mn, price)
-		mx = max(mx, price - mn)
-	return mx
+    mn = prices[0]
+    mx = mn - mn
+    for price in prices:
+        mn = min(mn, price)
+        mx = max(mx, price - mn)
+    return mx
