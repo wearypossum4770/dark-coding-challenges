2ff351cdfcb356f34f2127059ce0aa31a46a1484
wearypossum4770 <50812322+wearypossum4770@users.noreply.github.com>
2026-04-14 01:13:31 +0000

    Dusting

--- src/main/php/Easy/MaxStockProfit.php
+++ src/main/php/Easy/MaxStockProfit.php
@@ -1,21 +1,23 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Src\Easy;
-class MaxStockProfit {
-    /**
-     * @param Integer[] $prices
-     * @return Integer
-     */
 
-public function solve(array $prices): int
+class MaxStockProfit
 {
-	$min =  $prices[0];
-	$max = $min - $min;
-	foreach($prices as $price) {
-		$min = min($min, $price);
-		$max = max($max, $price - $min);
-	}
-return $max;
-}
+    /**
+     * @param  int[]  $prices
+     */
+    public function solve(array $prices): int
+    {
+        $min = $prices[0];
+        $max = $min - $min;
+        foreach ($prices as $price) {
+            $min = min($min, $price);
+            $max = max($max, $price - $min);
+        }
 
+        return $max;
+    }
 }
