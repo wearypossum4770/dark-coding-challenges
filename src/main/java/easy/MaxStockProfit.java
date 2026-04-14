551b429558b91b71d360ab2d262b48a5988e3f23
Your Name <wearypossum4770@yahoo.com>
2026-04-13 21:09:02 -0400

    chore(java): lint and format challenge 2f45c5e
    Relates-to: #121

--- src/main/java/easy/MaxStockProfit.java
+++ src/main/java/easy/MaxStockProfit.java
@@ -1,11 +1,13 @@
 package easy;
+
 public class MaxStockProfit {
-public int solve(int[] prices) {
-	int min = prices[0];
-	int max = 0;
-	for(int price: prices) {
-		min = Math.min(min, price);
-		max = Math.max(max, price - min);
-	}
-return max;
-}}
+  public int solve(int[] prices) {
+    int min = prices[0];
+    int max = 0;
+    for (int price : prices) {
+      min = Math.min(min, price);
+      max = Math.max(max, price - min);
+    }
+    return max;
+  }
+}
